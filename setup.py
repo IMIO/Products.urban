35bf41ef3e69755b6e911c7ba1f0e188fe97fe58
Godefroid Chapelle <gotcha@bubblenet.be>
2011-04-06 10:58:58 +0000

    import first step

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0b1'
+
+setup(name='Products.urban',
+      version=version,
+      description="Urban Certificate Management",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='',
+      author_email='',
+      url='http://www.communesplone.org/les-outils/applications-metier/'
+          'gestion-des-permis-durbanisme',
+      license='GPL',
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
+      namespace_packages=['Products'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'appy',
+          'Plone',
+          'Products.MasterSelectWidget',
+      ],
+      entry_points="""
+      """,
+      )
