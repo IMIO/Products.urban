3296fbee961f32719f328bfdd9821e118655020f
Gauthier Bastien <g.bastien@imio.be>
2011-05-10 14:36:53 +0000

    Adapted portletFirefox to FirefoxViewlet

--- src/Products/urban/browser/viewlets.py
+++ src/Products/urban/browser/viewlets.py
@@ -1,10 +1,12 @@
 # ------------------------------------------------------------------------------
 from plone.app.layout.viewlets import ViewletBase
-from zope.component import getMultiAdapter
-from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
-from Products.CMFCore.utils import getToolByName
-
 # ------------------------------------------------------------------------------
-class portletFirefox(ViewletBase):
+class FirefoxViewlet(ViewletBase):
     '''This viewlet displays the firefox-text if browser isn't firefox.'''    
-    index = ViewPageTemplateFile("portlet_firefox.pt")
+
+    def show(context):
+        """
+          Check if we need to show the viewlet content or not
+        """
+        #show the viewlet if we are not using Firefox
+        return 'Firefox' not in context.REQUEST.get('HTTP_USER_AGENT', '')
