69297f1c6cd9eb31067281368166f865e8006575
Gauthier Bastien <g.bastien@imio.be>
2011-05-11 08:27:09 +0000

    Do the viewlet work please

--- src/Products/urban/browser/viewlets.py
+++ src/Products/urban/browser/viewlets.py
@@ -4,9 +4,9 @@ from plone.app.layout.viewlets import ViewletBase
 class FirefoxViewlet(ViewletBase):
     '''This viewlet displays the firefox-text if browser isn't firefox.'''    
 
-    def show(context):
+    def show(self):
         """
           Check if we need to show the viewlet content or not
         """
         #show the viewlet if we are not using Firefox
-        return 'Firefox' not in context.REQUEST.get('HTTP_USER_AGENT', '')
+        return 'Firefox' not in self.request.get('HTTP_USER_AGENT', '')
