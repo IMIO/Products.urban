f9ee1013b233010f1d41ddf81586dbbec81d546b
Stephan Geulette <s.geulette@imio.be>
2011-05-18 14:24:08 +0000

    improved cleaning of architects

--- src/Products/urban/profiles/mons/exportimport.py
+++ src/Products/urban/profiles/mons/exportimport.py
@@ -1,12 +1,14 @@
-def cleanArchitects(self):
+def cleanArchitects():
     """
     """
-    filePath="/home/gbastien/urban/archis_mons.csv"
-    outFilePath="/home/gbastien/urban/archis_mons2.csv"
+#    filePath="/home/gbastien/urban/archis_mons.csv"
+    filePath="./files/architects.csv"
+#    outFilePath="/home/gbastien/urban/archis_mons2.csv"
+    outFilePath="./archis_mons2.csv"
     separator = ";"
 
     file = open(filePath, 'r')
-    outFile = open(filePath, 'r')
+    outFile = open(outFilePath, 'w')
     numberOfRecords = len(file.readlines())
     file.seek(0)
     i = 1
@@ -16,14 +18,21 @@ def cleanArchitects(self):
         #work on the address, here, the addressstreet contains informations about the addresshousnumber...
         addressInfos = addressstreet.split(',')
         street = number = ''
-        if not len(addressInfos) == 2:
+        if not addressstreet.strip():
+            pass
+        elif not len(addressInfos) == 2:
             errors.append("Address error line %d" % i)
         else:
-            street = addressInfos[0]
-            number = addressInfos[1]
+            street = addressInfos[0].strip()
+#            street = addressstreet
+            number = addressInfos[1].strip()
         #work on the name
         #humpfff...
-        outFile.write("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s" % title, name, firstname, society, street, number, addresszipcode, addresscity, email, telephone, fax, nrnumber
+        outFile.write("%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n" % (title, name, firstname, society, street, number, addresszipcode, addresscity, email, telephone, fax, nrnumber))
         i = i + 1
     file.close()
     outFile.close()
+    print '\n'.join(errors)
+
+if __name__ == '__main__':
+    cleanArchitects()
