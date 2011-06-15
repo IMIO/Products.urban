966d2acbef77ef902abcf80e149086ae2b6b6958
Gauthier Bastien <g.bastien@imio.be>
2011-06-15 15:20:56 +0000

    Removed EryrOpenLayers from DEPENDENCIES

--- src/Products/urban/config.py
+++ src/Products/urban/config.py
@@ -112,7 +112,7 @@ ADD_EMPTY_VOCAB_VALUE = True
 #topics
 TOPIC_TYPE = "urban_topic_type"
 #dependencies
-DEPENDENCIES = ["EryrOpenLayers","ReferenceDataGridField",]
+DEPENDENCIES = ["ReferenceDataGridField",]
 #name of the folder created in a licence that will contain additional
 #layers linked to the licence and used in the mapfile generation
 ADDITIONAL_LAYERS_FOLDER="additional_layers"
