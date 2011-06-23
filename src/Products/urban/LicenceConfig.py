46ff9d401477e91ec0fbe99b7e848d18e24c61d3
Stephan Geulette <s.geulette@imio.be>
2011-06-23 12:42:11 +0000

    changed default view

--- src/Products/urban/LicenceConfig.py
+++ src/Products/urban/LicenceConfig.py
@@ -102,6 +102,8 @@ class LicenceConfig(BaseFolder, BrowserDefaultMixin):
                     field.widget.label_msgid, domain=field.widget.i18n_domain, default=field.widget.label)))
         return DisplayList(tuple(res))
 
+
+
 registerType(LicenceConfig, PROJECTNAME)
 # end of class LicenceConfig
 
