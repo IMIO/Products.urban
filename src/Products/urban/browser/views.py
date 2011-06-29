9a64455ec8be750e98bf28249a08b152d21ff08a
Nicolas Dufrane <nicolas.dufrane@opengeode.be>
2011-06-29 10:15:59 +0000

    Cleanup & Simplification for additional layers

--- src/Products/urban/browser/views.py
+++ src/Products/urban/browser/views.py
@@ -26,19 +26,17 @@ class WMC(BrowserView):
         tool=getToolByName(context, "portal_urban")
         defaulturl='http://'+tool.getWebServerHost()+'/geoserver/wms'
         layers = [
-                {'url' : defaulturl,'srs':'EPSG:31370','title':'Parcellaire','name' : 'urban'+tool.getNISNum()+':capa','format':'image/png','style':'default','opacity':0.5, 'visibility': 'false','transparent':'true'},
-                {'url' : defaulturl,'srs':'EPSG:31370','title':'Noms de rue','name' : 'urban'+tool.getNISNum()+':toli','format':'image/png','style':'default','opacity':0.5,'visibility':'false','transparent':'true'},
-                {'url' : defaulturl,'srs':'EPSG:31370','title':'N° de parcelle','name' : 'urban'+tool.getNISNum()+':canu','format':'image/png','style':'ParcelsNum','opacity':0.5, 'visibility':'false','transparent':'true'},
+                {'url' : defaulturl,'srs':'EPSG:31370','title':'Parcellaire','name' : 'urban'+tool.getNISNum()+':capa','format':'image/png','style':'default'},
+                {'url' : defaulturl,'srs':'EPSG:31370','title':'Noms de rue','name' : 'urban'+tool.getNISNum()+':toli','format':'image/png','style':'default'},
+                {'url' : defaulturl,'srs':'EPSG:31370','title':'N° de parcelle','name' : 'urban'+tool.getNISNum()+':canu','format':'image/png','style':'ParcelsNum'},
                 ]
         for additional_layer in tool.additional_layers.objectValues():
             if additional_layer.getWMSUrl()=="":
                 url=defaulturl
             else:
                 url=additional_layer.getWMSUrl()
-            opacity=1
-            if additional_layer.getTransparent() == True:
-                opacity=0
-            layers.append({'url' : url,'srs':additional_layer.getSRS(),'title':additional_layer.Title,'name' : additional_layer.getLayers(),'format':additional_layer.getLayerFormat(),'style':additional_layer.getStyles(),'opacity':opacity,'visibility':additional_layer.getVisibility(),'opacity':0.5})
+
+            layers.append({'url' : url,'srs':additional_layer.getSRS(),'title':additional_layer.Title,'name' : additional_layer.getLayers(),'format':additional_layer.getLayerFormat(),'style':additional_layer.getStyles()})
         return layers
     def wmc(self):
 
