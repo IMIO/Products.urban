efb3c22505983880a59e3049a4d6bb6ad301911f
Nicolas Dufrane <nicolas.dufrane@opengeode.be>
2011-06-29 12:22:57 +0000

    Fix Styles for additional layers : styles are now used. TODO: change style string to array of styles in config

--- src/Products/urban/browser/views.py
+++ src/Products/urban/browser/views.py
@@ -26,8 +26,8 @@ class WMC(BrowserView):
         tool=getToolByName(context, "portal_urban")
         defaulturl='http://'+tool.getWebServerHost()+'/geoserver/wms'
         layers = [
-                {'url' : defaulturl,'srs':'EPSG:31370','title':'Parcellaire','name' : 'urban'+tool.getNISNum()+':capa','format':'image/png','style':'default','hidden': 0},
-                {'url' : defaulturl,'srs':'EPSG:31370','title':'Noms de rue','name' : 'urban'+tool.getNISNum()+':toli','format':'image/png','style':'default','hidden': 0},
+                {'url' : defaulturl,'srs':'EPSG:31370','title':'Parcellaire','name' : 'urban'+tool.getNISNum()+':capa','format':'image/png','style':'','hidden': 0},
+                {'url' : defaulturl,'srs':'EPSG:31370','title':'Noms de rue','name' : 'urban'+tool.getNISNum()+':toli','format':'image/png','style':'','hidden': 0},
                 {'url' : defaulturl,'srs':'EPSG:31370','title':'N° de parcelle','name' : 'urban'+tool.getNISNum()+':canu','format':'image/png','style':'ParcelsNum','hidden': 0},
                 ]
         for additional_layer in tool.additional_layers.objectValues():
