5f555e7512f28714022a672fa002a3c670ebc8f8
Gauthier Bastien <g.bastien@imio.be>
2011-08-03 07:34:19 +0000

    Import 'logger' to be able to use it...

--- src/Products/urban/UrbanEventType.py
+++ src/Products/urban/UrbanEventType.py
@@ -23,6 +23,7 @@ from Products.CMFDynamicViewFTI.browserdefault import BrowserDefaultMixin
 from Products.urban.config import *
 
 ##code-section module-header #fill in your manual code here
+logger = logging.getLogger('urban: UrbanEventType')
 from archetypes.referencebrowserwidget import ReferenceBrowserWidget
 from Products.CMFPlone.i18nl10n import utranslate
 from Products.CMFCore.Expression import Expression, createExprContext
