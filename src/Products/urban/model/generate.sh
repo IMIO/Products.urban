95053311e2fff62e5f156cedcf8add66995f1e26
Simon Delcourt <delcourt.simon@gmail.com>
2011-08-18 06:35:58 +0000

    reverted generate.sh

--- src/Products/urban/model/generate.sh
+++ src/Products/urban/model/generate.sh
@@ -1,4 +1,4 @@
-/home/sde/srv/archgenxml/agx2/bin/archgenxml --cfg generate.conf urban.zargo
+/srv/archgenxml/agx26/bin/archgenxml --cfg generate.conf urban.zargo
 #manage generated.pot
 cp ../i18n/generated.pot ../locales
 rm -rf ../i18n
