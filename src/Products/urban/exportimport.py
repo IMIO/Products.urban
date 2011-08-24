668ee7e61e467b7dcab5958d8ce66bd692c33aca
André NUYENS <andre.nuyens@imio.be>
2011-08-24 07:37:55 +0000

    add some comments

--- src/Products/urban/exportimport.py
+++ src/Products/urban/exportimport.py
@@ -44,29 +44,42 @@ def addUrbanEventTypes(context):
                     id = "%s.odt" % template['id']
                     loginfo = id
                     title = template['title']
-                    
+                    #read odt template
                     filePath = '%s/templates/%s' % (context._profile_path, id)
                     fileDescr = file(filePath, 'rb')
                     fileContent = fileDescr.read()
+                    #calculate the md5 for new template
                     md5 = hashlib.md5(fileContent)
                     md5SignatureFS=md5.digest()
                     fileTemplate=getattr(newUet,id,None)
+                    #if file exist, we must verify some conditions before update template
                     if fileTemplate:
                         profileNamePlone=fileTemplate.getProperty("profileName")
+                        #don't modify ANY templates if
+                        #   1. executing profile is tests and profile in use isn't tests
+                        #   2. executing profile is 'xxx' and profile in use is 'yyy'
                         if profileNamePlone!="tests" and (profile_name != profileNamePlone or profile_name == "tests"):
                             break
-                        md5SignatureProperty=fileTemplate.getProperty("md5Signature")                        
+                        #get the md5 in property of current template
+                        md5SignatureProperty=fileTemplate.getProperty("md5Signature")
+                        #calculate the md5 for current template
                         md5 = hashlib.md5(fileTemplate.data)
                         md5SignaturePlone=md5.digest()
+                        #don't modify THIS template if
+                        #   1. current template was manually modified by user
+                        #   2. the new template is the same that the current
                         if md5SignaturePlone!=md5SignatureProperty or md5SignatureFS == md5SignaturePlone:
                             continue
                         newUetFile=fileTemplate
                         newUetFile.setFile(fileContent)
+                    #if file not exist, we can create template
                     else:
                         newUetFileId = newUet.invokeFactory("File", id=id, title=title, file=fileContent)
                         newUetFile = getattr(newUet, newUetFileId)
+                    #modify template's content
                     newUetFile.setContentType("application/vnd.oasis.opendocument.text")
                     newUetFile.setFilename(id)
+                    #modify properties
                     dictProperties=dict(newUetFile.propertyItems())
                     if dictProperties.has_key("md5Signature"):
                         newUetFile.manage_changeProperties({"md5Signature":md5SignatureFS})
