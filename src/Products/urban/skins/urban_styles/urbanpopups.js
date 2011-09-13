2877e48f403c4560ed736dd79571593023459056
Gauthier Bastien <g.bastien@imio.be>
2011-09-13 10:01:08 +0000

    Minor change

--- src/Products/urban/skins/urban_styles/urbanpopups.js
+++ src/Products/urban/skins/urban_styles/urbanpopups.js
@@ -1,8 +1,10 @@
+/* The jQuery here above will load a jQuery popup */
+
 jQuery(function($){
 
     // Long text popup
     $('#urban-claims-text a').prepOverlay({
        subtype: 'ajax',
        urlmatch: '@@claimstextview',
-    });
+   });
 });
