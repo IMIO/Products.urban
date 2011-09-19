0c12aac3a64643f80f17f5b68fc0b3e7f93fbd98
Gauthier Bastien <g.bastien@imio.be>
2011-09-19 10:21:05 +0000

    When an applicant is removed, we need to update the parent's title

--- src/Products/urban/events/applicantEvents.py
+++ src/Products/urban/events/applicantEvents.py
@@ -2,4 +2,7 @@ def onDelete(ob, event):
     """
       reindex licence after deletion
     """
-    ob.aq_inner.aq_parent.reindexObject(idxs=('applicantInfosIndex',))
+    parent = ob.aq_inner.aq_parent
+    #as the name of the applicant appears in the licence's title, update it!
+    parent.updateTitle()
+    parent.reindexObject(idxs=('Title', 'applicantInfosIndex',))
