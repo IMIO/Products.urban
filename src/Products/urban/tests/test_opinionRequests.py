97a4838b9cba22d50c351341e2f31b89080c1028
Simon Delcourt <delcourt.simon@gmail.com>
2011-10-06 14:32:37 +0000

    #2828 added test : link between an inquiry with opinion request and its corresponding UrbanEvent

--- src/Products/urban/tests/test_opinionRequests.py
+++ src/Products/urban/tests/test_opinionRequests.py
@@ -4,6 +4,7 @@ from time import sleep
 from zope.component import createObject
 from zope import event
 from Products.Archetypes.event import ObjectInitializedEvent
+from Products.Archetypes.event import EditBegunEvent
 from plone.app.testing import login
 from Products.urban.testing import URBAN_TESTS_PROFILE_FUNCTIONAL
 
@@ -28,9 +29,25 @@ class TestOpinionRequest (unittest.TestCase):
         self.failUnless(term in self.portal_urban.buildlicence.foldermakers.objectValues())
 
     def testCreateLinkedUrbanEventType(self):
-        #when adding a new OrganisationTerm, a corresponding UrbanEvent opinion request should be created as well 
+        #when adding a new OrganisationTerm, a corresponding UrbanEventType 'opinion request' should be created as well 
         #and linked to it
         term_id = self.portal_urban.buildlicence.foldermakers.invokeFactory('OrganisationTerm', id='voodoo', title='Vood00', description='gni')
         term = getattr(self.portal_urban.buildlicence.foldermakers, term_id)
         event.notify(ObjectInitializedEvent(term))
         self.failUnless(term.getLinkedOpinionRequestEvent() in self.portal_urban.buildlicence.urbaneventtypes.objectValues()) 
+
+    def testInquiryWithOpinionRequestIsLinkedToItsUrbanEventOpinionRequest(self):
+        #if there is an inquiry with an opinion request and that its corresponding UrbanEventOpinionRequest 
+        #is added, a link should be created between this inquiry and this UrbanEventOpinionRequest
+        #setting an investigation start date to activate the inquiry
+        self.buildLicence.setInvestigationStart('18/09/86')
+        self.buildLicence.setSolicitOpinionsTo('sncb')
+        self.portal_urban.createUrbanEvent(self.buildLicence.UID(), 
+                  getattr(self.portal_urban.buildlicence.urbaneventtypes, 'sncb-opinion-request').UID())
+        UrbanEventOpinionRequest_sncb = None
+        for content in self.buildLicence.objectValues():
+            if content.portal_type == 'UrbanEventOpinionRequest':
+                UrbanEventOpinionRequest_sncb = content
+                break
+        event.notify(EditBegunEvent(UrbanEventOpinionRequest_sncb))
+        self.failUnless(self.buildLicence.getLinkedUrbanEventOpinionRequest('sncb') == UrbanEventOpinionRequest_sncb)
