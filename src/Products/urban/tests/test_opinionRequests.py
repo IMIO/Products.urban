d61777e8e478739f0644eb5b9df2a78ae410531a
Simon Delcourt <delcourt.simon@gmail.com>
2011-10-06 12:55:41 +0000

    #2828 added tests : createOrganisationTerm

---
+++ src/Products/urban/tests/test_opinionRequests.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import unittest
+from time import sleep
+from zope.component import createObject
+from zope import event
+from Products.Archetypes.event import ObjectInitializedEvent
+from plone.app.testing import login
+from Products.urban.testing import URBAN_TESTS_PROFILE_FUNCTIONAL
+
+
+class TestOpinionRequest (unittest.TestCase):
+
+    layer = URBAN_TESTS_PROFILE_FUNCTIONAL
+
+    def setUp(self):
+        portal = self.layer['portal']
+        self.portal_urban = portal.portal_urban
+        urban = portal.urban
+        self.buildLicences = urban.buildlicences
+        LICENCE_ID = 'licence1'
+        login(portal, 'urbanmanager')
+        self.buildLicences.invokeFactory('BuildLicence', LICENCE_ID)
+        self.buildLicence = getattr(self.buildLicences, LICENCE_ID)
+    
+    def testCreateOrganisationTerm(self):
+        term_id = self.portal_urban.buildlicence.foldermakers.invokeFactory('OrganisationTerm', id='voodoo', title='Vood00', description='gni')
+        term = getattr(self.portal_urban.buildlicence.foldermakers, term_id, 'NOT FOUND RHAAAAAAAAAAAAAAAAAAAAAA!!!!')
+        self.failUnless(term in self.portal_urban.buildlicence.foldermakers.objectValues())
+
+    def testCreateLinkedUrbanEventType(self):
+        #when adding a new OrganisationTerm, a corresponding UrbanEvent opinion request should be created as well 
+        #and linked to it
+        import pdb; pdb.set_trace()
+        term_id = self.portal_urban.buildlicence.foldermakers.invokeFactory('OrganisationTerm', id='voodoo', title='Vood00', description='gni')
+        term = getattr(self.portal_urban.buildlicence.foldermakers, term_id)
+        event.notify(ObjectInitializedEvent(term))
+        self.failUnless(term.getLinkedOpinionRequestEvent() in self.portal_urban.buildlicence.urbaneventtypes.objectValues()) 
