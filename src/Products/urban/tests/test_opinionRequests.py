dca37624c9bd15c7f10c667c0c7509acd1f5fff8
Simon Delcourt <delcourt.simon@gmail.com>
2011-10-07 12:18:44 +0000

    #2828 added a test for the reordering method

--- src/Products/urban/tests/test_opinionRequests.py
+++ src/Products/urban/tests/test_opinionRequests.py
@@ -3,6 +3,7 @@ import unittest
 from time import sleep
 from zope.component import createObject
 from zope import event
+from zope.interface import alsoProvides
 from Products.Archetypes.event import ObjectInitializedEvent
 from Products.Archetypes.event import EditBegunEvent
 from plone.app.testing import login
@@ -34,11 +35,28 @@ class TestOpinionRequest (unittest.TestCase):
         term_id = self.portal_urban.buildlicence.foldermakers.invokeFactory('OrganisationTerm', id='voodoo', title='Vood00', description='gni')
         term = getattr(self.portal_urban.buildlicence.foldermakers, term_id)
         event.notify(ObjectInitializedEvent(term))
-        self.failUnless(term.getLinkedOpinionRequestEvent() in self.portal_urban.buildlicence.urbaneventtypes.objectValues()) 
+        self.failUnless(term.getLinkedOpinionRequestEvent() in self.portal_urban.buildlicence.urbaneventtypes.objectValues())
+
+    def testCreatedLinkedUrbanEventTypeIsWellOrdered(self):
+        #when a linked UrbanEventType 'opinion request' is created, it should be positioned right after
+        #the last UrbanEventType 'opinion request' previously added
+
+        #create an OrganisationTerm and notify it to automatically create a linked UrbanEventType 'opinion request'
+        term_id = self.portal_urban.buildlicence.foldermakers.invokeFactory('OrganisationTerm', id='voodoo', title='Vood00', description='gni')
+        term = getattr(self.portal_urban.buildlicence.foldermakers, term_id)
+        event.notify(ObjectInitializedEvent(term))
+
+        urbaneventtypes = self.portal_urban.buildlicence.urbaneventtypes
+        created_urbaneventtype = term.getLinkedOpinionRequestEvent()
+        position = created_urbaneventtype.getObjectPosition(created_urbaneventtype.id)
+        self.failUnless(urbaneventtypes.objectValues()[position-1].getEventTypeType() == created_urbaneventtype.getEventTypeType())
+        if len(urbaneventtypes.objectValues()) > position+1:
+            self.failUnless(urbaneventtypes.objectValues()[position+1].getEventTypeType() != created_urbaneventtype.getEventTypeType()) 
 
     def testInquiryWithOpinionRequestIsLinkedToItsUrbanEventOpinionRequest(self):
         #if there is an inquiry with an opinion request and that its corresponding UrbanEventOpinionRequest 
         #is added, a link should be created between this inquiry and this UrbanEventOpinionRequest
+
         #set an investigation start date to activate the inquiry
         self.buildLicence.setInvestigationStart('18/09/86')
         self.buildLicence.setSolicitOpinionsTo('sncb')
