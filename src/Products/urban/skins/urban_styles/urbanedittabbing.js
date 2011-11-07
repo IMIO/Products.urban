2cf7857c6d4968d2f330b01c3247c1d0811349d3
Simon Delcourt <delcourt.simon@gmail.com>
2011-11-07 14:05:59 +0000

    #3020 moved code to override full content of the file 'form_tabbing.js' . This seems to fix the bug for ie8

--- src/Products/urban/skins/urban_styles/urbanedittabbing.js
+++ src/Products/urban/skins/urban_styles/urbanedittabbing.js
@@ -1,4 +1,30 @@
-/* Override the default ploneFormTabbing _buildTabs method to add icon.gif when needed */
+/*
+ * This is the code for the tabbed forms. It assumes the following markup:
+ *
+ * <form class="enableFormTabbing">
+ *   <fieldset id="fieldset-[unique-id]">
+ *     <legend id="fieldsetlegend-[same-id-as-above]">Title</legend>
+ *   </fieldset>
+ * </form>
+ *
+ * or the following
+ *
+ * <dl class="enableFormTabbing">
+ *   <dt id="fieldsetlegend-[unique-id]">Title</dt>
+ *   <dd id="fieldset-[same-id-as-above]">
+ *   </dd>
+ * </dl>
+ *
+ */
+
+
+var ploneFormTabbing = {
+        // standard jQueryTools configuration options for all form tabs
+        jqtConfig:{current:'selected'}
+    };
+
+
+(function($) {
 
 ploneFormTabbing._buildTabs = function(container, legends) {
     var threshold = legends.length > 6;
@@ -55,8 +81,8 @@ ploneFormTabbing._buildTabs = function(container, legends) {
     if (threshold) {
         tabs = $('<select class="formTabs">'+tabs+'</select>');
         tabs.change(function(){
-            var selected = $(this).attr('value');
-            jq('#'+selected).click();
+        	var selected = $(this).attr('value');
+        	jq('#'+selected).click();
         })
     } else {
         tabs = $('<ul class="formTabs">'+tabs+'</ul>');
@@ -64,3 +90,102 @@ ploneFormTabbing._buildTabs = function(container, legends) {
 
     return tabs;
 };
+
+
+ploneFormTabbing.initializeDL = function() {
+    var ftabs = $(ploneFormTabbing._buildTabs(this, $(this).children('dt')));
+    var targets = $(this).children('dd');
+    $(this).before(ftabs);
+    targets.addClass('formPanel');
+    ftabs.tabs(targets, ploneFormTabbing.jqtConfig);
+};
+
+
+ploneFormTabbing.initializeForm = function() {
+    var jqForm = $(this);
+    var fieldsets = jqForm.children('fieldset');
+
+    if (!fieldsets.length) {return;}
+
+    var ftabs = ploneFormTabbing._buildTabs(
+        this, fieldsets.children('legend'));
+    $(this).prepend(ftabs);
+    fieldsets.addClass("formPanel");
+
+
+    // The fieldset.current hidden may change, but is not content
+    $(this).find('input[name="fieldset.current"]').addClass('noUnloadProtection');
+
+    $(this).find('.formPanel:has(div.field span.required)').each(function() {
+        var id = this.id.replace(/^fieldset-/, "#fieldsetlegend-");
+        $(id).addClass('required');
+    });
+
+    // set the initial tab
+    var initialIndex = 0;
+    var count = 0;
+    var found = false;
+    $(this).find('.formPanel').each(function() {
+        if (!found && $(this).find('div.field.error').length!=0) {
+            initialIndex = count;
+            found = true;
+        }
+        count += 1;
+    });
+
+    var tabSelector = 'ul.formTabs';
+    if ($(ftabs).is('select.formTabs')) {
+        tabSelector = 'select.formTabs';
+    }
+    var tabsConfig = $.extend({}, ploneFormTabbing.jqtConfig, {'initialIndex':initialIndex});
+    jqForm.children(tabSelector).tabs(
+        'form.enableFormTabbing fieldset.formPanel',
+        tabsConfig
+        );
+
+    // save selected tab on submit
+    jqForm.submit(function() {
+    	var selected;
+    	if(ftabs.find('a.selected').length>=1){
+    		selected = ftabs.find('a.selected').attr('href').replace(/^#fieldsetlegend-/, "#fieldset-");
+    	}
+    	else{
+    		selected = ftabs.attr('value').replace(/^fieldsetlegend-/,'#fieldset-');
+    	}
+        var fsInput = jqForm.find('input[name="fieldset.current"]');
+        if (selected && fsInput) {
+            fsInput.val(selected);
+        }
+    });
+
+    $("#archetypes-schemata-links").addClass('hiddenStructure');
+    $("div.formControls input[name='form.button.previous']," +
+      "div.formControls input[name='form.button.next']").remove();
+
+};
+
+$.fn.ploneTabInit = function(pbo) {
+    return this.each(function() {
+        var item = $(this);
+
+        item.find("form.enableFormTabbing,div.enableFormTabbing").each(ploneFormTabbing.initializeForm);
+        item.find("dl.enableFormTabbing").each(ploneFormTabbing.initializeDL);
+
+        //Select tab if it's part of the URL or designated in a hidden input
+        var targetPane = item.find('.enableFormTabbing input[name="fieldset.current"]').val() || window.location.hash;
+        if (targetPane) {
+            item.find(".enableFormTabbing .formTab a[href='" +
+             targetPane.replace("'", "").replace(/^#fieldset-/, "#fieldsetlegend-") +
+             "']").click();
+        }
+    });
+};
+
+// initialize is a convenience function
+ploneFormTabbing.initialize = function() {
+    $('body').ploneTabInit();
+};
+
+})(jQuery);
+
+jQuery(function(){ploneFormTabbing.initialize();});
