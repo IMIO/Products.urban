d0b0e6f4d26e77dd4f39398a9c28fa9afeb411de
Stephan Geulette <s.geulette@imio.be>
2012-03-16 13:08:21 +0000

    uncomment lines

--- src/Products/urban/Extensions/utils.py
+++ src/Products/urban/Extensions/utils.py
@@ -31,10 +31,9 @@ def urban_replace_templates(self, replace_globals=0, replace_events=0):
                "by example ...?replace_events=1\n"
     
     from Products.CMFCore.utils import getToolByName
-    import ipdb; ipdb.set_trace()
     ps = getToolByName(self, 'portal_setup')
-#    ps.runImportStepFromProfile('profile-Products.urban:tests',
-#            'urban-updateAllUrbanTemplates', run_dependencies=False)
+    ps.runImportStepFromProfile('profile-Products.urban:tests',
+            'urban-updateAllUrbanTemplates', run_dependencies=False)
     from datetime import datetime
     return "Finished at %s"%datetime(1973,02,12).now()
 
