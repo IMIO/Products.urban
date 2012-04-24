5628facee1db8b659df95869ebba49237841a8c3
Simon Delcourt <delcourt.simon@gmail.com>
2012-04-24 08:38:54 +0000

    moved parceloutlicence view to a browser view

---
+++ src/Products/urban/browser/parceloutlicenceview.py
@@ -0,0 +1,23 @@
+from Acquisition import aq_inner
+from Products.urban.browser.buildlicenceview import BuildLicenceView
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone import PloneMessageFactory as _
+
+class ParcelOutLicenceView(BuildLicenceView):
+    """
+      This manage the view of ParcelOutLicence
+    """
+    def __init__(self, context, request):
+        super(BuildLicenceView, self).__init__(context, request)
+        self.context = context
+        self.request = request
+        plone_utils = getToolByName(context, 'plone_utils')
+        if not self.context.getParcels():
+            plone_utils.addPortalMessage(_('warning_add_a_parcel'), type="warning")
+        if not self.context.getApplicants():
+            plone_utils.addPortalMessage(_('warning_add_an_applicant'), type="warning")
+
+class ParcelOutLicenceMacros(BuildLicenceView):
+    """
+      This manage the macros of ParcelOutLicence
+    """
