14178a7c99bc636b2c2294777787c943b531a744
Stephan Geulette <s.geulette@imio.be>
2012-04-27 12:05:57 +0000

    updated pattern to match apache domain (#4053)

--- src/Products/urban/templates.py
+++ src/Products/urban/templates.py
@@ -21,7 +21,7 @@ def makeContext(pif):
     sqlurlre = re.compile(
         r'^ *sqlalchemy.url *= *postgresql://(\w+):(\w+)@([\w\.]+):\d+/(\w+)',
         re.I)
-    urbanmapre = re.compile(r'^ *urbanmap_url *= *http://([\d\.\:]+)', re.I)
+    urbanmapre = re.compile(r'^ *urbanmap_url *= *http://([^ ]+)', re.I)
 
     sites = {}
 
