f621a78ffc4dcea64970c1d0cb657ffc54904416
Simon Delcourt <delcourt.simon@gmail.com>
2013-02-11 07:52:18 +0000

    #5476, #5250 adapted call of method 'getCurrentFolderManager'

--- src/Products/urban/events/licenceEvents.py
+++ src/Products/urban/events/licenceEvents.py
@@ -4,4 +4,4 @@ from Products.CMFCore.utils import getToolByName
 def setDefaultFolderManagersEvent(object, event):
     if object.checkCreationFlag():
         tool = getToolByName(object, 'portal_urban')
-        object.setFoldermanagers(tool.getCurrentFolderManager(object,initials=False))
+        object.setFoldermanagers(tool.getCurrentFolderManager(initials=False))
