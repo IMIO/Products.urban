a3bff3b6a02e66885c98d929eed804bb2763f075
Simon Delcourt <delcourt.simon@gmail.com>
2013-03-07 14:59:43 +0000

    #5576 added migration step for SpecificFeatureTerm

--- src/Products/urban/migration/migrate_to_117.py
+++ src/Products/urban/migration/migrate_to_117.py
@@ -3,7 +3,16 @@ from Products.CMFCore.utils import getToolByName
 import logging
 import re
 
+from Products.contentmigration.walker import CustomQueryWalker
+from Products.contentmigration.archetypes import InplaceATFolderMigrator
+
 logger = logging.getLogger('urban: migrations')
+
+
+def contentmigrationLogger(oldObject, **kwargs):
+    """ Generic logger method to be used with CustomQueryWalker """
+    kwargs['logger'].info('/'.join(kwargs['purl'].getRelativeContentPath(oldObject)))
+    return True
 
 
 def migrateToUrban117(context):
@@ -17,6 +26,8 @@ def migrateToUrban117(context):
     migrateNumerotationExpressions(context)
     # the default text of inquiry 'claimsText' field is now defined on the urbaneventtype
     migrateInquiryDefaultText(context)
+    # specific feature vocabulary terms have their own type (SpecificFeatureTerm)
+    migrateSpecificFeatureTerms(context)
 
     # finish with reinstalling urban and adding the templates
     logger.info("starting to reinstall urban...")
@@ -32,7 +43,7 @@ def migrateNumerotationExpressions(context):
      adapt the way to call getCurrentFolderManager method in numerotation expressions
     """
     urban_tool = getToolByName(context, 'portal_urban')
-    logger = logging.getLogger('urban: migrate to environmental declarations ->')
+    logger = logging.getLogger('urban: migrate numerotation expressions->')
     logger.info("starting migration step")
 
     configs = urban_tool.objectValues('LicenceConfig')
@@ -56,7 +67,7 @@ def migrateInquiryDefaultText(context):
      set a default value to the field 'claimsText' of urban event 'enquête-publique'
     """
     urban_tool = getToolByName(context, 'portal_urban')
-    logger = logging.getLogger('urban: migrate to environmental declarations ->')
+    logger = logging.getLogger('urban: migrate default text of inquiry events ->')
     logger.info("starting migration step")
 
     default_text = [
@@ -82,5 +93,56 @@ def migrateInquiryDefaultText(context):
         if inquiry_event:
             logger.info("Migrated default text of %s inquiry" % configname)
             inquiry_event.setTextDefaultValues([{'text': '\n'.join(default_text), 'fieldname': 'claimsText'}])
+
+    logger.info("migration step done!")
+
+
+class UrbanVocabularyTermToSpecificFeatureTermMigrator(object, InplaceATFolderMigrator):
+    """
+     Migrate the UrbanEvent having an id of 'enquete-publique'
+     to UrbanEventInquiries
+    """
+    walker = CustomQueryWalker
+    src_meta_type = "UrbanVocabularyTerm"
+    src_portal_type = "UrbanVocabularyTerm"
+    dst_meta_type = "SpecificFeatureTerm"
+    dst_portal_type = "SpecificFeatureTerm"
+
+    def __init__(self, *args, **kwargs):
+        InplaceATFolderMigrator.__init__(self, *args, **kwargs)
+
+
+def migrateSpecificFeatureTerms(context):
+    """
+     The voc used for the specific features has now its own type : SpecificFeatureTerm
+     We have to migrate the UrbanVocabularyTerm used for the specific features to this new Type
+    """
+    urban_tool = getToolByName(context, 'portal_urban')
+    logger = logging.getLogger('urban: migrate SpecificFeatureTerm ->')
+    logger.info("starting migration step")
+
+    migrator = UrbanVocabularyTermToSpecificFeatureTermMigrator
+    portal_url = getToolByName(context, 'portal_url')
+    portal = portal_url.getPortalObject()
+    #to avoid link integrity problems, disable checks
+    portal.portal_properties.site_properties.enable_link_integrity_checks = False
+
+    #Run the migrations
+    foldernames = ['specificfeatures', 'roadspecificfeatures', 'locationspecificfeatures', 'townshipspecificfeatures']
+    licences = ['urbancertificateone', 'urbancertificatetwo', 'notaryletter']
+    for licence_name in licences:
+        licence_config = getattr(urban_tool, licence_name)
+        for foldername in foldernames:
+            folder = getattr(licence_config, foldername)
+            # we have to change the allowed content types on this folder
+            folder.setLocallyAllowedTypes('SpecificFeatureTerm')
+            folder.setImmediatelyAddableTypes('SpecificFeatureTerm')
+            folder_path = '/'.join(folder.getPhysicalPath())
+            walker = migrator.walker(portal, migrator, query={'path': folder_path}, callBefore=contentmigrationLogger, logger=logger, purl=portal.portal_url)
+            walker.go()
+        # we need to reset the class variable to avoid using current query in next use of CustomQueryWalker
+        walker.__class__.additionalQuery = {}
+    #enable linkintegrity checks
+    portal.portal_properties.site_properties.enable_link_integrity_checks = True
 
     logger.info("migration step done!")
