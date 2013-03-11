437a4802c472a7c36df4ea76b48af1b86b5e62d4
Simon Delcourt <delcourt.simon@gmail.com>
2013-03-11 08:50:09 +0000

    moves test profiles registration to testing zcml

--- src/Products/urban/testing.py
+++ src/Products/urban/testing.py
@@ -19,17 +19,23 @@ URBAN_TESTS_PROFILE_DEFAULT = PloneWithPackageLayer(
     name="URBAN_TESTS_PROFILE_DEFAULT")
 
 
-class UrbanTestLayer(IntegrationTesting):
-
-    def setUp(self):
-        with helpers.ploneSite() as portal:
-            helpers.applyProfile(portal, 'Products.urban:testsWithLicences')
-
 URBAN_TESTS_PROFILE_INTEGRATION = IntegrationTesting(
     bases=(URBAN_TESTS_PROFILE_DEFAULT,), name="URBAN_TESTS_PROFILE_INTEGRATION")
 
 URBAN_TESTS_PROFILE_FUNCTIONAL = FunctionalTesting(
     bases=(URBAN_TESTS_PROFILE_DEFAULT,), name="URBAN_TESTS_PROFILE_FUNCTIONAL")
+
+
+class UrbanTestLayer(IntegrationTesting):
+    """
+    Instanciate licences
+
+    Must collaborate with a layer that installs Plone and Urban
+    Useful for performances: Plone site is instanciated only once
+    """
+    def setUp(self):
+        with helpers.ploneSite() as portal:
+            helpers.applyProfile(portal, 'Products.urban:testsWithLicences')
 
 URBAN_TESTS_LICENCES = UrbanTestLayer(
     bases=(URBAN_TESTS_PROFILE_DEFAULT, ), name="URBAN_TESTS_LICENCES")
