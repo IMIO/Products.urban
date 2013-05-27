464d4fa1f339404980d6183c3bf85d665b7dd9c7
Simon Delcourt <delcourt.simon@gmail.com>
2013-05-27 08:33:23 +0000

    remove specific city profiles out of the trunk

--- src/Products/urban/profiles/labruyere/exportimport.py
+++
@@ -1,8 +0,0 @@
-from Products.urban.exportimport import addUrbanEventTypes
-from Products.urban.exportimport import addGlobalTemplates
-
-def updateAllLabruyereTemplates(context):
-    if context.readDataFile('urban_labruyere_marker.txt') is None:
-        return
-    addGlobalTemplates(context)
-    addUrbanEventTypes(context)
