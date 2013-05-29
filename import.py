5a446d04f830ee11fcccc2607b9f6feaf3c00e77
Simon Delcourt <delcourt.simon@gmail.com>
2013-05-29 09:40:37 +0000

    adapted aimh migration call to be able to pass custom values mapping
    Former-commit-id: 82d41d5de50b25747bc6d886e26e45e177a2aafd

--- import.py
+++ import.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from Products.urban.profiles.dataMigration.AIHM_migration.AIHM_import import importAIHM
+from Products.urban.profiles.dataMigration.AIHM_migration.AIHM_misc import table
 
 
 def importLalouviereAIHM(context):
@@ -7,4 +8,24 @@ def importLalouviereAIHM(context):
     if context.readDataFile('lalouviere_aihm_marker.txt') is None:
         return
 
-    importAIHM(context)
+    # !!!!! dirty override, but im just too fuckin lazy.. !!!!!
+    custom_mapping = {
+        'TYPE_map' : table({
+        'header': ['portal_type',         'foldercategory', 'abreviation'],
+        '0'     : ['BuildLicence',        'uap',            'PU'],
+        '1'     : ['BuildLicence',        'upp',            'PU'],
+        '2'     : ['UrbanCertificateOne', 'cu1',            'CU1'],
+        '3'     : ['UrbanCertificateTwo', 'cu2',            'CU2'],
+        '4'     : ['ParcelOutLicence',    '',               'PL'],
+        '5'     : ['NotaryLetter',        '',               'Not'],
+        '7'     : ['MiscDemand',          'apct',           'AP'],
+        '8'     : [None,                  '',               'INF'],
+        '9'     : ['BuildLicence',        'uap',            'FR'],
+        '10'    : ['Declaration',         'dup',            'Decl'],
+        '11'    : ['MiscDemand',          'rendez-vous',    'RV'],
+        })
+    }
+
+    # !!!!! dirty override end !!!!!
+
+    importAIHM(context, aihm_filename='export.csv', custom_mapping=custom_mapping)
