1084080f5191177c6b4e5db3377819ba7c98c7cb
Franck Ngaha <franck.o.ngaha@gmail.com>
2013-10-23 10:13:08 +0000

    Add link to licence folder into urban default view

--- src/Products/urban/tests/test_EnvClassOne.py
+++ src/Products/urban/tests/test_EnvClassOne.py
@@ -10,7 +10,7 @@ class TestEnvClassOne(unittest.TestCase):
 
     def setUp(self):
         self.portal = self.layer['portal']
-	self.urban = self.portal.urban
+        self.urban = self.portal.urban
         self.portal_urban = self.portal.portal_urban
         self.browser = Browser(self.portal)
         self.browserLogin('urbaneditor')
@@ -31,16 +31,24 @@ class TestEnvClassOne(unittest.TestCase):
     def test_envclassone_config_folder_is_visible(self):
         msg = 'envclassone config folder is not visible in urban config'
         self.browser.open(self.portal_urban.absolute_url())
-        self.assertTrue(self.browser.contents.find('Permis d\'environnement classe 1') != -1, msg)
-    
+        contents = self.browser.contents
+        self.assertTrue(
+            contents.find('Permis d\'environnement classe 1') != -1,
+            msg,
+        )
+
     def test_envclassone_folder_exist(self):
         msg = 'envclassones folder not created'
         self.assertTrue('envclassones' in self.urban.objectIds(), msg)
 
     def test_envclassone_addable_types(self):
-	msg = 'cannot create EnvClassOne in licence folder'
-	addable_types = self.urban.envclassones.immediatelyAddableTypes
-	self.assertTrue('EnvClassOne' in  addable_types, msg)
-	msg = 'can create an other content type in licence folder'
-	self.assertEqual(len(addable_types), 1, msg)
+        msg = 'cannot create EnvClassOne in licence folder'
+        addable_types = self.urban.envclassones.immediatelyAddableTypes
+        self.assertTrue('EnvClassOne' in addable_types, msg)
+        msg = 'can create an other content type in licence folder'
+        self.assertEqual(len(addable_types), 1, msg)
 
+    def test_envclassone_licence_folder_link_in_urban_default_view(self):
+        self.browser.open(self.urban.absolute_url())
+        link = self.browser.getLink(url="envclassones")
+        self.assertEqual(link.text, "Permis d'environnement classe 1")
