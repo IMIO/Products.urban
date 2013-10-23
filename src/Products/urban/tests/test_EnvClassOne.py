009d8acbfa355398072a8d359cb88ca09d6a7a61
Simon Delcourt <delcourt.simon@gmail.com>
2013-10-23 06:57:36 +0000

    pep8, pyflakes, do not mix camelcase and lowercase/underscores

--- src/Products/urban/tests/test_EnvClassOne.py
+++ src/Products/urban/tests/test_EnvClassOne.py
@@ -1,19 +1,18 @@
 #-*- coding: utf-8 -*-
 import unittest
-from plone.app.testing import login
 from Products.urban.testing import URBAN_TESTS_PROFILE_INTEGRATION
 from plone.testing.z2 import Browser
 
 
 class TestConfig(unittest.TestCase):
 
-    layer = URBAN_TESTS_PROFILE_INTEGRATION 
+    layer = URBAN_TESTS_PROFILE_INTEGRATION
 
     def setUp(self):
         self.portal = self.layer['portal']
         self.portal_urban = self.portal.portal_urban
         self.browser = Browser(self.portal)
-        self.browserLogin('urbaneditor') 
+        self.browserLogin('urbaneditor')
 
     def browserLogin(self, user):
         self.browser.open(self.portal.absolute_url() + "/login_form")
@@ -21,16 +20,14 @@ class TestConfig(unittest.TestCase):
         self.browser.getControl(name='__ac_password').value = user
         self.browser.getControl(name='submit').click()
 
-
-    def test_EnvCLassOne_config_folder_exists(self):
+    def test_envclassone_config_folder_exists(self):
         msg = 'envclassone config folder not created'
         self.assertTrue('envclassone' in self.portal_urban.objectIds(), msg)
         envclassone = self.portal_urban.envclassone
         from Products.urban.LicenceConfig import LicenceConfig
         self.assertTrue(isinstance(envclassone, LicenceConfig))
 
-    def test_EnvCLassOne_traduction(self):
-        msg = 'envclassone not translated'
+    def test_envclassone_config_folder_is_visible(self):
+        msg = 'envclassone config folder is not visible in urban config'
         self.browser.open(self.portal_urban.absolute_url())
         self.assertTrue(self.browser.contents.find('Permis d\'environnement classe 1') != -1, msg)
-    
