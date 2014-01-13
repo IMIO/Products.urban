016d2f28ce6b2e4570bb7d5035feafc2062b2f6c
Simon Delcourt <delcourt.simon@gmail.com>
2014-01-13 09:19:45 +0000

    fix edit redirection bug when creating urbanevent

--- src/Products/urban/skins/urban_templates/create_urban_event.py
+++ src/Products/urban/skins/urban_templates/create_urban_event.py
@@ -1,4 +1,4 @@
-## Script (Python) "createUrbanEvent"
+## Script (Python) "create_urban_event"
 ##title=Create an UrbanEvent
 ##bind container=container
 ##bind context=context
@@ -7,4 +7,4 @@
 ##bind subpath=traverse_subpath
 ##parameters=urban_event_type_uid=''
 ##
-return  self.REQUEST.RESPONSE.redirect(context.createUrbanEvent(urban_event_type_uid).absolute_url() + '/edit')
+return  context.REQUEST.RESPONSE.redirect(context.createUrbanEvent(urban_event_type_uid).absolute_url() + '/edit')
