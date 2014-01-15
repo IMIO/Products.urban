dd9da0a6e3f1155e2c741448a8b15414ee44cf80
Simon Delcourt <delcourt.simon@gmail.com>
2014-01-15 13:18:24 +0000

    search licences in progress

--- src/Products/urban/browser/scheduleview.py
+++ src/Products/urban/browser/scheduleview.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
 from Products.Five import BrowserView
-from Acquisition import aq_inner
+from Products.urban.interfaces import IGenericLicence
 #from Products.urban.browser.urbantable import ScheduleTable
+
+from plone import api
 
 
 class ScheduleView(BrowserView):
@@ -13,3 +15,12 @@ class ScheduleView(BrowserView):
         super(ScheduleView, self).__init__(context, request)
         self.context = context
         self.request = request
+
+    def getLicencesInProgress(self):
+        catalog = api.portal.get_tool('portal_catalog')
+        licences_in_progress = catalog(
+            object_provides=IGenericLicence.__identifier__,
+            review_state='in_progress',
+        )
+
+        return licences_in_progress
