b275c353441b82ece6d659e90a86d52f64e3ea0c
Simon Delcourt <delcourt.simon@gmail.com>
2014-01-28 12:44:29 +0000

    hide EnvClassOne licences from root view because its not finished yet

--- src/Products/urban/browser/urbanview.py
+++ src/Products/urban/browser/urbanview.py
@@ -90,7 +90,8 @@ class UrbanRootView(UrbanView):
         self.request = request
 
     def getLicenceTypes(self):
-        return ORDERED_URBAN_TYPES
+        # exclude EnvironmentLicence Class 1 because its under dev.
+        return [l_type for l_type in ORDERED_URBAN_TYPES if l_type != 'EnvClassOne']
 
     def getLicenceCreationURL(self, licencetype):
         context = aq_inner(self.context)
