02f072ab4d89a7063531fa0f1b514b2a807800de
Simon Delcourt <delcourt.simon@gmail.com>
2014-01-30 12:58:39 +0000

    restore stats view tests

--- src/Products/urban/tests/test_StatsLicences.py
+++ src/Products/urban/tests/test_StatsLicences.py
@@ -1,16 +1,19 @@
 #-*- coding: utf-8 -*-
 import unittest
 from plone.app.testing import login
-from Products.urban.testing import URBAN_TESTS_PROFILE_FUNCTIONAL
+from Products.urban.interfaces import IGenericLicence
+from Products.urban.testing import URBAN_TESTS_LICENCES
 
 from plone.testing.z2 import Browser
 from Products.CMFCore.utils import getToolByName
 from testfixtures import compare, StringComparison as S
 
+from plone import api
+
 
 class TestLicenceStatsView(unittest.TestCase):
 
-    layer = URBAN_TESTS_PROFILE_FUNCTIONAL
+    layer = URBAN_TESTS_LICENCES
 
     def setUp(self):
         self.portal = self.layer['portal']
@@ -42,10 +45,12 @@ class TestLicenceStatsView(unittest.TestCase):
         compare(S("(?s).*0 dossiers.*"), self.browser.contents)
 
     def testStatsViewsResult(self):
+        catalog = api.portal.get_tool('portal_catalog')
+        licences_number = len(catalog(object_provides=IGenericLicence.__identifier__))
         #check the normal case display result
         self.browser.open(self.urban.absolute_url() + '/urbanstatsview')
         self.browser.getControl(name="licence_states").getControl(value='in_progress').click()
         self.browser.getControl("Statistics").click()
         new_url = '%s/urbanstatsview%s' % (self.urban.absolute_url(), self.browser.url.split('/urban')[1])
         self.browser.open(new_url)
-        compare(S("(?s).*8 dossiers.*"), self.browser.contents)
+        compare(S("(?s).*{} dossiers.*".format(licences_number)), self.browser.contents)
