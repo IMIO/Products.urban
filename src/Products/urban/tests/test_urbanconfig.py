707e6d788f685b7ac096bda2f6dfebfa6bdee106
Simon Delcourt <delcourt.simon@gmail.com>
2014-01-31 15:31:30 +0000

    test urban config view display

--- src/Products/urban/tests/test_urbanconfig.py
+++ src/Products/urban/tests/test_urbanconfig.py
@@ -8,15 +8,14 @@ from plone.testing.z2 import Browser
 import unittest
 
 
-class TestSearchView(unittest.TestCase):
+class TestUrbanConfig(unittest.TestCase):
 
     layer = URBAN_TESTS_CONFIG
 
     def setUp(self):
         portal = self.layer['portal']
         self.portal = portal
-        self.urban = portal.urban
-        self.searchview = portal.restrictedTraverse('urbansearchview')
+        self.config = portal.portal_urban
 
         login(portal, 'urbanmanager')
         self.browser = Browser(self.portal)
@@ -29,9 +28,8 @@ class TestSearchView(unittest.TestCase):
         self.browser.getControl(name='__ac_password').value = user
         self.browser.getControl(name='submit').click()
 
-    def test_search_view_display(self):
+    def test_urbanconfig_view_display(self):
         """
          Tests search  view is not broken for whatsoever reason
         """
-        search_url = '{base_url}/schedule'.format(base_url=self.urban.absolute_url())
-        self.browser.open(search_url)
+        self.browser.open(self.config.absolute_url())
