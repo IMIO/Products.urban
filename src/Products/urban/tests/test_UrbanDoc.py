bc9f58079c24cb005f6cb6bc7ea875aa2d89eae9
Simon Delcourt <delcourt.simon@gmail.com>
2014-02-04 08:20:22 +0000

    test TAL condition of document template to allow document generation

--- src/Products/urban/tests/test_UrbanDoc.py
+++ src/Products/urban/tests/test_UrbanDoc.py
@@ -1,21 +1,38 @@
 #-*- coding: utf-8 -*-
 import unittest
 from plone.app.testing import login
-from Products.urban.testing import URBAN_TESTS_LICENCES
+from Products.urban.testing import URBAN_TESTS_CONFIG
+
+from plone import api
 from plone.testing.z2 import Browser
-from Products.CMFCore.utils import getToolByName
+
+import transaction
 
 
 class TestUrbanDoc(unittest.TestCase):
 
-    layer = URBAN_TESTS_LICENCES
+    layer = URBAN_TESTS_CONFIG
 
     def setUp(self):
         self.portal = self.layer['portal']
         self.urban = self.portal.urban
         self.portal_urban = self.portal.portal_urban
-        self.catalog = getToolByName(self.portal, 'portal_catalog')
+
+        # create a test BuildLicence
         login(self.portal, 'urbaneditor')
+        buildlicence_folder = self.urban.buildlicences
+        testlicence_id = 'test_buildlicence'
+        if testlicence_id not in buildlicence_folder.objectIds():
+            buildlicence_folder.invokeFactory('BuildLicence', id=testlicence_id)
+        self.licence = getattr(buildlicence_folder, testlicence_id)
+        # create a test UrbanEvent in test_buildlicence
+        self.catalog = api.portal.get_tool('portal_catalog')
+        event_type_brain = self.catalog(portal_type='UrbanEventType', id='accuse-de-reception')[0]
+        self.event_type = event_type_brain.getObject()
+        self.urban_event = self.licence.createUrbanEvent(self.event_type.UID())
+        self.urbandoc_model = getattr(self.event_type, 'urb-accuse.odt')
+        transaction.commit()
+
         self.browser = Browser(self.portal)
         self.browserLogin('urbaneditor')
 
@@ -25,17 +42,50 @@ class TestUrbanDoc(unittest.TestCase):
         self.browser.getControl(name='__ac_password').value = user
         self.browser.getControl(name='submit').click()
 
-    def testTALConditionVisibleInConfig(self):
-        #check that the TAL condition of urbanDoc is displayed in the urban config
-        self.browser.open(self.portal_urban.absolute_url() + '/buildlicence/urbaneventtypes/accuse-de-reception/urb-accuse.odt/view')
-        self.failUnless('TALCondition' in self.browser.contents)
+    def test_TALCondition_visible_on_document_templates(self):
+        self.browser.open(self.urbandoc_model.absolute_url() + '/view')
+        self.assertTrue('TALCondition' in self.browser.contents)
 
-    def testTALConditionHidden(self):
-        #check that the TAL condition of urbanDoc is hidden for generated documents
-        # retrieve the event 'accusé de réception'
-        licence = self.portal.urban.buildlicences.objectValues()[-1]
-        events = licence.objectValues('UrbanEvent')
-        event = [event for event in events if event.Title().startswith('Accus')][0]
-        doc = event.objectValues()[0]
-        self.browser.open(doc.absolute_url() + '/view')
-        self.failUnless('TALCondition' not in self.browser.contents)
+    def test_TALCondition_hidden_on_generated_documents(self):
+        event = self.urban_event
+        generated_doc = event.objectValues('UrbanDoc')[0]
+        self.browser.open(generated_doc.absolute_url() + '/view')
+        self.assertTrue('TALCondition' not in self.browser.contents)
+
+    def test_mayGenerateUrbanDoc_with_false_TALcondition(self):
+        self.urbandoc_model.setTALCondition('python: False')
+        may_generate_document = self.urbandoc_model.mayGenerateUrbanDoc(self.licence)
+        self.assertTrue(not may_generate_document)
+
+    def test_mayGenerateUrbanDoc_with_true(self):
+        self.urbandoc_model.setTALCondition('python: True')
+        may_generate_document = self.urbandoc_model.mayGenerateUrbanDoc(self.licence)
+        self.assertTrue(may_generate_document)
+
+    def test_link_to_generate_document_is_hidden_when_TALcondition_is_false(self):
+        self.urbandoc_model.setTALCondition('python: False')
+
+        # delete generated document (if any) to be sure to not confuse links to
+        # generate documents from the link of generated documents
+        document = self.urban_event.objectValues()[0]
+        api.content.delete(document)
+        transaction.commit()
+
+        self.browser.open(self.urban_event.absolute_url())
+        link = '{title}</a>'.format(title=self.urbandoc_model.Title())
+
+        self.assertTrue(link not in self.browser.contents)
+
+    def test_link_to_generate_document_is_visible_when_TALcondition_is_true(self):
+        self.urbandoc_model.setTALCondition('python: True')
+
+        # delete generated document (if any) to be sure to not confuse links to
+        # generate documents from the link of generated documents
+        document = self.urban_event.objectValues()[0]
+        api.content.delete(document)
+        transaction.commit()
+
+        self.browser.open(self.urban_event.absolute_url())
+        link = '{title}</a>'.format(title=self.urbandoc_model.Title())
+
+        self.assertTrue(link in self.browser.contents)
