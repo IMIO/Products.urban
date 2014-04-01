c274bbd037bcc302e409e09ba979b8fe827feeb7
Simon Delcourt <delcourt.simon@gmail.com>
2014-04-01 12:50:38 +0000

    hide EnvClassOne as long its in dev..

--- src/Products/urban/browser/urbanview.py
+++ src/Products/urban/browser/urbanview.py
@@ -99,7 +99,7 @@ class UrbanRootView(UrbanView):
         self.request = request
 
     def getLicenceTypes(self):
-        return ORDERED_URBAN_TYPES
+        return [urban_type for urban_type in ORDERED_URBAN_TYPES if urban_type != 'EnvClassOne']
 
     def getLicenceCreationURL(self, licencetype):
         context = aq_inner(self.context)
