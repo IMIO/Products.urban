c90561a71e9e9ea1a9fb4a0e25e2a86e0831645a
Simon Delcourt <delcourt.simon@gmail.com>
2015-02-27 13:37:40 +0000

    reactive robot tests

--- src/Products/urban/tests/test_robot.py
+++ src/Products/urban/tests/test_robot.py
@@ -4,7 +4,6 @@ import robotsuite
 from Products.urban.testing import URBAN_TEST_ROBOT
 from plone.testing import layered
 
-"""
 def test_suite():
     suite = unittest.TestSuite()
     suite.addTests([
@@ -14,4 +13,3 @@ def test_suite():
         ),
     ])
     return suite
-"""
