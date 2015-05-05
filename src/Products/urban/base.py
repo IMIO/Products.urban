b7898d3c62f2c16013bc8330666c64a24784e5be
Franck Ngaha <franck.o.ngaha@gmail.com>
2015-05-05 09:37:40 +0000

    #10944 backport

--- src/Products/urban/base.py
+++ src/Products/urban/base.py
@@ -242,6 +242,88 @@ class UrbanBase(object):
                 return True
         return False
 
+    def hasSingleMaleApplicant(self):
+        """
+            return true if the licence has a single male applicant
+        """
+        answer = False
+        applicants = self.getApplicants()  # applicant can also be proprietaries..
+        if len(applicants) <= 1:
+            applicant = applicants[0]
+            field = applicant.getField('personTitle')
+            titles = field.vocabulary.getAllVocTerms(applicant)
+            title = titles[applicant.getPersonTitle()]
+            if title.getMultiplicity() == 'single':
+                if title.getGender() == 'male':
+                    answer = True
+        return answer
+
+    def hasSingleFemaleApplicant(self):
+        """
+            return true if the licence has a single female applicant
+        """
+        answer = False
+        applicants = self.getApplicants()  # applicant can also be proprietaries..
+        if len(applicants) <= 1:
+            applicant = applicants[0]
+            field = applicant.getField('personTitle')
+            titles = field.vocabulary.getAllVocTerms(applicant)
+            title = titles[applicant.getPersonTitle()]
+            if title.getMultiplicity() == 'single':
+                if title.getGender() == 'female':
+                    answer = True
+        return answer
+
+    def hasMultipleFemaleApplicants(self):
+        """
+            return true if the licence has a multiple female applicants
+        """
+        answer = False
+        applicants = self.getApplicants()  # applicant can also be proprietaries..
+        if len(applicants) > 1:
+            answer = True
+            for applicant in applicants:
+                field = applicant.getField('personTitle')
+                titles = field.vocabulary.getAllVocTerms(applicant)
+                title = titles[applicant.getPersonTitle()]
+                if title.getGender() == 'male':
+                    answer = False
+        return answer
+
+    def hasMultipleMaleApplicants(self):
+        """
+            return true if the licence has a multiple male applicants
+        """
+        answer = False
+        applicants = self.getApplicants()  # applicant can also be proprietaries..
+        if len(applicants) > 1:
+            answer = True
+            for applicant in applicants:
+                field = applicant.getField('personTitle')
+                titles = field.vocabulary.getAllVocTerms(applicant)
+                title = titles[applicant.getPersonTitle()]
+                if title.getGender() == 'female':
+                    answer = False
+        return answer
+
+    def hasMixedApplicants(self):
+        """
+            return true if the licence has a mixed applicants
+        """
+        answer = False
+        applicants = self.getApplicants()  # applicant can also be proprietaries..
+        applicant0 = applicants[0]
+        field = applicant0.getField('personTitle')
+        titles = field.vocabulary.getAllVocTerms(applicant0)
+        title0 = titles[applicant0.getPersonTitle()]
+        gender0 = title0.getGender()
+        if len(applicants) > 1:
+            for applicant in applicants[1:]:
+                title = titles[applicant.getPersonTitle()]
+                if title.getGender() != gender0:
+                    answer = True
+        return answer
+
     security.declarePublic('hasMultipleApplicants')
     def hasMultipleApplicants(self):
         """
