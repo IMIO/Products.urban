1a183300305e4d972e25007a8af8084f1fbeaa8c
Simon Delcourt <delcourt.simon@gmail.com>
2015-06-17 07:22:18 +0000

    backport r19948

--- src/Products/urban/skins/urban_plone/displayValue.py
+++ src/Products/urban/skins/urban_plone/displayValue.py
@@ -1,11 +1,12 @@
-## Script (Python) "Display Value"
-##title=Use DisplayList getValue method to translate internal value to a label
+## Script (Python) "displayValue"
 ##bind container=container
 ##bind context=context
 ##bind namespace=
 ##bind script=script
 ##bind subpath=traverse_subpath
-##parameters=vocab, value
+##parameters=vocab, value, widget=None
+##title=Use DisplayList getValue method to translate internal value to a label
+##
 
 #XXX changes from urban
 #this script comes from Products.Archetypes
@@ -13,19 +14,5 @@
 if not value:
     return "<span class='discreet'>%s</span>" % context.utranslate(msgid='content_none', domain='urban')
 #XXX end of changes
-
-try:
-    # Maybe a string?
-    value.capitalize()
-except AttributeError:
-    try:
-        # Maybe a list?
-        return ', '.join([vocab.getValue(context.unicodeEncode(v),
-                                         context.unicodeEncode(v)) \
-                          for v in value if v])
-    except TypeError:
-        pass
-
-# Try to convert to a string and do the dirty job.
-return vocab.getValue(context.unicodeEncode(value),
-                      context.unicodeEncode(value))
+t = context.restrictedTraverse('@@at_utils').translate
+return t(vocab, value, widget)
