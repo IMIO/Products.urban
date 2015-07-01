818b890ff8e496990e972c6b6c89e58f1bfc1b4e
Simon Delcourt <delcourt.simon@gmail.com>
2015-07-01 09:15:47 +0000

    backport getMultipleClaimantsCSV templating function

--- src/Products/urban/base.py
+++ src/Products/urban/base.py
@@ -334,6 +334,20 @@ class UrbanBase(object):
         return toreturn
     getMultipleApplicants = getMultipleApplicantsCSV
 
+    security.declarePublic('getMultipleClaimantsCSV')
+    def getMultipleClaimantsCSV(self):
+       """
+       Returns a formatted version of claimants to be used in POD templates
+       """
+       claimants = self._getLastEvent(interfaces.IUrbanEventInquiry).getClaimants()
+       toreturn = '<CSV>Titre|Nom|Prenom|AdresseLigne1|AdresseLigne2'
+       for claimant in claimants:
+           toreturn = toreturn + '%' + claimant.getPersonTitleValue() + '|' + claimant.getName1() + '|' +\
+           claimant.getName2() + '|' + claimant.getNumber() + ', ' + claimant.getStreet() + '|' + \
+           claimant.getZipcode() + ' ' + claimant.getCity()
+       toreturn = toreturn + '</CSV>'
+       return toreturn
+
     security.declarePublic('getMultipleArchitectsCSV')
     def getMultipleArchitectsCSV(self):
         """
