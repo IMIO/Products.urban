20db1eeb4b79d4c53a892572a9b602c037307aa2
Simon Delcourt <delcourt.simon@gmail.com>
2015-09-10 12:28:57 +0000

    add autocomplete for licence reference filter

--- src/Products/urban/dashboard/autocomplete.py
+++ src/Products/urban/dashboard/autocomplete.py
@@ -3,13 +3,19 @@
 from Products.Five import BrowserView
 from Products.ZCTextIndex.ParseTree import ParseError
 
+from eea.faceted.vocabularies.autocomplete import IAutocompleteSuggest
+
 from plone import api
+
+from zope.interface import implements
 
 import json
 
 
 class UrbanStreetsSuggest(BrowserView):
     """ Autocomplete suggestions on urban streets."""
+
+    implements(IAutocompleteSuggest)
 
     label = 'Rues urban'
 
@@ -32,10 +38,45 @@ class UrbanStreetsSuggest(BrowserView):
         }
 
         catalog = api.portal.get_tool('portal_catalog')
-        results = catalog(**kwargs)
+        brains = catalog(**kwargs)
         try:
             return json.dumps(
-                [{'label': x.Title, 'value': x.UID} for x in results]
+                [{'label': b.Title, 'value': b.UID} for b in brains]
+            )
+        except ParseError:
+            pass
+
+
+class LicenceReferenceSuggest(BrowserView):
+    """ Autocomplete suggestions of licence references."""
+
+    implements(IAutocompleteSuggest)
+
+    label = 'Référence des dossiers'
+
+    def __call__(self):
+        term = self.request.get('term')
+        if not term:
+            return
+
+        terms = term.strip().split()
+        portal = api.portal.get()
+        path = '/'.join(portal.urban.getPhysicalPath())
+
+        kwargs = {
+            'Title': ' AND '.join(["%s*" % t for t in terms]),
+            'sort_on': 'sortable_title',
+            'sort_order': 'reverse',
+            'path': path,
+            'object_provides': 'Products.urban.interfaces.IGenericLicence',
+        }
+
+        catalog = api.portal.get_tool('portal_catalog')
+        brains = catalog(**kwargs)
+
+        try:
+            return json.dumps(
+                [{'label': b.getReference, 'value': b.getReference} for b in brains]
             )
         except ParseError:
             pass
@@ -43,6 +84,8 @@ class UrbanStreetsSuggest(BrowserView):
 
 class CadastralReferenceSuggest(BrowserView):
     """ Autocomplete suggestions on cadastral references."""
+
+    implements(IAutocompleteSuggest)
 
     label = 'Parcelles urban'
 
