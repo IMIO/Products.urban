f40db27066ad9a5bd3af7dfc29487799d6ceb8d8
Simon Delcourt <delcourt.simon@gmail.com>
2015-11-17 13:33:23 +0000

    get docs as downloadable zip

--- src/Products/urban/Extensions/export.py
+++ src/Products/urban/Extensions/export.py
@@ -7,21 +7,41 @@ from Products.urban.config import URBAN_TYPES
 from plone import api
 
 import os
+import shutil
+import zipfile
 
 VAR_DIR = 'var'
 EXPORT_DIR = '{}/export'.format(VAR_DIR)
 
 
-def export_document_templates(licence_types=URBAN_TYPES):
+def get_document_templates(licence_types=URBAN_TYPES):
 
     if 'export' not in os.listdir(VAR_DIR):
         os.mkdir(EXPORT_DIR)
 
-    export_name = '{path}/urban_templates-{timestamp}'.format(
+    export_path = _export_document_templates(licence_types=licence_types)
+    zip_name, zip_file = _zip_folder(export_path)
+    _set_header_response(zip_name)
+    return zip_file
+
+
+def _set_header_response(filename):
+    site = api.portal.get()
+    response = site.REQUEST.RESPONSE
+    response.setHeader('Content-type', 'application/zip')
+    response.setHeader(
+        'Content-disposition',
+        'attachment;filename="{}"'.format(filename)
+    )
+
+
+def _export_document_templates(licence_types=URBAN_TYPES):
+
+    export_path = '{path}/urban_templates-{timestamp}'.format(
         path=EXPORT_DIR,
         timestamp=DateTime().millis()
     )
-    os.mkdir(export_name)
+    os.mkdir(export_path)
 
     portal_urban = api.portal.get_tool('portal_urban')
 
@@ -30,7 +50,7 @@ def export_document_templates(licence_types=URBAN_TYPES):
         config = portal_urban.get(licence_type.lower())
 
         docs_path = '{path}/{licence_type}'.format(
-            path=export_name,
+            path=export_path,
             licence_type=licence_type.lower()
         )
         os.mkdir(docs_path)
@@ -46,3 +66,23 @@ def export_document_templates(licence_types=URBAN_TYPES):
                 doc_export = open(doc_name, 'arw')
                 doc_export.write(doc.get_file().data)
                 doc_export.close()
+
+    return export_path
+
+
+def _zip_folder(path):
+    zip_name = 'Modèles_urban.zip'
+    zip_file = zipfile.ZipFile(zip_name, 'w')
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            zip_file.write(os.path.join(root, file))
+    zip_file.close()
+
+    zip_file = open(zip_name, 'r')
+    payload = zip_file.read()
+    zip_file.close()
+
+    shutil.rmtree(path)
+    os.remove(zip_name)
+
+    return zip_name, payload
