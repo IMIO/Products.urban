736d533f92df47b2beaf922cde9d909575366464
Simon Delcourt <delcourt.simon@gmail.com>
2016-05-27 08:12:22 +0000

    add env due date fix method

---
+++ src/Products/urban/Extensions/fix_environment_due_date.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from plone import api
+
+from Products.urban.events.environmentLicenceEvents import createLicenceExpirationEvent
+from Products.urban.interfaces import IAcknowledgmentEvent
+from Products.urban.interfaces import ITheLicence
+
+
+def recompute_due_dates():
+    """
+    Send "ObjectModifiedEvent' on all the acknowledgement or Licence events
+    to recompute due date event.
+    """
+
+    marker_interfaces = [ITheLicence.__identifier__, IAcknowledgmentEvent.__identifier__]
+    catalog = api.portal.get_tool('portal_catalog')
+
+    event_brains = catalog(object_provides=marker_interfaces)
+
+    for event_brain in event_brains:
+        event = event_brain.getObject()
+        createLicenceExpirationEvent(event, None)
--- src/Products/urban/events/opinionRequestEvents.py
+++ src/Products/urban/events/opinionRequestEvents.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 def setDefaultLinkedInquiry(opinionRequest, event):
     if opinionRequest.checkCreationFlag():
         licence = opinionRequest.aq_inner.aq_parent
