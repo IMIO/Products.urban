a0609f52292259f3cae5a869c6eea20ee8ae5759
Simon Delcourt <delcourt.simon@gmail.com>
2016-05-27 08:15:40 +0000

    add env due date fix method

--- src/Products/urban/Extensions/fix_environment_due_date.py
+++ src/Products/urban/Extensions/fix_environment_due_date.py
@@ -4,7 +4,7 @@ from plone import api
 
 from Products.urban.events.environmentLicenceEvents import createLicenceExpirationEvent
 from Products.urban.interfaces import IAcknowledgmentEvent
-from Products.urban.interfaces import ITheLicence
+from Products.urban.interfaces import ITheLicenceEvent
 
 
 def recompute_due_dates():
@@ -13,7 +13,7 @@ def recompute_due_dates():
     to recompute due date event.
     """
 
-    marker_interfaces = [ITheLicence.__identifier__, IAcknowledgmentEvent.__identifier__]
+    marker_interfaces = [ITheLicenceEvent.__identifier__, IAcknowledgmentEvent.__identifier__]
     catalog = api.portal.get_tool('portal_catalog')
 
     event_brains = catalog(object_provides=marker_interfaces)
