f8dda376c8b30f4aba82ba4e09bf0cbc55c6d055
Simon Delcourt <delcourt.simon@gmail.com>
2016-07-07 07:47:52 +0000

    index licence address and reference on tasks

--- src/Products/urban/schedule/indexes.py
+++ src/Products/urban/schedule/indexes.py
@@ -4,6 +4,9 @@ from imio.schedule.content.task import IAutomatedTask
 
 from plone.indexer import indexer
 
+from Products.urban.indexes import genericlicence_streetsuid
+from Products.urban.indexes import genericlicence_streetnumber
+
 
 @indexer(IAutomatedTask)
 def licence_reference_index(task):
@@ -14,3 +17,25 @@ def licence_reference_index(task):
     licence = task.get_container()
     reference = licence.getReference()
     return reference
+
+
+@indexer(IAutomatedTask)
+def licence_street_index(task):
+    """
+    Index licence street on their tasks to be able
+    to query on it.
+    """
+    licence = task.get_container()
+    street_UIDS = genericlicence_streetsuid(licence)
+    return street_UIDS
+
+
+@indexer(IAutomatedTask)
+def licence_streetnumber_index(task):
+    """
+    Index licence street number on their tasks to be able
+    to query on it.
+    """
+    licence = task.get_container()
+    street_numbers = genericlicence_streetnumber(licence)
+    return street_numbers
