d44af634a86d747523fd47615422fb14ce473a92
Simon Delcourt <delcourt.simon@gmail.com>
2016-08-16 12:06:34 +0000

    add more schedule conditions to handle FD opinion case

--- src/Products/urban/schedule/conditions/creation.py
+++ src/Products/urban/schedule/conditions/creation.py
@@ -31,6 +31,17 @@ class WillHaveInquiry(CreationCondition):
         return 'inquiry' in licence.getProcedureChoice()
 
 
+class NoInquiryCondition(CreationCondition):
+    """
+    Licence has no inquiry selected on procedureChoice field.
+    """
+
+    def evaluate(self):
+        licence = self.task_container
+        no_inquiry = 'inquiry' not in licence.getProcedureChoice()
+        return no_inquiry
+
+
 class InquiryCondition(CreationCondition):
     """
     Licence has an inquiry start date and end date defined.
@@ -54,6 +65,26 @@ class HasOpinionRequests(CreationCondition):
     def evaluate(self):
         licence = self.task_container
         return licence.getSolicitOpinionsTo()
+
+
+class OpinionRequestsDone(CreationCondition):
+    """
+    Each opinion request event has received an opinion.
+    <=> is on the state 'opinion_given'
+    """
+
+    def evaluate(self):
+        licence = self.task_container
+        or_events = licence.getOpinionRequests()
+
+        if len(or_events) != len(licence.getSolicitOpinionsTo()):
+            return False
+
+        for opinion in or_events:
+            if api.content.get_state(opinion) != 'opinion_given':
+                return False
+
+        return True
 
 
 class HasFDOpinionRequest(CreationCondition):
