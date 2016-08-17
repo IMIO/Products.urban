bd325be56a2d835c6fefabd0f5f9f2837700a6cc
Simon Delcourt <delcourt.simon@gmail.com>
2016-08-17 07:46:26 +0000

    add annexes transfer to pm

---
+++ src/Products/urban/browser/urbandoc2pm.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from Products.Five import BrowserView
+
+import base64
+
+
+class UrbanDoc2PloneMeeting(BrowserView):
+    """
+    """
+    def getAnnexes(self):
+        documents = self.context.getAttachments()
+        annexes = []
+        for doc in documents:
+            annexes.append(
+                {
+                    'title': doc.title.encode('utf-8'),
+                    'filename': doc.getFilename().encode('utf-8'),
+                    'file': base64.b64encode(doc.getFile().data),
+                }
+            )
+        return annexes
