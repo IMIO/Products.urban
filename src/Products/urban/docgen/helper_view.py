6c982c65dd6bf20b44fe98ce085bbd2e3d20aa27
Franck Ngaha <franck.o.ngaha@gmail.com>
2016-08-30 13:35:16 +0000

    Redo get_division

--- src/Products/urban/docgen/helper_view.py
+++ src/Products/urban/docgen/helper_view.py
@@ -11,6 +11,13 @@ class UrbanDocGenerationHelperView(ATDocumentGenerationHelperView):
     Urban implementation of document generation helper methods.
     """
 
+    def get_division(self):
+        parcels = self.context.getParcels()
+        parcel_view = parcels and parcels[0].restrictedTraverse('document_generation_helper_view')
+        raw_div = parcel_view.context.division
+        division = raw_div and raw_div[raw_div.find('(') + 1:-1] or 'DIVISION INCONNUE'
+        return division
+
     def formatDate(self, date=_date.today(), translatemonth=True, long_format=False):
         """
           Format the date for printing in pod templates
