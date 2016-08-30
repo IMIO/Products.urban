04be5fa5f1b833fe06fd03b6091c5af188c1b6f5
Franck Ngaha <franck.o.ngaha@gmail.com>
2016-08-30 08:24:29 +0000

    backport r24057

--- src/Products/urban/docgen/helper_view.py
+++ src/Products/urban/docgen/helper_view.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from collective.documentgenerator.helper.archetypes import ATDocumentGenerationHelperView
+from datetime import date as _date
+from Products.CMFPlone.i18nl10n import ulocalized_time
+from zope.i18n import translate
 
 
 class UrbanDocGenerationHelperView(ATDocumentGenerationHelperView):
@@ -8,11 +11,43 @@ class UrbanDocGenerationHelperView(ATDocumentGenerationHelperView):
     Urban implementation of document generation helper methods.
     """
 
-    def get_division(self):
+    def formatDate(self, date=_date.today(), translatemonth=True, long_format=False):
         """
+          Format the date for printing in pod templates
         """
-        parcels = self.context.getParcels()
-        parcel_view = parcels and parcels[0].restrictedTraverse('document_generation_helper_view')
-        raw_div = parcel_view.context.division
-        division = raw_div and raw_div[raw_div.find('(') + 1:-1] or 'DIVISION INCONNUE'
-        return division
+        if date:
+            if not translatemonth:
+                return ulocalized_time(date, long_format=long_format, context=self, request=self.request).encode('utf8')
+            else:
+                #we need to translate the month and maybe the day (1er)
+                year, month, day, hour = str(date.strftime('%Y/%m/%d/%Hh%M')).split('/')
+                #special case when the day need to be translated
+                #for example in french '1' becomes '1er' but in english, '1' becomes '1st'
+                #if no translation is available, then we use the default where me remove foregoing '0'
+                #'09' becomes '9', ...
+                daymsgid = "date_day_%s" % day
+                translatedDay = translate(daymsgid, 'urban', context=self.request, default=day.lstrip('0')).encode('utf8')
+                #translate the month
+                #msgids already exist in the 'plonelocales' domain
+                monthMappings = {
+                    '01': 'jan',
+                    '02': 'feb',
+                    '03': 'mar',
+                    '04': 'apr',
+                    '05': 'may',
+                    '06': 'jun',
+                    '07': 'jul',
+                    '08': 'aug',
+                    '09': 'sep',
+                    '10': 'oct',
+                    '11': 'nov',
+                    '12': 'dec',
+                }
+                monthmsgid = "month_%s" % monthMappings[month]
+                translatedMonth = translate(monthmsgid, 'plonelocales', context=self.request).encode('utf8').lower()
+            if long_format:
+                at_hour = translate('at_hour', 'urban', mapping={'hour': hour}, context=self.request).encode('utf-8')
+                return "%s %s %s %s" % (translatedDay, translatedMonth, year, at_hour)
+            else:
+                return "%s %s %s" % (translatedDay, translatedMonth, year)
+        return ''
