6300255f8d6c1dbcd3ffbae3a55497e09c9edc1f
Franck Ngaha <franck.o.ngaha@gmail.com>
2016-09-26 09:58:58 +0000

    Update reindex method

--- src/Products/urban/Extensions/reindex_object.py
+++ src/Products/urban/Extensions/reindex_object.py
@@ -1,8 +1,8 @@
 from plone import api
 
 
-def reindex_object_by_uid(uid):
+def reindex_object_uid_by_uid(uid):
     cat = api.portal.get_tool('portal_catalog')
     brains = cat(UID=uid)
     obj = brains[0].getObject()
-    obj.reindexObject()
+    obj.reindexObject(idxs=["UID"])
