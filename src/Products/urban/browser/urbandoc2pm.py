3926237afe2c4c4bfbe89fbc77f188d3373652a0
Simon Delcourt <delcourt.simon@gmail.com>
2016-10-12 13:19:51 +0000

    normaize doc title for annex transfer to pm

--- src/Products/urban/browser/urbandoc2pm.py
+++ src/Products/urban/browser/urbandoc2pm.py
@@ -2,6 +2,8 @@
 
 from Products.Five import BrowserView
 
+from plone import api
+
 import base64
 
 
@@ -9,14 +11,15 @@ class UrbanDoc2PloneMeeting(BrowserView):
     """
     """
     def getAnnexes(self):
+	plone_utils = api.portal.get_tool('plone_utils')
         documents = self.context.getAttachments()
         documents.extend(self.context.getDocuments())
         annexes = []
         for doc in documents:
             annexes.append(
                 {
-                    'title': doc.title.encode('utf-8'),
-                    'filename': doc.getFilename().encode('utf-8'),
+                    'title': plone_utils.normalizeString(doc.title),
+                    'filename': plone_utils.normalizeString(doc.getFilename().encode('utf-8')),
                     'file': base64.b64encode(doc.getFile().data),
                 }
             )
