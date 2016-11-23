ad45de6860eb7433ff5475bf04e3ed9ac7497c22
Simon Delcourt <delcourt.simon@gmail.com>
2016-11-23 10:41:45 +0000

    add a portlet to navigate between the different schedules views

--- src/Products/urban/browser/portlets.py
+++ src/Products/urban/browser/portlets.py
@@ -115,3 +115,73 @@ class ConfigEditForm(base.EditForm):
     form_fields = form.Fields(IUrbanConfigPortlet)
     label = _(u"Edit Urban config Portlet")
     description = _(u"This portlet lists urban configuration.")
+
+
+class IUrbanScheduledLicencesPortlet(IPortletDataProvider):
+    """ A portlet listing scheduled licences."""
+
+
+class ScheduledLicencesAssignment(base.Assignment):
+    implements(IUrbanScheduledLicencesPortlet)
+
+    def title(self):
+        return u"Urban scheduled licences widget"
+
+
+class ScheduledLicencesRenderer(base.Renderer):
+
+    @property
+    def available(self):
+        return True
+        if api.user.is_anonymous():
+            return False
+
+        site = api.portal.get()
+        if self.context == site.urban:
+            context = self.context.buildlicences
+        else:
+            context = self.context
+
+        roles = api.user.get_roles(user=api.user.get_current(), obj=context)
+        available = 'Manager' in roles or 'Editor' in roles or 'Reader' in roles
+        return available
+
+    def site_url(self):
+        portal = api.portal.get()
+        url = portal.absolute_url()
+        return url
+
+    def render(self):
+        return ViewPageTemplateFile('templates/portlet_scheduled_licences.pt')(self)
+
+    def scheduled_licences_links(self):
+        urban_tool = api.portal.get_tool('portal_urban')
+        licence_configs = urban_tool.objectValues('LicenceConfig')
+        site_url = api.portal.get().absolute_url()
+        links = []
+
+        for licence_cfg in licence_configs:
+            schedule_cfg = getattr(licence_cfg, 'schedule', None)
+            if schedule_cfg and not schedule_cfg.is_empty():
+                portal_type = schedule_cfg.get_scheduled_portal_type()
+                licence_type = portal_type.lower()
+                href = '{}/urban/schedule/{}'.format(site_url, licence_type)
+                klass = 'content-shortcuts contenttype-{}'.format(licence_type)
+                links.append((href, klass, portal_type))
+
+        return links
+
+
+class ScheduledLicencesAddForm(base.AddForm):
+    form_fields = form.Fields(IUrbanScheduledLicencesPortlet)
+    label = _(u"Add Urban scheduled licences Portlet")
+    description = _(u"This portlet lists scheduled licences.")
+
+    def create(self, data):
+        return ScheduledLicencesAssignment(**data)
+
+
+class ScheduledLicencesEditForm(base.EditForm):
+    form_fields = form.Fields(IUrbanScheduledLicencesPortlet)
+    label = _(u"Edit Urban scheduled licences Portlet")
+    description = _(u"This portlet lists scheduled licences.")
