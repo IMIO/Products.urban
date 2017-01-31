7d4840a224ba2b353caf1ce9dbd17b5102b2cafb
Simon Delcourt <delcourt.simon@gmail.com>
2017-01-31 08:58:29 +0000

    can only generate documents if you have the permissions to add File on the context

--- src/Products/urban/docgen/condition.py
+++ src/Products/urban/docgen/condition.py
@@ -17,5 +17,10 @@ class UrbanTemplateCondition(ConfigurablePODTemplateCondition):
 
     def evaluate(self):
         base_condition = super(UrbanTemplateCondition, self).evaluate()
+
         state_enabled = api.content.get_state(self.pod_template) == 'enabled'
-        return base_condition and state_enabled
+
+        permissions = api.user.get_permissions(user=api.user.get_current(), obj=self.context)
+        can_add_documents = permissions['ATContentTypes: Add File']
+
+        return base_condition and state_enabled and can_add_documents
