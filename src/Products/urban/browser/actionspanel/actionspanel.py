eec544f813ce55c641d34be3091f94718605cd06
Simon Delcourt <delcourt.simon@gmail.com>
2017-02-01 04:58:43 +0000

    fix events hook

--- src/Products/urban/browser/actionspanel/actionspanel.py
+++ src/Products/urban/browser/actionspanel/actionspanel.py
@@ -24,7 +24,7 @@ class EventActionsPanelView(ActionsPanelView):
     """
     def __init__(self, context, request):
         super(EventActionsPanelView, self).__init__(context, request)
-        self.SECTIONS_TO_RENDER = ('renderTransitions', 'renderEdit', 'renderActions', 'renderOwnDelete',)
+        self.SECTIONS_TO_RENDER = ('renderEdit', 'renderTransitions', 'renderActions', 'renderOwnDelete',)
         self.ACCEPTABLE_ACTIONS = ('plonemeeting_wsclient_action_1',)
 
 
