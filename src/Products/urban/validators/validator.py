49a1df4249fe5fcaa46ddfdb22dde93f5d8d7317
Simon Delcourt <delcourt.simon@gmail.com>
2017-02-03 16:20:46 +0000

    force to share generated reference as well

--- src/Products/urban/validators/validator.py
+++ src/Products/urban/validators/validator.py
@@ -4,6 +4,7 @@ from zope.i18n import translate
 from Products.urban import UrbanMessage as _
 from Products.CMFCore.utils import getToolByName
 
+
 class isTextFieldConfiguredValidator:
     """
     Check if a text field has been already configured or not, so it cannot be configured twice
@@ -20,9 +21,11 @@ class isTextFieldConfiguredValidator:
             if val['fieldname'] not in configured_fields:
                 configured_fields.append(val['fieldname'])
             else:
-                return translate(_('error_textcfg',
-                                    default=u"The field '${fieldname}' is configured twice",
-                                    mapping={'fieldname':val['fieldname']}))
+                return translate(
+                    _('error_textcfg',
+                      default=u"The field '${fieldname}' is configured twice",
+                      mapping={'fieldname': val['fieldname']})
+                )
         return 1
 
 
@@ -38,10 +41,10 @@ class isValidStreetNameValidator:
     def __call__(self, value, *args, **kwargs):
         for line in value:
             if line['orderindex_'] != 'template_row_marker' and not line['street']:
-                return translate(_('error_streetname',
-                                    default=u"Please select a valid street"))
+                return translate(
+                    _('error_streetname', default=u"Please select a valid street")
+                )
         return 1
-
 
 
 class isNotDuplicatedReferenceValidator:
@@ -57,11 +60,12 @@ class isNotDuplicatedReferenceValidator:
     def __call__(self, value, *args, **kwargs):
         licence = kwargs['instance']
         catalog = getToolByName(licence, 'portal_catalog')
-        similar_licences = catalog(portal_type=licence.portal_type, getReference=value)
+        similar_licences = catalog(getReference=value)
         if not similar_licences or (len(similar_licences) == 1 and licence.UID() == similar_licences[0].UID):
             return 1
-        return translate(_('error_reference',
-                            default=u"This reference has already been encoded"))
+        return translate(
+            _('error_reference', default=u"This reference has already been encoded")
+        )
 
 
 class procedureChoiceValidator:
@@ -73,19 +77,18 @@ class procedureChoiceValidator:
         self.name = name
 
     def __call__(self, value, *args, **kwargs):
-        licence = kwargs['instance']
         if 'ukn' in value and len(value) > 2:
             return translate(
                 _('error_procedure_choice',
-                default=u"Cannot select 'unknown' with another value")
+                  default=u"Cannot select 'unknown' with another value")
             )
         return True
 
 
-"""
-Validators for parcel reference values, used in the case where the parcel is addded manually
-"""
 class isValidSectionValidator:
+    """
+    Validators for parcel reference values, used in the case where the parcel is addded manually
+    """
     implements(IValidator)
 
     def __init__(self, name):
