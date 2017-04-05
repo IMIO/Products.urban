b35687315b7a473ba36bdab4cef966a59251767d
Simon Delcourt <delcourt.simon@gmail.com>
2017-04-05 09:24:57 +0000

    make sure to keep eventype interfaces up to date on urban events

--- src/Products/urban/events/urbanEventTypesEvents.py
+++ src/Products/urban/events/urbanEventTypesEvents.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
+
 from Products.CMFCore.utils import getToolByName
+
+from Products.urban.events.urbanEventEvents import setEventTypeType
+from Products.urban.interfaces import IEventTypeType
+
+from plone import api
+
+from zope.interface import noLongerProvides
+from zope.interface import providedBy
+
 
 def updateKeyEvent(urbanEventType, event):
     catalog = getToolByName(urbanEventType, 'portal_catalog')
@@ -11,3 +21,19 @@ def updateKeyEvent(urbanEventType, event):
         urban_event = brain.getObject()
         licence = urban_event.aq_parent
         licence.reindexObject(['last_key_event'])
+
+
+def updateEventType(urban_event_type, event):
+    """
+    """
+    ref_catalog = api.portal.get_tool('reference_catalog')
+    ref_brains = ref_catalog(targetUID=urban_event_type.UID())
+    for ref_brain in ref_brains:
+        ref = ref_brain.getObject()
+        urban_event = ref.getSourceObject()
+        # clean previous event type interface
+        for provided_interface in providedBy(urban_event).flattened():
+            if IEventTypeType.providedBy(provided_interface):
+                noLongerProvides(urban_event, provided_interface)
+        # add new provided interface
+        setEventTypeType(urban_event, event)
