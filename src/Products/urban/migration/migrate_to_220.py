42db91eb91bf5c6203fa3aa5af8812c4e7f6f281
Simon Delcourt <delcourt.simon@gmail.com>
2017-05-03 12:53:18 +0000

    update upgrade steop to 2.2

--- src/Products/urban/migration/migrate_to_220.py
+++ src/Products/urban/migration/migrate_to_220.py
@@ -1,6 +1,11 @@
 # encoding: utf-8
 
 from plone import api
+from plone.portlets.constants import CONTEXT_CATEGORY, GROUP_CATEGORY, CONTENT_TYPE_CATEGORY
+from plone.portlets.interfaces import IPortletManager
+from plone.portlets.interfaces import ILocalPortletAssignmentManager
+from zope.component import getMultiAdapter
+from zope.component import getUtilitiesFor
 
 import logging
 
@@ -22,8 +27,8 @@ def migrate_inquiry_tabs():
     logger.info("migration step done!")
 
 
-def migrate_inquiry_eventtype_eventportaltype():
-    logger = logging.getLogger('urban: set default eventPortalType of inquiries to UrbanEventInquiry')
+def migrate_inquiry_eventtype():
+    logger = logging.getLogger('urban: migrate inquiry event type')
     logger.info("starting migration step")
     portal_urban = api.portal.get_tool('portal_urban')
     licence_configs = portal_urban.objectValues('LicenceConfig')
@@ -32,13 +37,38 @@ def migrate_inquiry_eventtype_eventportaltype():
         for event_type in eventtype_folder.objectValues():
             if 'enquete-publique' in event_type.id:
                 event_type.setEventPortalType('UrbanEventInquiry')
+                old_fields = event_type.getActivatedFields()
+                new_fields = ['investigationStart', 'investigationEnd'] + old_fields
+                event_type.setActivatedFields(new_fields)
 
+    logger.info("migration step done!")
+
+
+def block_urban_parent_portlets():
+    logger = logging.getLogger('urban: block urban folder portlets')
+    logger.info("starting migration step")
+    portal = api.portal.get()
+    urban_folder = portal.urban
+    for manager_name, src_manager in getUtilitiesFor(IPortletManager, context=urban_folder):
+        assignment_manager = getMultiAdapter(
+            (urban_folder, src_manager),
+            ILocalPortletAssignmentManager
+        )
+        assignment_manager.setBlacklistStatus(CONTEXT_CATEGORY, True)
+        for category in (GROUP_CATEGORY, CONTENT_TYPE_CATEGORY):
+            assignment_manager.setBlacklistStatus(
+                category,
+                assignment_manager.getBlacklistStatus(category)
+            )
     logger.info("migration step done!")
 
 
 def migrate(context):
     logger = logging.getLogger('urban: migrate to 2.1')
     logger.info("starting migration steps")
+    setup_tool = api.portal.get_tool(context, 'portal_setup')
+    setup_tool.runAllImportStepsFromProfile('profile-imio.schedule:default')
+    block_urban_parent_portlets()
     migrate_inquiry_tabs()
-    migrate_inquiry_eventtype_eventportaltype()
+    migrate_inquiry_eventtype()
     logger.info("migration done!")
