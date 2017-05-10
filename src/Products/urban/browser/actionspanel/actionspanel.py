35a1487bd5d1eda6c053446a6dfc52b7a32d8265
Simon Delcourt <delcourt.simon@gmail.com>
2017-05-10 08:09:04 +0000

    render delete action for urban events again

--- src/Products/urban/browser/actionspanel/actionspanel.py
+++ src/Products/urban/browser/actionspanel/actionspanel.py
@@ -24,7 +24,7 @@ class EventActionsPanelView(ActionsPanelView):
     """
     def __init__(self, context, request):
         super(EventActionsPanelView, self).__init__(context, request)
-        self.SECTIONS_TO_RENDER = ('renderEdit', 'renderActions')
+        self.SECTIONS_TO_RENDER = ('renderEdit', 'renderOwnDelete', 'renderActions')
         self.ACCEPTABLE_ACTIONS = ('plonemeeting_wsclient_action_1',)
 
 
