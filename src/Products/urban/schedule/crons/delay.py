f5753544ec5d89ad73b840a81b24cb16e8686b9a
Martin Peeters <martin.peeters@affinitic.be>
2017-05-19 12:58:08 +0000

    Implement the cron mechanism and the cron for the auto acceptation of CODT folder after 20 days
    This refs #16911

---
+++ src/Products/urban/schedule/crons/delay.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+from Products.urban.schedule.cron import TaskCron
+from plone import api
+from DateTime import DateTime
+
+
+class AutoAccepted20days(TaskCron):
+
+    def execute(self):
+        for brain in self.brains:
+            api.content.transition(
+                obj=brain.getObject(),
+                to_state='accepted',
+            )
+
+    @property
+    def brains(self):
+        date_range = {
+            'query': (DateTime() - 50, DateTime() - 20),
+            'range': 'min:max'
+        }
+        return api.content.find(
+            portal_type='CODT_BuildLicence',
+            review_state='in_progress',
+            getDepositDate=date_range,
+        )
