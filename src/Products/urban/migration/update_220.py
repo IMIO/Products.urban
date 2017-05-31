6e0ce3a5632c52e0b0321aa1e0fe0b8a6fea89b4
Simon Delcourt <delcourt.simon@gmail.com>
2017-05-31 13:37:50 +0000

    add update steps for schedule

--- src/Products/urban/migration/update_220.py
+++ src/Products/urban/migration/update_220.py
@@ -2,6 +2,8 @@
 
 from plone import api
 
+from Products.urban.profiles.extra.schedule_config import schedule_config
+
 import logging
 
 logger = logging.getLogger('urban: migrations')
@@ -10,10 +12,14 @@ logger = logging.getLogger('urban: migrations')
 def migrate(context):
     logger = logging.getLogger('urban: update to 2.2')
     logger.info("starting migration steps")
+
+    portal_urban = api.portal.get_tool('portal_urban')
+    for config_id in schedule_config.keys():
+        schedule = getattr(portal_urban, config_id).schedule
+        schedule.manage_delObjects(ids=schedule.objectIds())
+
     setup_tool = api.portal.get_tool('portal_setup')
-    setup_tool.runImportStepFromProfile('profile-collective.faceted.task:default', 'componentregistry')
-    setup_tool.runAllImportStepsFromProfile('profile-collective.faceted.task:default')
-    setup_tool.runAllImportStepsFromProfile('profile-Products.urban:default')
-    setup_tool.runImportStepFromProfile('profile-Products.urban:extra', 'urban-extraPostInstall')
-    setup_tool.runAllImportStepsFromProfile('profile-imio.schedule:default')
+    setup_tool.runImportStepFromProfile('profile-Products.urban:extra', 'urban-extra')
+    catalog = api.portal.get_tool('portal_catalog')
+    catalog.refreshCatalog(clear=True)
     logger.info("migration done!")
