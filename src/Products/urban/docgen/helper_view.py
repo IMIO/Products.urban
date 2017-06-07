df4ed5e7bde720045c64269a6a4a4fd63c1a3181
Franck Ngaha <franck.o.ngaha@gmail.com>
2017-06-07 11:27:19 +0000

    refactor helpers and use foldermanager title instead of id

--- src/Products/urban/docgen/helper_view.py
+++ src/Products/urban/docgen/helper_view.py
@@ -81,43 +81,67 @@ class UrbanDocGenerationHelperView(ATDocumentGenerationHelperView):
 # Demandeur(s)
 #------------------------------------------------------------------------------
 
-    def get_applicants_names_and_address(
-            self,
-            applicant_separator=', ',
-            representedBy_separator=' et ',
-            resident={
-                'Masculin-Singulier':' domicilié ',
-                'Masculin-Pluriel':' domiciliés ',
-                'Féminin-Singulier':' domiciliée',
-                'Féminin-Pluriel':' domiciliées'
-            },
-            represented={
-                'Masculin-Singulier':' représenté par ',
-                'Masculin-Pluriel':' représentés par ',
-                'Féminin-Singulier':' représentée par',
-                'Féminin-Pluriel':' représentées par'
-            },
-            reversed_name=True,
-        ):
-        applicants = self.get_applicants_list_dict()
-        applicants_names_and_address = ""
-        if applicants:
-            applicants_names_and_address = self._get_applicant_names_and_address(
-                    applicants[0],
+    def _get_applicant_names_and_address(self, applicant, resident, represented, reversed_name, representedBy_separator):
+        applicant_names_and_address = self._get_contact_names_and_address(applicant, resident, reversed_name)
+        if applicant['representedBySociety']:
+            gender_multiplicity = applicant['gender'] + '-' + applicant['multiplicity']
+            applicant_names_and_address += represented[gender_multiplicity] +\
+                    self._get_representedBy_names_and_address(applicant, resident, reversed_name, representedBy_separator)
+        return applicant_names_and_address
+
+    def get_applicants_list_dict(self):
+        context = self.real_context
+        applicants = context.getApplicants()
+        applicants_list_dict = []
+        for i in range(len(applicants)):
+            applicants_list_dict.append(self.get_applicant_dict(i))
+        return applicants_list_dict
+
+    def get_applicant_dict(self, index):
+        context = self.real_context
+        applicant = context.getApplicants()[index]
+        applicant_dict = self._get_contact_dict(applicant)
+        applicant_dict['representedBySociety'] = applicant.getRepresentedBySociety()
+        applicant_dict['isSameAddressAsWorks'] = applicant.getIsSameAddressAsWorks()
+        applicant_dict['representedBy'] = applicant.getRepresentedBy()
+        return applicant_dict
+
+    def _get_representedBy_names_and_address(self, applicant, resident, reversed_name, representedBy_separator):
+        representedBy_list = self._get_representedBy_list(applicant)
+        representedBy_names_and_address = ""
+        if representedBy_list:
+            representedBy_names_and_address = self._get_contact_names_and_address(
+                    representedBy_list[0],
                     resident,
-                    represented,
-                    reversed_name,
-                    representedBy_separator
+                    reversed_name
             )
-            for applicant in applicants[1:]:
-                applicants_names_and_address += applicant_separator + self._get_applicant_names_and_address(
-                        applicant,
+            for representedBy in representedBy_list[1:]:
+                representedBy_names_and_address += representedBy_separator + self._get_contact_names_and_address(
+                        representedBy,
                         resident,
-                        represented,
-                        reversed_name,
-                        representedBy_separator
+                        reversed_name
                 )
-        return applicants_names_and_address
+        return representedBy_names_and_address
+
+    def _get_representedBy_list(self, applicant):
+        representedBy_UIDs = applicant['representedBy']
+        representedBy_list = []
+        for representedBy_UID in representedBy_UIDs:
+            catalog = self.portal.portal_catalog
+            brains = catalog.searchResults(UID=representedBy_UID)
+            representedBy = brains[0].getObject()
+            contact_dict = self._get_contact_dict(representedBy)
+            representedBy_list.append(contact_dict)
+        return representedBy_list
+
+    def get_applicants_names(self, separator=', ', reversed_name=True):
+        applicants = self.get_applicants_list_dict()
+        applicants_names = ""
+        if applicants:
+            applicants_names = self._get_applicant_names(applicants[0], reversed_name)
+            for applicant in applicants[1:]:
+                applicants_names += separator + self._get_applicant_names(applicant, reversed_name)
+        return applicants_names
 
     def _get_applicant_names(self, applicant, reversed_name=True):
         applicant_names = applicant['personTitle'] + ' ' + applicant['name2'] + ' ' + applicant['name1']
@@ -659,7 +683,10 @@ class LicenceDisplayProxyObject(ATDisplayProxyObject):
             foldermanager = foldermanagers[index]
             result = self._get_contact_dict(foldermanager)
             result['initials'] = foldermanager.getInitials()
-            result['grade'] = foldermanager.getGrade()
+            grades = UrbanVocabulary('persons_grades', inUrbanConfig=False)
+            allVocTerms = grades.getAllVocTerms(context)
+            grade = allVocTerms[foldermanager.getGrade()]
+            result['grade'] = grade.Title()
             result['ploneUserId'] = foldermanager.getPloneUserId()
             result['manageableLicences'] = foldermanager.getManageableLicences()
         return result
