bf1fad132a5d67bd4fbca4bdf91a930580643d1f
Simon Delcourt <delcourt.simon@gmail.com>
2017-07-05 07:41:56 +0000

    make sure to active facteted navigation on all licences now

--- src/Products/urban/events/licenceEvents.py
+++ src/Products/urban/events/licenceEvents.py
@@ -6,6 +6,8 @@ from Products.urban.interfaces import IUrbanEvent
 from Products.urban.utils import getCurrentFolderManager
 from Products.urban.schedule.vocabulary import URBAN_TYPES_INTERFACES
 
+from collective.faceted.task.event.task_events import activate_faceted_tasks_listing
+
 from zope.interface import alsoProvides
 
 
@@ -112,3 +114,9 @@ def reindex_attachments_permissions(container, event):
     if IUrbanEvent.providedBy(container):
         licence = container.aq_parent
         reindex_attachments_permissions(licence, event)
+
+
+def set_faceted_navigation(licence, event):
+    """
+    """
+    activate_faceted_tasks_listing(licence, event)
