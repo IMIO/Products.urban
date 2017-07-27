0f00786c7a43a98b94cdfacbf8d06e085a9ddf13
Simon Delcourt <delcourt.simon@gmail.com>
2017-07-27 13:19:01 +0000

    only update dashboard collection of schedule

--- src/Products/urban/events/dashboard_collection.py
+++ src/Products/urban/events/dashboard_collection.py
@@ -1,21 +1,23 @@
 # -*- coding: utf-8 -*-
 
 from imio.schedule.content.schedule_config import IScheduleConfig
+from imio.schedule.content.task_config import ITaskConfig
 
 
 def set_dashboard_columns(dashboard_collection, event):
     """
     """
-    columns = (
-        u'sortable_title',
-        u'address_column',
-        u'assigned_user_column',
-        u'status',
-        u'due_date',
-        u'licence_final_duedate',
-        u'task_actions_column'
-    )
-    if IScheduleConfig.providedBy(dashboard_collection.aq_parent):
+    if ITaskConfig.providedBy(dashboard_collection.aq_parent):
+        columns = (
+            u'sortable_title',
+            u'address_column',
+            u'assigned_user_column',
+            u'status',
+            u'due_date',
+            u'licence_final_duedate',
+            u'task_actions_column'
+        )
+    elif IScheduleConfig.providedBy(dashboard_collection.aq_parent):
         columns = (
             u'sortable_title',
             u'pretty_link',
@@ -26,4 +28,7 @@ def set_dashboard_columns(dashboard_collection, event):
             u'licence_final_duedate',
             u'task_actions_column'
         )
+    else:
+        return
+
     dashboard_collection.setCustomViewFields(columns)
