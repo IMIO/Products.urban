e715c5dc6406aba6e0a5fad3ebf4fa0835c652e1
Simon Delcourt <delcourt.simon@gmail.com>
2018-01-18 10:42:04 +0000

    light fix

--- src/Products/urban/docgen/helper_view.py
+++ src/Products/urban/docgen/helper_view.py
@@ -28,8 +28,8 @@ class UrbanDocGenerationHelperView(ATDocumentGenerationHelperView):
 
     def xhtml(self, html_code, style='UrbanBody'):
         urban_tool = api.portal.get_tool('portal_urban')
-        decorated_html = urban_tool.decorateHTML(html_code, style)
-        xhtml = self.helper_view.appy_renderer.renderXhtml(decorated_html)
+        decorated_html = urban_tool.decorateHTML(style, html_code)
+        xhtml = self.appy_renderer.renderXhtml(decorated_html)
         return xhtml
 
     def get_current_foldermanager(self):
