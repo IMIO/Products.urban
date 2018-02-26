83f352523d274392669ae665b3360601ab0191ab
Simon Delcourt <delcourt.simon@gmail.com>
2018-02-26 07:57:49 +0000

    add conditional link to external opinions schedule view

--- src/Products/urban/browser/portlets.py
+++ src/Products/urban/browser/portlets.py
@@ -48,6 +48,13 @@ class ToolsRenderer(base.Renderer):
     def render(self):
         return ViewPageTemplateFile('templates/portlet_urbantools.pt')(self)
 
+    def is_opinion_editor(self):
+        current_user = api.user.get_current()
+        user_groups = api.group.get_groups(user=current_user)
+        group_ids = [g.id for g in user_groups]
+        is_opinion_editor = 'opinions_editors' in group_ids or api.user.has_permission('Manage portal')
+        return is_opinion_editor
+
 
 class ToolsAddForm(base.AddForm):
     form_fields = form.Fields(IUrbanToolsPortlet)
