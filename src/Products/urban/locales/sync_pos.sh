0d6c3327b690706123cd6ba7ceff5b28a763ab76
Martin Peeters <martin.peeters@affinitic.be>
2018-03-08 10:07:21 +0000

    Refactor translations file to automate extraction

--- src/Products/urban/locales/sync_pos.sh
+++
@@ -1,5 +0,0 @@
-files="urban plone datagridfield collective.eeafaceted.z3ctable imio.schedule"
-for file in $files; do
-    i18ndude sync --pot $file.pot fr/LC_MESSAGES/$file.po
-    msgfmt -o fr/LC_MESSAGES/$file.mo fr/LC_MESSAGES/$file.po
-done
