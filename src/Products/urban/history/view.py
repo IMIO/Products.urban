a56d9f8d8ead29d81bb07b3a743d0855042408a3
Sebastien Sirtoli <sebastien.sirtoli@affinitic.be>
2018-07-04 10:55:22 +0200

    Add historize popup

--- src/Products/urban/history/view.py
+++ src/Products/urban/history/view.py
@@ -9,6 +9,15 @@ class HistoryView(IHContentHistoryView):
         u'workflow',
         u'update_rubrics',
         u'rubrics_history',
-        u'update_additionalLegalCondition',
+        u'update_additionalLegalConditions',
         u'additionalLegalConditions_history',
     )
+
+
+class SpecificHistoryView(IHContentHistoryView):
+
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+        self.histories_to_handle = (u'update_{0}'.format(self.request.get('item')),
+                                    u'{0}_history'.format(self.request.get('item')))
