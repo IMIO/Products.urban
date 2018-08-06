9f3437c37e441d60d88c42798e66195e085cebe6
Sebastien Sirtoli <sebastien.sirtoli@affinitic.be>
2018-08-06 14:59:38 +0200

    WIP road decree

--- src/Products/urban/content/licence/RoadDecree.py
+++ src/Products/urban/content/licence/RoadDecree.py
@@ -5,7 +5,7 @@ from AccessControl import ClassSecurityInfo
 from Products.Archetypes.atapi import *
 from zope.interface import implements
 from Products.urban import interfaces
-from Products.urban.content.licence.BuildLicence import BuildLicence
+from Products.urban.content.licence.CODT_BuildLicence import CODT_BuildLicence
 from Products.CMFDynamicViewFTI.browserdefault import BrowserDefaultMixin
 from Products.MasterSelectWidget.MasterSelectWidget import MasterSelectWidget
 from Products.urban.content.licence.EnvClassOne import EnvClassOne
@@ -16,37 +16,25 @@ from Products.urban.config import *
 ##code-section module-header #fill in your manual code here
 ##/code-section module-header
 
-schema = Schema((
-    StringField(
-            name='envclasschoices',
-            default='ukn',
-            widget=MasterSelectWidget(
-                    label='Type de classe d\'environement',
-                    label_msgid='urban_label_listenvclasschoices',
-                    i18n_domain='urban',
-            ),
-            schemata='urban_description',
-            multiValued=1,
-            vocabulary='listEnvClassChoices',
-    ),
-),
-)
+
 
 ##code-section after-local-schema #fill in your manual code here
 ##/code-section after-local-schema
 
-RoadDecree_schema = BuildLicence.schema.copy() + schema.copy()
+RoadDecree_schema = CODT_BuildLicence.schema.copy()
 
 
 ##code-section after-schema #fill in your manual code here
 ##/code-section after-schema
 
-class RoadDecree(BaseFolder):
+class RoadDecree(CODT_BuildLicence):
     """
     """
     implements(interfaces.IRoadDecree)
 
     meta_type = 'RoadDecree'
+
+    RoadDecree_schema['roadAdaptation'].schemata = 'urban_road'
 
     schema = RoadDecree_schema
     ##code-section class-header #fill in your manual code here
