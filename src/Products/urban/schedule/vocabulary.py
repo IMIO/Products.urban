1a96633808090a59372ef46af29c9b9be23508ea
Simon Delcourt <delcourt.simon@gmail.com>
2018-08-29 16:44:01 +0200

    Fixed internal opinions schedule view.

--- src/Products/urban/schedule/vocabulary.py
+++ src/Products/urban/schedule/vocabulary.py
@@ -135,3 +135,10 @@ class UsersFromGroupsVocabularyFactory(object):
 
         vocabulary = SimpleVocabulary(base_terms + sorted(voc_terms, key=lambda term: term.title))
         return vocabulary
+
+
+class OpinionUsersVocabularyFactory(UsersFromGroupsVocabularyFactory):
+    """
+    Vocabulary factory listing all the users of the survey group.
+    """
+    group_ids = ['opinions_editors']
