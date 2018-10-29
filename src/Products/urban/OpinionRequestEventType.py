385a01efad286c1d44b007b4fbee9ae88e9d4aac
Simon Delcourt <delcourt.simon@gmail.com>
2018-10-29 09:41:43 +0100

    Handle case of empty registry.

--- src/Products/urban/OpinionRequestEventType.py
+++ src/Products/urban/OpinionRequestEventType.py
@@ -139,7 +139,7 @@ class OpinionRequestEventType(OrderedBaseFolder, UrbanEventType, UrbanVocabulary
 
     def listInternalServices(self):
         registry = api.portal.get_tool('portal_registry')
-        registry_field = registry['Products.urban.interfaces.IInternalOpinionServices.services']
+        registry_field = registry['Products.urban.interfaces.IInternalOpinionServices.services'] or {}
         voc_terms = [(key, value['full_name']) for key, value in registry_field.iteritems()]
         vocabulary = DisplayList(voc_terms)
         return vocabulary
