1cce7336a8f483b4697fbd0d0c7a7f0104635d73
Simon Delcourt <delcourt.simon@gmail.com>
2019-06-12 09:50:08 +0200

    Use capakey for parcel catalog search.

--- src/Products/urban/browser/parcelsinfo.py
+++ src/Products/urban/browser/parcelsinfo.py
@@ -20,7 +20,7 @@ class ParcelsInfo(BrowserView):
         catalog = api.portal.get_tool('portal_catalog')
         licence_brains = catalog(
             object_provides=IGenericLicence.__identifier__,
-            parcelInfosIndex=parcel.to_index()
+            parcelInfosIndex=parcel.capakey
         )
         return licence_brains
 
