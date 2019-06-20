3bba8d0ff71bd506528c84698b943e39710d5cce
Simon Delcourt <delcourt.simon@gmail.com>
2019-06-20 13:52:42 +0200

    Safer guard in case of bad indexes for cadastral search.

--- src/Products/urban/dashboard/autocomplete.py
+++ src/Products/urban/dashboard/autocomplete.py
@@ -164,7 +164,7 @@ class CadastralReferenceSuggest(SuggestView):
             v[11:13].lstrip('0'),
             v[13].lstrip('0'),
             v[14:].lstrip('0')), v)
-            for v in values]
+            for v in values if len(v) > 14]
         return all_values
 
     def compute_suggestions(self):
