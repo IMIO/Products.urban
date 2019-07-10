f370515ac0eca6aa631a9f4508e827dc33089ab4
Simon Delcourt <delcourt.simon@gmail.com>
2019-07-10 11:14:18 +0200

    Stronger check to verify if config elements are vocterms before caching them.

--- src/Products/urban/browser/urban_vocabularies.py
+++ src/Products/urban/browser/urban_vocabularies.py
@@ -4,6 +4,8 @@ from collections import OrderedDict
 
 from Products.Five import BrowserView
 
+from Products.urban.interfaces import IUrbanConfigurationValue
+
 from zope.annotation import IAnnotations
 
 
@@ -48,8 +50,9 @@ class UrbanVocabulariesCache(BrowserView):
     def voc_folder_to_vocabulary_list(self, folder):
         vocabulary_list = []
         for voc_term in folder.objectValues():
-            vocterm_dict = voc_term.to_dict()
-            vocabulary_list.append(vocterm_dict)
+            if IUrbanConfigurationValue.providedBy(voc_term):
+                vocterm_dict = voc_term.to_dict()
+                vocabulary_list.append(vocterm_dict)
         return vocabulary_list
 
     def reset_all_cache(self):
