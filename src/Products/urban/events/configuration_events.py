18cc6b9857aece71c6f865ba31bd13afabc629bf
Julien Jaumotte <julien.jaumotte@imio.be>
2019-09-10 16:01:38 +0200

    Add remove check on Streets in configuration

--- src/Products/urban/events/configuration_events.py
+++ src/Products/urban/events/configuration_events.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from plone import api
+from zope.globalrequest import getRequest
+from zope.i18n import translate
+from OFS.ObjectManager import BeforeDeleteException
+from Products.urban.interfaces import IGenericLicence
+from Products.urban import UrbanMessage as _
 
 
 def update_vocabulary_term_cache(config_obj, event):
@@ -18,3 +23,28 @@ def update_vocabulary_folder_cache(voc_folder, event):
     with api.env.adopt_roles(['Manager']):
         cache_view = portal_urban.restrictedTraverse('urban_vocabulary_cache')
         cache_view.update_procedure_vocabulary_cache(config_folder, voc_folder)
+
+
+def before_street_delete(current_street, event):
+    """
+        Checks if the street can be deleted
+    """
+
+    if event.object.meta_type == 'Plone Site':
+        return
+
+    street_uid = current_street.UID()
+    request = getRequest()
+    catalog = api.portal.get_tool('portal_catalog')
+    licence_brains = catalog(object_provides=IGenericLicence.__identifier__)
+    licences = [l.getObject() for l in licence_brains if IGenericLicence.providedBy(l.getObject())]
+    for licence in licences:
+        address = licence.getWorkLocations()
+        for wl in address:
+            if wl['street'] == street_uid:
+                raise BeforeDeleteException(u"{} {}"
+                                            .format(translate(_(u"can_not_delete_street_in_config"),
+                                                              domain='urban',
+                                                              context=request)
+                                                    ,
+                                                    " : {}".format(licence.reference)))
