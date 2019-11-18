f986817498b22bd3f1b7637ef2fd61c9e6f7fa54
Simon Delcourt <delcourt.simon@gmail.com>
2019-11-18 11:13:37 +0100

    Fix display of licences bound tickets.

--- src/Products/urban/events/ticketEvents.py
+++ src/Products/urban/events/ticketEvents.py
@@ -7,17 +7,16 @@ from zope.annotation import IAnnotations
 
 def setTicketBoundLicence(licence, event):
     annotations = IAnnotations(licence)
-    previous_bound_UID = annotations.get('urban.ticket_bound_licence', set([]))
-    new_bound_UID = licence.getField('bound_licence').getRaw(licence)
-    if previous_bound_UID == new_bound_UID:
+    previous_bound_UIDs = annotations.get('urban.ticket_bound_licences', set([]))
+    new_bound_UIDs = licence.getField('bound_licences').getRaw(licence)
+    if set(previous_bound_UIDs) == set(new_bound_UIDs):
         return
 
     catalog = api.portal.get_tool('portal_catalog')
     # unrefer previous licence
-    if previous_bound_UID:
-        previous_licence = catalog(UID=previous_bound_UID)
-        previous_licence = previous_licence and previous_licence[0].getObject()
-        if previous_licence:
+    if previous_bound_UIDs:
+        for previous_licence_brain in catalog(UID=previous_bound_UIDs):
+            previous_licence = previous_licence_brain.getObject()
             previous_licence_annotations = IAnnotations(previous_licence)
             values = previous_licence_annotations.get('urban.bound_tickets', set([]))
             if licence.UID() in values:
@@ -25,14 +24,13 @@ def setTicketBoundLicence(licence, event):
                 previous_licence_annotations['urban.bound_tickets'] = values
 
     # refer new licence
-    if new_bound_UID:
-        new_licence = catalog(UID=new_bound_UID)
-        new_licence = new_licence and new_licence[0].getObject()
-        if new_licence:
+    if new_bound_UIDs:
+        for new_licence_brain in catalog(UID=previous_bound_UIDs):
+            new_licence = new_licence_brain.getObject()
             new_licence_annotations = IAnnotations(new_licence)
             values = new_licence_annotations.get('urban.bound_tickets', set([]))
             if licence.UID() not in values:
                 values.add(licence.UID())
                 new_licence_annotations['urban.bound_tickets'] = values
 
-    annotations['urban.ticket_bound_licence'] = new_bound_UID
+    annotations['urban.ticket_bound_licences'] = new_bound_UIDs
