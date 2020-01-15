77789f6061afffcd4ef57a2c0d2194ce559433a6
Simon Delcourt <delcourt.simon@gmail.com>
2020-01-15 16:12:03 +0100

    Add rubrics automatic sorting.

--- src/Products/urban/events/rubricTermEvents.py
+++ src/Products/urban/events/rubricTermEvents.py
@@ -4,3 +4,16 @@ def updateTitle(rubricterm, event):
      Update title after each change
     """
     rubricterm.updateTitle()
+
+
+def updateIdAndSort(rubricterm, event):
+    """
+     Update id after each change and re-sort the rubrics (if needed).
+    """
+    rubricterm.id = rubricterm.getNumber()
+    folder = rubricterm.aq_parent
+    sorted_ids = sorted(folder.objectIds())
+    for index in range(len(sorted_ids)):
+        rurbric_id = sorted_ids[index]
+        if folder.getObjectPosition(rurbric_id) != index:
+            folder.moveObject(rurbric_id, index)
