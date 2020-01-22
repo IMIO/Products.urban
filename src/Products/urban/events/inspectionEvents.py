d9506a1526de8b6272be6b12c65679d6019a8125
Simon Delcourt <delcourt.simon@gmail.com>
2020-01-22 13:41:56 +0100

    Better fallback in case of empty annotation.

--- src/Products/urban/events/inspectionEvents.py
+++ src/Products/urban/events/inspectionEvents.py
@@ -7,7 +7,7 @@ from zope.annotation import IAnnotations
 
 def setInspectionBoundLicence(licence, event):
     annotations = IAnnotations(licence)
-    previous_bound_UID = list(annotations.get('urban.inspection_bound_licence', set([])))
+    previous_bound_UID = list(annotations.get('urban.inspection_bound_licence') or set([]))
     new_bound_UID = licence.getField('bound_licence').getRaw(licence)
     if set(previous_bound_UID) == set(new_bound_UID):
         return
@@ -19,7 +19,7 @@ def setInspectionBoundLicence(licence, event):
         previous_licence = previous_licence and previous_licence[0].getObject()
         if previous_licence:
             previous_licence_annotations = IAnnotations(previous_licence)
-            values = previous_licence_annotations.get('urban.bound_inspections', set([]))
+            values = previous_licence_annotations.get('urban.bound_inspections') or set([])
             if licence.UID() in values:
                 values.remove(licence.UID())
                 previous_licence_annotations['urban.bound_inspections'] = values
@@ -30,7 +30,7 @@ def setInspectionBoundLicence(licence, event):
         new_licence = new_licence and new_licence[0].getObject()
         if new_licence:
             new_licence_annotations = IAnnotations(new_licence)
-            values = new_licence_annotations.get('urban.bound_inspections', set([]))
+            values = new_licence_annotations.get('urban.bound_inspections') or set([])
             if licence.UID() not in values:
                 values.add(licence.UID())
                 new_licence_annotations['urban.bound_inspections'] = values
