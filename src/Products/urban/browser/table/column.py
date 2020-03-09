99b160311f3613076f2018176521b14fe74f928f
Simon Delcourt <delcourt.simon@gmail.com>
2020-03-09 16:14:25 +0100

    Fix encoding error.

--- src/Products/urban/browser/table/column.py
+++ src/Products/urban/browser/table/column.py
@@ -209,7 +209,7 @@ class ContacTitleDisplay(TitleDisplay):
 
     def render(self):
         contact = self.obj
-        title = self.column.renderTitleLink(self.urbanlist_item)
+        title = self.column.renderTitleLink(self.urbanlist_item).encode('utf-8')
 
         address = ''
         street = contact.getStreet()
