0cc95c7762bf3ee77638487fe6378e3fc7d711cf
Julien Jaumotte <julien.jaumotte@imio.be>
2020-03-09 15:46:00 +0100

    Fix licence type folder names for article127
    The 220 list export no longer appeared

--- src/Products/urban/viewlets/urbain_220.py
+++ src/Products/urban/viewlets/urbain_220.py
@@ -37,13 +37,13 @@ class Urbain220Viewlet(ViewletBase):
         """
         allowed_contexts = [
             'urban',
-            'articles127s',
+            'article127s',
             'buildlicences',
             'declarations',
             'integratedlicences',
             'uniquelicences',
             'preliminarynotices',
-            'codt_articles127s',
+            'codt_article127s',
             'codt_buildlicences',
             'codt_integratedlicences',
             'codt_uniquelicences',
