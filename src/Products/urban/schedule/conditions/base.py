28508ac5068b006cc7199d9b4c08e669fa2b89be
Simon Delcourt <delcourt.simon@gmail.com>
2020-03-12 14:37:07 +0100

    URBLIE-372. Move schedule conditions from liege.urban.

--- src/Products/urban/schedule/conditions/base.py
+++ src/Products/urban/schedule/conditions/base.py
@@ -23,3 +23,23 @@ class BaseInspection:
         licence = self.task_container
         followup_events = licence.getCurrentFollowUpEvents()
         return followup_events
+
+    def get_current_followup_ticket(self):
+        licence = self.task_container
+        tickets = licence.getBoundTickets()
+        if not tickets:
+            return None
+
+        report = self.get_current_inspection_report()
+        # check the current report with 'ticket' in the followup propositions
+        if 'ticket' in report.getFollowup_proposition():
+            report_workflow_history = report.workflow_history.values()[0]
+            report_creation_date = report_workflow_history[0]['time']
+            # if a referring ticket has been created after this report
+            # return True
+            for ticket in tickets:
+                ticket_workflow_history = ticket.workflow_history.values()[0]
+                ticket_creation_date = ticket_workflow_history[0]['time']
+                if ticket_creation_date > report_creation_date:
+                    return ticket
+                return None
