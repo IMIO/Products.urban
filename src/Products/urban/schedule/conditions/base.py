3f781e49fa05b05c3eaad017094c3e83ab2b7192
Simon Delcourt <delcourt.simon@gmail.com>
2020-03-13 09:57:42 +0100

    Typo.

--- src/Products/urban/schedule/conditions/base.py
+++ src/Products/urban/schedule/conditions/base.py
@@ -24,22 +24,24 @@ class BaseInspection:
         followup_events = licence.getCurrentFollowUpEvents()
         return followup_events
 
-    def get_current_followup_ticket(self):
+    def get_last_followup_ticket(self):
         licence = self.task_container
         tickets = licence.getBoundTickets()
         if not tickets:
             return None
 
-        report = self.get_current_inspection_report()
-        # check the current report with 'ticket' in the followup propositions
-        if 'ticket' in report.getFollowup_proposition():
-            report_workflow_history = report.workflow_history.values()[0]
-            report_creation_date = report_workflow_history[0]['time']
-            # if a referring ticket has been created after this report
-            # return True
-            for ticket in tickets:
-                ticket_workflow_history = ticket.workflow_history.values()[0]
-                ticket_creation_date = ticket_workflow_history[0]['time']
-                if ticket_creation_date > report_creation_date:
-                    return ticket
-                return None
+        report_events = licence.getAllReportEvents()[::-1]
+        # check the most recent report with 'ticket' in the followup
+        # proposition
+        for report in report_events:
+            if 'ticket' in report.getFollowup_proposition():
+                report_workflow_history = report.workflow_history.values()[0]
+                report_creation_date = report_workflow_history[0]['time']
+                # if a referring ticket has been created after this report
+                # return True
+                for ticket in tickets:
+                    ticket_workflow_history = ticket.workflow_history.values()[0]
+                    ticket_creation_date = ticket_workflow_history[0]['time']
+                    if ticket_creation_date > report_creation_date:
+                        return ticket
+                    return None
