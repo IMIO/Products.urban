c5cfa8342fb5be77f29c3c5d617d05b7a3c6e319
Simon Delcourt <delcourt.simon@gmail.com>
2020-05-27 11:40:57 +0200

    Fix external editor settings + upgrade step.

--- src/Products/urban/migration/update_240.py
+++ src/Products/urban/migration/update_240.py
@@ -20,3 +20,26 @@ def fix_licences_breadcrumb(context):
         collection = getattr(folder, collection_id)
         _updateDefaultCollectionFor(folder, collection.UID())
     logger.info("upgrade done!")
+
+
+def fix_external_edition_settings(context):
+    logger = logging.getLogger('urban: fix external edition settings')
+    logger.info("starting upgrade steps")
+
+    values = api.portal.get_registry_record('externaleditor.externaleditor_enabled_types')
+    if 'UrbanDoc' not in values:
+        values.append('UrbanDoc')
+    if 'UrbanTemplate' not in values:
+        values.append('UrbanTemplate')
+    if 'ConfigurablePODTemplate' not in values:
+        values.append('ConfigurablePODTemplate')
+    if 'SubTemplate' not in values:
+        values.append('SubTemplate')
+    if 'StyleTemplate' not in values:
+        values.append('StyleTemplate')
+    if 'DashboardPODTemplate' not in values:
+        values.append('DashboardPODTemplate')
+    if 'MailingLoopTemplate' not in values:
+        values.append('MailingLoopTemplate')
+    api.portal.set_registry_record('externaleditor.externaleditor_enabled_types', values)
+    logger.info("upgrade done!")
