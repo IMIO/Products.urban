a0213ee8a3b2d3065ec2ec2e78737dc39c8c2f80
Julien Jaumotte <julien.jaumotte@imio.be>
2020-06-12 16:06:30 +0200

    Move migration utils and view to specific to_DX folder
    Fix urban_vocabulary_cache scope
    Use logger instead of print for utilsview output
    Add utilsview log type by default

--- src/Products/urban/migration/to_DX/utilsview.py
+++ src/Products/urban/migration/to_DX/utilsview.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 
+import logging
+
 from datetime import datetime
 from plone import api
 
 from Products.Five import BrowserView
+
+logger = logging.getLogger('urban: migrations utils')
+
 
 class PortalTypesInfosView(BrowserView):
 
@@ -13,19 +18,22 @@ class PortalTypesInfosView(BrowserView):
 
     def __call__(self):
         catalog = api.portal.get_tool('portal_catalog')
-        type = self.request.form['type']
+        if 'type' in self.request.form:
+            type = self.request.form['type']
+        else:
+            type = 'log'
         portal_type = self.request.form['portal_type']
         if type and portal_type:
             portal_types = [brain.getObject() for brain in catalog(portal_type=portal_type)]
 
             if portal_types:
-                if type == 'print':
+                if type == 'log':
                     for portal_type in portal_types:
-                        print(portal_type.absolute_url())
-                    print("Portal type found : {}".format(len(portal_types)))
+                        logger.info(portal_type.absolute_url())
+                    logger.info("Portal type found : {}".format(len(portal_types)))
                 if type == 'csv':
                     with open("{}_{}.csv".format(datetime.today().strftime('%Y_%m_%d_%H_%M_%S'),portal_type), "a") as file:
                         for portal_type in portal_types:
                             file.write(portal_type.absolute_url() + "\n")
             else:
-                print("Portal type not found in linked catalog")
+                logger.info("Portal type not found in linked catalog")
