36268800546edeef9f38fc40a0c74a79024cb945
Simon Delcourt <delcourt.simon@gmail.com>
2020-06-23 14:38:38 +0200

    Mark ILicencePortionOut interface to be removed.

--- src/Products/urban/interfaces.py
+++ src/Products/urban/interfaces.py
@@ -654,7 +654,7 @@ class IUrbanDoc(Interface):
     """Marker interface for generated document.
     """
 
-
+# [DX] TO DELETE > 2.5 [DX]
 class ILicencePortionOut(Interface):
     """Marker interface for portionOut in a licence.
     """
