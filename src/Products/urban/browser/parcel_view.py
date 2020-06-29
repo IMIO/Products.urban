390ccfcd5c6ea14ae327f76e93b67c2caa25d3da
Simon Delcourt <delcourt.simon@gmail.com>
2020-06-29 14:08:40 +0200

    Default parcel view redirects to its container. Add an overlay display view for z3c.table parcels listing.

--- src/Products/urban/browser/parcel_view.py
+++ src/Products/urban/browser/parcel_view.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from plone.dexterity.browser import edit
-
-# from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone.dexterity.browser import view
 
 
 class ParcelEditForm(edit.DefaultEditForm):
@@ -18,5 +17,27 @@ class ParcelEditForm(edit.DefaultEditForm):
         self.request.set('disable_plone.rightcolumn', 1)
         self.request.set('disable_plone.leftcolumn', 1)
 
-#    def render(self):
-#        return ViewPageTemplateFile("templates/parcel_edit.pt")(self)
+
+class ParcelView(view.DefaultView):
+    """
+    Parcel display view to be called with an overlay in z3c.table listings.
+    """
+
+    def __init__(self, context, request):
+        super(ParcelView, self).__init__(context, request)
+        self.context = context
+        self.request = request
+        # disable portlets on licences
+        self.request.set('disable_plone.rightcolumn', 1)
+        self.request.set('disable_plone.leftcolumn', 1)
+
+
+class ParcelViewRedirects(view.DefaultView):
+    """
+    Parcel default view redirects to the parent container.
+    """
+
+    def __call__(self):
+        return self.context.REQUEST.RESPONSE.redirect(
+            self.context.aq_parent.absolute_url()
+        )
