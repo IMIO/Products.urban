fd3781c976dbaffd77747b402a8e3a5811e6ba08
Simon Delcourt <delcourt.simon@gmail.com>
2020-07-08 16:03:45 +0200

    SUP-13679. Add migration step.

--- src/Products/urban/migration/update_240.py
+++ src/Products/urban/migration/update_240.py
@@ -43,3 +43,13 @@ def fix_external_edition_settings(context):
         values.append('MailingLoopTemplate')
     api.portal.set_registry_record('externaleditor.externaleditor_enabled_types', values)
     logger.info("upgrade done!")
+
+
+def fix_add_inquiry_permissions(context):
+    logger = logging.getLogger('urban: fix add Inquiry permissions for CODT_UniqueLicence and CODT_IntegratedLicence')
+    setup_tool = api.portal.get_tool('portal_setup')
+    setup_tool.runImportStepFromProfile('profile-Products.urban:preinstall', 'update-workflow-rolemap')
+    workflow_tool = api.portal.get_tool('portal_setup')
+    workflow_tool.updateRoleMappings()
+    logger.info("starting upgrade steps")
+    logger.info("upgrade done!")
