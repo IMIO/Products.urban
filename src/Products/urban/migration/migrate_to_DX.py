a68a304d46cd5f0bd78b13fd2849e49398149b40
Simon Delcourt <delcourt.simon@gmail.com>
2020-07-23 14:34:04 +0200

    Complete parcelling migration to preserve references.

--- src/Products/urban/migration/migrate_to_DX.py
+++ src/Products/urban/migration/migrate_to_DX.py
@@ -1,14 +1,13 @@
 # encoding: utf-8
 
+from plone import api
 from plone.app.contenttypes.migration.migration import migrateCustomAT
+
+from Products.urban.migration.to_DX.migration_utils import migrate_date
 
 
 def migrate_PortionOut_to_DX(context):
     fields_mapping = (
-        {
-            'AT_field_name': 'divisionCode',
-            'DX_field_name': 'divisionCode',
-        },
         {
             'AT_field_name': 'division',
             'DX_field_name': 'division',
@@ -51,4 +50,63 @@ def migrate_PortionOut_to_DX(context):
         src_type='PortionOut',
         dst_type='Parcel'
     )
+    return result
+
+
+def migrate_ParcellingTerm_to_DX(context):
+    # delete parcels in parcellings first
+    portal = api.portal.get()
+    parcellings_folder = portal.urban.parcellings
+    for parcelling in parcellings_folder.objectValues():
+        api.content.delete(objects=parcelling.objectValues())
+
+    fields_mapping = (
+        {
+            'AT_field_name': 'label',
+            'DX_field_name': 'label',
+        },
+        {
+            'AT_field_name': 'subdividerName',
+            'DX_field_name': 'subdividerName',
+        },
+        {
+            'AT_field_name': 'authorizationDate',
+            'DX_field_name': 'authorizationDate',
+            'field_migrator': migrate_date,
+        },
+        {
+            'AT_field_name': 'approvalDate',
+            'DX_field_name': 'approvalDate',
+            'field_migrator': migrate_date,
+        },
+        {
+            'AT_field_name': 'communalReference',
+            'DX_field_name': 'communalReference',
+        },
+        {
+            'AT_field_name': 'DGO4Reference',
+            'DX_field_name': 'DGO4Reference',
+        },
+        {
+            'AT_field_name': 'numberOfParcels',
+            'DX_field_name': 'numberOfParcels',
+        },
+        {
+            'AT_field_name': 'changesDescription',
+            'DX_field_name': 'changesDescription',
+        },
+    )
+    result = migrateCustomAT(
+        fields_mapping,
+        src_type='ParcellingTerm',
+        dst_type='Parcelling'
+    )
+
+    # should at least recatalog them in the archetypes UID catalog
+    uid_catalog = api.portal.get_tool('uid_catalog')
+    portal = api.portal.get()
+    parcellings = portal.urban.parcellings.objectValues()
+    for parcelling in parcellings:
+        uid_catalog.catalog_object(parcelling, '/'.join(parcelling.getPhysicalPath()))
+
     return result
