f5ac1fecac01db4f6dc2d42c5ba74ac5a220a4a4
Simon Delcourt <delcourt.simon@gmail.com>
2020-10-15 10:57:11 +0200

    Remove useless __init__ .

--- src/Products/urban/content/licence/GenericLicence.py
+++ src/Products/urban/content/licence/GenericLicence.py
@@ -34,6 +34,8 @@ from Products.DataGridField import DataGridField, DataGridWidget
 from Products.DataGridField.Column import Column
 from Products.DataGridField.SelectColumn import SelectColumn
 
+from collective.plonefinder.browser.interfaces import IFinderUploadCapable
+from collective.quickupload.interfaces import IQuickUploadCapable
 from Products.urban.config import *
 from Products.urban import UrbanMessage as _
 
@@ -1023,7 +1025,9 @@ class GenericLicence(BaseFolder, UrbanBase, BrowserDefaultMixin):
     implements(
         interfaces.IGenericLicence,
         IFacetedTaskContainer,
-        IPossibleFacetedNavigable
+        IPossibleFacetedNavigable,
+        IFinderUploadCapable,
+        IQuickUploadCapable
     )
 
     meta_type = 'GenericLicence'
