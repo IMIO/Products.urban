6f238279abe0c40a48a2226e74827385415f6b70
Simon Delcourt <delcourt.simon@gmail.com>
2021-01-04 14:09:15 +0100

    Make sure to update voci cache: when the voc term is fully created.

--- src/Products/urban/events/configuration_events.py
+++ src/Products/urban/events/configuration_events.py
@@ -7,14 +7,16 @@ def update_vocabulary_term_cache(config_obj, event):
     portal_urban = api.portal.get_tool('portal_urban')
     voc_folder = config_obj.aq_parent
     config_folder = voc_folder.aq_parent
-    with api.env.adopt_roles(['Manager']):
-        cache_view = portal_urban.restrictedTraverse('urban_vocabulary_cache')
-        cache_view.update_procedure_vocabulary_cache(config_folder, voc_folder)
+    if config_folder.getid() != 'portal_factory':
+        with api.env.adopt_roles(['Manager']):
+            cache_view = portal_urban.restrictedTraverse('urban_vocabulary_cache')
+            cache_view.update_procedure_vocabulary_cache(config_folder, voc_folder)
 
 
 def update_vocabulary_folder_cache(voc_folder, event):
     portal_urban = api.portal.get_tool('portal_urban')
     config_folder = voc_folder.aq_parent
-    with api.env.adopt_roles(['Manager']):
-        cache_view = portal_urban.restrictedTraverse('urban_vocabulary_cache')
-        cache_view.update_procedure_vocabulary_cache(config_folder, voc_folder)
+    if config_folder.getid() != 'portal_factory':
+        with api.env.adopt_roles(['Manager']):
+            cache_view = portal_urban.restrictedTraverse('urban_vocabulary_cache')
+            cache_view.update_procedure_vocabulary_cache(config_folder, voc_folder)
