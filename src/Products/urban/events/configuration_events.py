c875951f41cd15f4c979ad5d45a8e246abef5ea2
Simon Delcourt <delcourt.simon@gmail.com>
2021-01-04 14:12:03 +0100

    Typo.

--- src/Products/urban/events/configuration_events.py
+++ src/Products/urban/events/configuration_events.py
@@ -7,7 +7,7 @@ def update_vocabulary_term_cache(config_obj, event):
     portal_urban = api.portal.get_tool('portal_urban')
     voc_folder = config_obj.aq_parent
     config_folder = voc_folder.aq_parent
-    if config_folder.getid() != 'portal_factory':
+    if config_folder.getId() != 'portal_factory':
         with api.env.adopt_roles(['Manager']):
             cache_view = portal_urban.restrictedTraverse('urban_vocabulary_cache')
             cache_view.update_procedure_vocabulary_cache(config_folder, voc_folder)
@@ -16,7 +16,7 @@ def update_vocabulary_term_cache(config_obj, event):
 def update_vocabulary_folder_cache(voc_folder, event):
     portal_urban = api.portal.get_tool('portal_urban')
     config_folder = voc_folder.aq_parent
-    if config_folder.getid() != 'portal_factory':
+    if config_folder.getId() != 'portal_factory':
         with api.env.adopt_roles(['Manager']):
             cache_view = portal_urban.restrictedTraverse('urban_vocabulary_cache')
             cache_view.update_procedure_vocabulary_cache(config_folder, voc_folder)
