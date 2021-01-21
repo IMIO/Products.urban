73ee0464a508593e46f94c9fbbcb746a57359a41
Simon Delcourt <delcourt.simon@gmail.com>
2021-01-21 16:20:23 +0100

    Fix test.

--- src/Products/urban/tests/test_streets.py
+++ src/Products/urban/tests/test_streets.py
@@ -2,42 +2,41 @@
 
 
 from OFS.ObjectManager import BeforeDeleteException
-from Products.urban.testing import URBAN_TESTS_CONFIG
-from Products.urban.tests.helpers import BrowserTestCase
+from Products.urban.testing import URBAN_TESTS_LICENCES_FUNCTIONAL
 
 from plone.app.testing import login
+from plone import api
+
+import unittest
 
 
 
 
-class TestUrbanStreets(BrowserTestCase):
+class TestUrbanStreets(unittest.TestCase):
 
-    layer = URBAN_TESTS_CONFIG
+    layer = URBAN_TESTS_LICENCES_FUNCTIONAL
 
     def setUp(self):
         site = self.layer['portal']
         self.site = site
         self.buildlicence = site.urban.buildlicences.objectValues()[-1]
         self.city = site.urban.portal_urban.streets.objectValues()[-1]
-        portal = self.layer['portal']
-        login(portal, 'urbanmanager')
+        self.portal = self.layer['portal']
 
     def test_streets_delete(self):
-            # create a street
-            self.city.invokeFactory('Street', id="streetId1", streetCode="666", streetName="Rue de la gare",
-                                                bestAddressKey="123456")
-            wl = self.buildlicence.getWorkLocations()
-            wll = list(wl)
-            wll.append({'street': self.city.streetId1.UID(), 'number': '123'})
-            # link licence to this first street
-            self.buildlicence.setWorkLocations(tuple(wll))
+        # create a street
+        wl = self.buildlicence.getWorkLocations()
+        wll = list(wl)
+        street_1 = self.city.objectValues()[0]
+        street_2 = self.city.objectValues()[1]
+        wll.append({'street': street_1.UID(), 'number': '123'})
+        # link licence to this first street
+        self.buildlicence.setWorkLocations(tuple(wll))
 
-            # street must not be deleted if linked to a licence
-            self.assertRaises(BeforeDeleteException, self.city.manage_delObjects, [self.city.streetId1.id])
+        # street must not be deleted if linked to a licence
+        login(self.portal, 'urbanmanager')
+        self.assertRaises(BeforeDeleteException, self.city.manage_delObjects, [street_1.id])
 
-            # create a street and don't link it to a licence
-            self.city.invokeFactory('Street', id="streetId2", streetCode="777", streetName="Rue de la suppression",
-                                                bestAddressKey="654321")
-            # delete this former street must not raise a BeforeDeleteException
-            self.city.manage_delObjects([self.city.streetId2.id])
-            print('end')
+        # delete this former street must not raise a BeforeDeleteException
+        self.city.manage_delObjects([street_2.id])
+        print('end')
