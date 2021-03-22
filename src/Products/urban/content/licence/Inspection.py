18310f22576c43a4dba57405ae773af88f4fcbd6
Simon Delcourt <delcourt.simon@gmail.com>
2021-03-22 14:39:28 +0100

    Typo.

--- src/Products/urban/content/licence/Inspection.py
+++ src/Products/urban/content/licence/Inspection.py
@@ -125,7 +125,7 @@ class Inspection(BaseFolder, GenericLicence, Inquiry, BrowserDefaultMixin):
            Update the title to clearly identify the licence
         """
         proprietary = ''
-        proprietaries = [pro for pro context.getProprietaries()
+        proprietaries = [pro for pro in context.getProprietaries()
                          if api.content.get_state(pro) =='enabled']
         if proprietaries:
             proprietary = proprietaries[0].Title()
