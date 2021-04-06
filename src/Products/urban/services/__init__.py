42f7a5cecc8845ebad8dde0914db4bb01caf4693
Simon Delcourt <delcourt.simon@gmail.com>
2021-04-06 10:20:02 +0200

    URB-1766. Do not set bestaddress connection when starting urban.

--- src/Products/urban/services/__init__.py
+++ src/Products/urban/services/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from Products.urban.config import ExternalConfig
-from Products.urban.services.bestaddress import BestaddressService
 from Products.urban.services.cadastral import CadastreService
 from Products.urban.services.parcel_coring import ParcelCoringService
 
@@ -13,5 +12,4 @@ except:
     config = {}
 
 cadastre = CadastreService(**(config_cadastre and config_cadastre.cadastre))
-bestaddress = BestaddressService(**(config_bestaddress and config_bestaddress.bestaddress))
 parcel_coring = ParcelCoringService(**(config_parcel_coring and config_parcel_coring.parcel_coring))
