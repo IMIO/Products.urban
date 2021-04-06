0f3139498813ad1f574fb7304306959a64a3854e
Simon Delcourt <delcourt.simon@gmail.com>
2021-04-06 11:43:11 +0200

    Remove unused var.

--- src/Products/urban/services/__init__.py
+++ src/Products/urban/services/__init__.py
@@ -6,7 +6,6 @@ from Products.urban.services.parcel_coring import ParcelCoringService
 
 try:
     config_cadastre = ExternalConfig('cadastre')
-    config_bestaddress = ExternalConfig('bestaddress')
     config_parcel_coring = ExternalConfig('parcel_coring')
 except:
     config = {}
