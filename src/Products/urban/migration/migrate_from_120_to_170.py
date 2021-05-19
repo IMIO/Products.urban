368de8e7a1baf2defe7556899dbeb29a92005c10
Julien Jaumotte <julien.jaumotte@imio.be>
2021-05-19 17:03:02 +0200

    Add 1.7 step in global upgrade step

--- src/Products/urban/migration/migrate_from_120_to_170.py
+++ src/Products/urban/migration/migrate_from_120_to_170.py
@@ -2,8 +2,12 @@
 
 import logging
 
+from plone import api
+
+from Products.urban.migration import migrate_to_1110
 from Products.urban.migration.migrate_to_130 import migrateToUrban130
 from Products.urban.migration.migrate_to_160 import migrateToUrban160
+from Products.urban.migration.migrate_to_170 import migrateToUrban170
 
 logger = logging.getLogger('urban: migrations')
 
@@ -16,10 +20,14 @@ def contentmigrationLogger(oldObject, **kwargs):
 
 def migrate(context):
     """
-     Launch every migration steps for the version 1.6.0 from 1.2.0
+     Launch every migration steps for the version 1.11.0 from 1.2.0
     """
     logger = logging.getLogger('urban: migrate to 1.6.0')
     logger.info("starting global migration steps")
     migrateToUrban130(context)
     migrateToUrban160(context)
+    migrateToUrban170(context)  # 1h
+    # migrate_to_1110.migrate(context)  # 1h 50m
+    catalog = api.portal.get_tool('portal_catalog')
+    catalog.refreshCatalog(clear=True)  # 20m
     logger.info("global migration done!")
