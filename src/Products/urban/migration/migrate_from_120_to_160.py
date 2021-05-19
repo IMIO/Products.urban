b8cdab334d5205f87dd00d7594541dc43fea44a2
Julien Jaumotte <julien.jaumotte@imio.be>
2021-05-19 09:43:50 +0200

    Define multi-version upgrade step

---
+++ src/Products/urban/migration/migrate_from_120_to_160.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+import logging
+
+from Products.urban.migration.migrate_to_130 import migrateToUrban130
+from Products.urban.migration.migrate_to_160 import migrateToUrban160
+
+logger = logging.getLogger('urban: migrations')
+
+
+def contentmigrationLogger(oldObject, **kwargs):
+    """ Generic logger method to be used with CustomQueryWalker """
+    kwargs['logger'].info('/'.join(kwargs['purl'].getRelativeContentPath(oldObject)))
+    return True
+
+
+def migrateFromUrban120ToUrban160(context):
+    """
+     Launch every migration steps for the version 1.6.0 from 1.2.0
+    """
+    logger = logging.getLogger('urban: migrate to 1.6.0')
+    logger.info("starting global migration steps")
+    migrateToUrban130(context)
+    migrateToUrban160(context)
+    logger.info("global migration done!")
