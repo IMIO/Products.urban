ee19a1487742511bdbb496784f188e16d6c8926c
sdelcourt <simon.delcourt@imio.be>
2021-06-28 15:31:15 +0200

    URB-2252. Add update step to duplicate the misclicence event configs to inspection.

--- src/Products/urban/migration/update_250.py
+++ src/Products/urban/migration/update_250.py
@@ -12,3 +12,15 @@ def add_new_default_personTitle(context):
     portal_setup = api.portal.get_tool('portal_setup')
     portal_setup.runImportStepFromProfile('profile-Products.urban:extra', 'urban-extraPostInstall')
     logger.info("upgrade done!")
+
+
+def copy_MiscLicence_cfg_to_Inspection(context):
+    """
+    """
+    logger = logging.getLogger('urban: add new default personTitle')
+    logger.info("starting upgrade steps")
+    portal_urban = api.portal.get_tool('portal_urban')
+    logger.info("duplicate event configs")
+    for eventconfig in portal_urban.miscdemand.eventconfigs.objectValues():
+        api.content.copy(eventconfig, portal_urban.inspection.eventconfigs)
+    logger.info("upgrade done!")
