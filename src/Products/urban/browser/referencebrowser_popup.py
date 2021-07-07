107d6ca760269b4cd5ab0208d5d4c051536db382
MagDhyIm <magali.dhyne@imio.be>
2021-07-07 17:08:47 +0200

    use template factory for notary and add method for common template

--- src/Products/urban/browser/referencebrowser_popup.py
+++ src/Products/urban/browser/referencebrowser_popup.py
@@ -9,7 +9,17 @@ from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 architect_popup_template = utils.named_template_adapter(
     ViewPageTemplateFile('templates/architect_popup.pt'))
 
+notary_popup_template = utils.named_template_adapter(
+    ViewPageTemplateFile('templates/notary_popup.pt'))
+
 
 class UrbanReferenceBrowserPopup(ReferenceBrowserPopup):
     """
     """
+    def get_creation_url(self):
+        url = 'http://localhost:8081/Plone/urban/{}/createObject'.format(self.context.id)
+        return url
+
+    def get_type_name_value(self):
+        val = '{}'.format(''.join(self.context.immediatelyAddableTypes).encode('utf-8'))
+        return val
