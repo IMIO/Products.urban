801ba705b16278dbbffbb022b92417cebe136923
sdelcourt <simon.delcourt@imio.be>
2021-08-10 13:34:56 +0200

    Copy inquiry fields of bound licence during roaddecree creation.

--- src/Products/urban/events/roaddecreeEvents.py
+++ src/Products/urban/events/roaddecreeEvents.py
@@ -5,10 +5,30 @@ from plone import api
 from zope.annotation import IAnnotations
 
 
+def copy_bound_licence_inquiry_fields(roaddecree, event):
+    """
+    Copy inquiry fields if theres a bound licence set on creation.
+    """
+    catalog = api.portal.get_tool('portal_catalog')
+    bound_UIDs = roaddecree.getField('bound_licence').getRaw(roaddecree) or []
+    brains = catalog(UID=bound_UIDs)
+    if brains:
+        bound_licence = brains[0].getObject()
+        inquiry = bound_licence.getAllInquiries()[-1]
+        fields = roaddecree.schema.getSchemataFields('urban_inquiry')
+        for original_field in fields:
+            destination_field = inquiry.getField(original_field.getName())
+            if destination_field:
+                destination_mutator = destination_field.getMutator(roaddecree)
+                value = original_field.getAccessor(inquiry)()
+                destination_mutator(value)
+
+
 def setRoadDecreeBoundLicence(roaddecree, event):
     annotations = IAnnotations(roaddecree)
     previous_bound_UIDs = list(annotations.get('urban.roaddecree_bound_licence') or set([]))
-    new_bound_UIDs = [roaddecree.getField('bound_licence').getRaw(roaddecree)] or []
+    new_bound_UIDs = roaddecree.getField('bound_licence').getRaw(roaddecree)
+    new_bound_UIDs = new_bound_UIDs and [new_bound_UIDs] or []
     if set(previous_bound_UIDs) == set(new_bound_UIDs):
         return
 
