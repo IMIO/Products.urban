8ef1b0df009e787822edf1f58cf899d15fef57a2
sdelcourt <simon.delcourt@imio.be>
2021-12-02 15:07:19 +0100

    Update selector fun syntax for notary field edit overlay.

--- src/Products/urban/skins/urban_styles/fieldeditoverlay.js
+++ src/Products/urban/skins/urban_styles/fieldeditoverlay.js
@@ -1,23 +1,23 @@
 function getField() {
-        jq('[id^=pb_] [id^=fieldtoreplace_]').each(function(index) {
-            here = jq(this)
+        $('[id^=pb_] [id^=fieldtoreplace_]').each(function(index) {
+            here = $(this)
             field_id = here.attr('class');
-            oldcontent = jq('#archetypes-fieldname-' + field_id);
+            oldcontent = $('#archetypes-fieldname-' + field_id);
 
             // use this trick to get fields hidden by master select widget
             if (oldcontent.hasClass('refreshed')){
                 newcontent = oldcontent.clone();
                 // copy/paste the 'select' property manually since the clone method is bugged ..
                 var selects = oldcontent.find("select");
-                jq(selects).each(function(i) {
+                $(selects).each(function(i) {
                     var select = this;
-                    jq(newcontent).find("select").eq(i).val(jq(select).val());
+                    $(newcontent).find("select").eq(i).val($(select).val());
                 });
                 //finally  do the replace
                 here.replaceWith(newcontent);
             }
             else {
-                loaded_field = jq('#' + here.attr('id') + ' div:first');
+                loaded_field = $('#' + here.attr('id') + ' div:first');
                 loaded_field.addClass('refreshed')
             }
         });
@@ -28,13 +28,13 @@ function updateField(field_ids) {
         for (var i=0; i < field_ids.length; i++)
         {
             field_id = field_ids[i];
-            newcontent = jq('[id^=pb_] #archetypes-fieldname-' + field_id);
-            jq('#archetypes-fieldname-' + field_id).replaceWith(newcontent);
+            newcontent = $('[id^=pb_] #archetypes-fieldname-' + field_id);
+            $('#archetypes-fieldname-' + field_id).replaceWith(newcontent);
         }
         closePopup();
 };
 
 function closePopup() {
-        overlay = jq('[id^=pb_]').data('overlay');
+        overlay = $('[id^=pb_]').data('overlay');
         overlay.close();
 };
