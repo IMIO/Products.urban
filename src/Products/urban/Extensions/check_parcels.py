7491589488a478998488ecfb675d9a09984b8580
sdelcourt <simon.delcourt@imio.be>
2021-12-22 16:04:34 +0100

    SUP-21326. Add script to fix wrong parcel divisions.

--- src/Products/urban/Extensions/check_parcels.py
+++ src/Products/urban/Extensions/check_parcels.py
@@ -2,7 +2,7 @@ from plone import api
 from Products.urban.interfaces import IGenericLicence
 
 
-def check():
+def remove_invalid_parcels():
     catalog = api.portal.get_tool('portal_catalog')
     licence_brains = catalog(object_provides=IGenericLicence.__identifier__)
     licences = [l.getObject() for l in licence_brains if IGenericLicence.providedBy(l.getObject())]
@@ -29,3 +29,24 @@ def check():
     for licence in invalid_licences:
         log.write(licence.Title() + '\n')
     log.close()
+
+
+def fix_parcels_division():
+    catalog = api.portal.get_tool('portal_catalog')
+    portal_urban = api.portal.get_tool('portal_urban')
+    divisions_by_name = dict([(row['name'], row['division']) for row in portal_urban.getDivisionsRenaming()])
+    encoded_divisions_by_name = dict([(row['name'].decode('utf-8'), row['division']) for row in portal_urban.getDivisionsRenaming()])
+    divisions_by_name.update(encoded_divisions_by_name)
+
+    wrong_values = [v for v in catalog.Indexes['parcelInfosIndex'].uniqueValues() if v and not v[0].isdigit()]
+    licence_brains = catalog(object_provides=IGenericLicence.__identifier__, parcelInfosIndex=wrong_values)
+
+    for licence_brain in licence_brains:
+        licence = licence_brain.getObject()
+        for parcel in licence.getParcels():
+            if not parcel.division.isdigit() and parcel.division in divisions_by_name:
+                print "fixed parcel %s" % parcel.capakey
+                parcel.division = str(divisions_by_name[parcel.division])
+            elif not parcel.division.isdigit():
+                print "COULD NOT fix parcel %s" % parcel.capakey
+        licence.reindexObject(idxs=['parcelInfosIndex'])
