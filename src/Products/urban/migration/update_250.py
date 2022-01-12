8251b840deb5f683eeedcdce16ee3ea2e4fbeacc
sdelcourt <simon.delcourt@imio.be>
2022-01-12 09:38:25 +0100

    Handle case of delegated template.

--- src/Products/urban/migration/update_250.py
+++ src/Products/urban/migration/update_250.py
@@ -265,7 +265,7 @@ def fix_PODTemplates_empty_filename(context):
         # odt_file can be stored in tuples
         if template.odt_file and hasattr(template.odt_file, '__iter__'):
             template.odt_file = template.odt_file[0]
-        if not template.odt_file.filename:
+        if template.odt_file and not template.odt_file.filename:
             template_id = template.id
             if type(template_id) is str:
                 template_id = template_id.decode('utf-8')
