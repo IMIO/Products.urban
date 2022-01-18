5b04cd8703a5d0e0c4173eb3254dd95db2fe7fb3
MagDhyIm <magali.dhyne@imio.be>
2022-01-18 14:21:52 +0100

    add connexion gig db methods and services

--- src/Products/urban/services/__init__.py
+++ src/Products/urban/services/__init__.py
@@ -3,13 +3,21 @@
 from Products.urban.config import ExternalConfig
 from Products.urban.services.cadastral import CadastreService
 from Products.urban.services.parcel_coring import ParcelCoringService
+from Products.urban.services.gig import GigService
+
+
+
 
 try:
     config_cadastre = ExternalConfig('cadastre')
     config_bestaddress = ExternalConfig('bestaddress')
     config_parcel_coring = ExternalConfig('parcel_coring')
+    config_gig = ExternalConfig('gig')
+    config_northwind = ExternalConfig('northwind')
 except:
     config = {}
 
 cadastre = CadastreService(**(config_cadastre and config_cadastre.cadastre))
 parcel_coring = ParcelCoringService(**(config_parcel_coring and config_parcel_coring.parcel_coring))
+# gig = GigService(**(config_gig and config_gig.gig))
+gig = GigService(**(config_northwind and config_northwind.northwind))
