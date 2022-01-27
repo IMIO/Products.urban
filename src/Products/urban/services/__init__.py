4d558366306a09de4228c29eb38e149ea5162d5e
MagDhyIm <magali.dhyne@imio.be>
2022-01-27 13:04:24 +0100

    replace config northwind by gig

--- src/Products/urban/services/__init__.py
+++ src/Products/urban/services/__init__.py
@@ -6,18 +6,16 @@ from Products.urban.services.parcel_coring import ParcelCoringService
 from Products.urban.services.gig import GigService
 
 
-
-
 try:
     config_cadastre = ExternalConfig('cadastre')
     config_bestaddress = ExternalConfig('bestaddress')
     config_parcel_coring = ExternalConfig('parcel_coring')
     config_gig = ExternalConfig('gig')
-    config_northwind = ExternalConfig('northwind')
+#    config_northwind = ExternalConfig('northwind')
 except:
     config = {}
 
 cadastre = CadastreService(**(config_cadastre and config_cadastre.cadastre))
 parcel_coring = ParcelCoringService(**(config_parcel_coring and config_parcel_coring.parcel_coring))
-# gig = GigService(**(config_gig and config_gig.gig))
-gig = GigService(**(config_northwind and config_northwind.northwind))
+gig = GigService(**(config_gig and config_gig.gig))
+# gig = GigService(**(config_northwind and config_northwind.northwind))
