d776d686b9ba85651978c0d8b4e10d6e33d41d15
MagDhyIm <magali.dhyne@imio.be>
2022-01-27 08:43:31 +0100

    add query in string

--- src/Products/urban/services/gig.py
+++ src/Products/urban/services/gig.py
@@ -2,6 +2,7 @@
 
 from Products.urban.services.mysqlbase import MySQLService
 from Products.urban.services.mysqlbase import MySQLSession
+from datetime import datetime
 
 
 class GigService(MySQLService):
@@ -25,20 +26,28 @@ class GigSession(MySQLSession):
         filenis = '/srv/instances/server_urban25/var/urban/urbanmap.cfg'
         with open(filenis, 'r') as f:
             lines = f.readlines()
-        nis = lines[1][4:]
-        rec_test = "SELECT * FROM customers WHERE id = 29;"
-        query = session.execute(rec_test)
-        session.commit()
-        result = query.fetchall()
-#        rec_i = "INSERT INTO customers ('company', 'last_name', 'first_name', 'email_address', 'job_title', 'business_phone', 'home_phone', 'mobile_phone', 'fax_number', 'address', 'city', 'state_province', 'zip_postal_code', 'country_region', 'web_page', 'notes', 'attachements') VALUES ('Company DD', 'Cornick', 'Anna', None, 'Owner', '(123)555-0100', None, None, '(123)555-0101', '123 1st Street', 'Seattle', 'WA', '99999', 'USA', None, None, '');"
+        nis = lines[1][4:9]
+        today = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        fileportail = '/srv/instances/server_urban25/var/urban/northwind.cfg'
+        with open(fileportail, 'r') as f:
+            l = f.readlines()
+        user = l[8][5:27]
+
+#        rec_test = "SELECT * FROM customers WHERE id >= 42;"
+        for key in parcels_keys:
+            try:
+#                rec_i = "INSERT INTO customers (company, last_name, first_name, email_address, job_title, business_phone, home_phone, mobile_phone, fax_number, address, city, state_province, zip_postal_code, country_region, web_page, notes, attachments) VALUES ('Company DD', 'Cornick', 'Anna', '{user}', 'Owner', '(123)555-0100', '{today}', NULL, '(123)555-0101', '123 1st Street', 'Seattle', 'WA', '99999', 'USA', '{cap}', '{nis}', '');".format(user=user, today=today, cap=key, nis=nis)
+#                query = self.session.execute(rec_i)
+                new_rec = "INSERT INTO GIG_TRANSIT (NUM_SIG, user_id, copy_time, work_id, INS) VALUES ('{cap}', '{user}', '{today}', 1, '{nis}');".format(cap=key, user=user, today=today, nis=nis)
+                query = self.session.execute(new_rec)
+            except pymysql.Error, e:
+                print("fail to insert: " + str(e))
+#        query = self.session.execute(rec_test)
+        self.session.commit()
+#        result = query.fetchall()
 
 #        new_rec = "INSERT INTO GIG_TRANSIT (NUM_SIG, user_id, copy_time, work_id, INS) VALUES ('25090A013100E000', 'simon.delcourt@imio.be', NOW(), 1, '25123');"
 #        rec2 = "SELECT * FROM GIG_TRANSIT WHERE NUM_SIG = '25090A013100E000';"
-#        resinsert = session.execute(new_rec)
-#        resselect = session.execute(rec2)
-#        session.commit()
-#        resselect.fetchall()
-#        session.close()
 #        engine.dispose()
 #        gig_engine = services.gig.new_session()
         import ipdb; ipdb.set_trace()
