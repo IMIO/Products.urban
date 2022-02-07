3e930119cd5bd2bd564019559f3fc2670c8895d7
sdelcourt <simon.delcourt@imio.be>
2022-02-07 16:21:25 +0100

    Remove NOTICe from urban2.5.x and work in another branch.

--- src/Products/urban/services/__init__.py
+++ src/Products/urban/services/__init__.py
@@ -9,10 +9,8 @@ try:
     config_cadastre = ExternalConfig('cadastre')
     config_bestaddress = ExternalConfig('bestaddress')
     config_parcel_coring = ExternalConfig('parcel_coring')
-    config_NOTICe = ExternalConfig('NOTICe')
 except:
     config = {}
 
 cadastre = CadastreService(**(config_cadastre and config_cadastre.cadastre))
 parcel_coring = ParcelCoringService(**(config_parcel_coring and config_parcel_coring.parcel_coring))
-NOTICe = NOTICeService(**(config_NOTICe and config_NOTICe.NOTICe))
