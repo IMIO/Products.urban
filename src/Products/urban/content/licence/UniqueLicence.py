810f06b35fca0d5d57c14f956ac61407e532ee27
MagDhyIm <magali.dhyne@imio.be>
2022-02-14 14:55:01 +0100

    remove -choisissez-

--- src/Products/urban/content/licence/UniqueLicence.py
+++ src/Products/urban/content/licence/UniqueLicence.py
@@ -68,7 +68,7 @@ schema = Schema((
             label=_('urban_label_authority', default='Authority'),
         ),
         schemata='urban_description',
-        vocabulary=UrbanVocabulary('authority', inUrbanConfig=True, with_empty_value=True),
+        vocabulary=UrbanVocabulary('authority', inUrbanConfig=True, with_empty_value=False),
         default_method='getDefaultValue',
     ),
     LinesField(
