896efebf56b21d7a0ecaf2153e2029e7f5ebee35
sdelcourt <simon.delcourt@imio.be>
2022-04-06 15:24:58 +0200

    MURBCGIA-22. Add patrimony tab on Division licences.

--- src/Products/urban/browser/licence/divisionview.py
+++ src/Products/urban/browser/licence/divisionview.py
@@ -1,14 +1,14 @@
-from Products.urban.browser.licence.licenceview import LicenceView
+from Products.urban.browser.licence.licenceview import CODTLicenceView
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone import PloneMessageFactory as _
 
 
-class DivisionView(LicenceView):
+class DivisionView(CODTLicenceView):
     """
       This manage the view of Division
     """
     def __init__(self, context, request):
-        super(LicenceView, self).__init__(context, request)
+        super(CODTLicenceView, self).__init__(context, request)
         self.context = context
         self.request = request
         # disable portlets on licences
