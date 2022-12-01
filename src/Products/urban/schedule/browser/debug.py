bcf07b39787ae91ddfffda72563ece7bc8142f71
Martin Peeters <martin.peeters@affinitic.be>
2022-12-01 14:23:04 +0100

    Add a `@@debug` view for administrators to help understand task data

---
+++ src/Products/urban/schedule/browser/debug.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+
+from Products.Five import BrowserView
+from imio.schedule.interfaces import IStartDate
+from plone import api
+from zope.component import queryMultiAdapter
+
+
+class TaskDebugView(BrowserView):
+    @property
+    def licence(self):
+        return self.context.get_container()
+
+    @property
+    def task_config(self):
+        return self.context.get_task_config()
+
+    @property
+    def values(self):
+        base = []
+        base.extend(self.attributes)
+        base.extend([("review state", api.content.get_state(self.context))])
+        return base
+
+    @property
+    def attributes(self):
+        keys = (
+            "creation_date",
+            "modification_date",
+            "due_date",
+            "assigned_group",
+            "assigned_user",
+        )
+        return [(k, getattr(self.context, k, "")) for k in keys]
+
+    @property
+    def licence_values(self):
+        licence = self.licence
+        return [
+            ("title", licence.Title()),
+            ("url", licence.absolute_url()),
+        ]
+
+    @property
+    def start_date_values(self):
+        base = []
+        adapter_name = self.task_config.start_date
+        adapter = queryMultiAdapter(
+            (self.licence, self.context), IStartDate, name=adapter_name
+        )
+        if not adapter:
+            return base
+        licence = adapter.task_container
+        deposit = licence.getLastDeposit()
+        deposit_date = deposit and deposit.getEventDate()
+        has_modified_blueprints = (
+            hasattr(licence, "getHasModifiedBlueprints")
+            and licence.getHasModifiedBlueprints()
+        )
+        base.extend(
+            [
+                ("adapter", adapter.__class__),
+                ("start date", adapter.start_date()),
+                ("task_config", adapter.task_config.absolute_url()),
+                ("last deposit event", deposit.absolute_url() or ""),
+                ("last deposit date", deposit_date or ""),
+                ("Has modified blueprints", has_modified_blueprints),
+            ]
+        )
+        return base
