5cda27eb861e29a90d0e7fc721efffcd28765235
sdelcourt <simon.delcourt@imio.be>
2023-03-24 10:40:06 +0100

    SUP-29360. Add show_description external_method.

--- src/Products/urban/Extensions/fix_corrupted_description.py
+++ src/Products/urban/Extensions/fix_corrupted_description.py
@@ -13,7 +13,16 @@ def fix_corrupted_description():
     context = request['PARENTS'][0]
     if not getattr(context, "description", False):
         return
-    
+
     context.description.raw = ""
     context.description.original_encoding = "ascii"
     transaction.commit()
+
+
+def show_description():
+    """
+    """
+    portal = api.portal.get()
+    request = portal.REQUEST
+    context = request['PARENTS'][0]
+    return context.description.raw
