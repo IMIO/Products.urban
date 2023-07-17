d67fb78f2b24e971706ac085e6ed2688802df183
Nicolas Demonté <nicolas@affinitic.be>
2023-07-17 16:16:36 +0200

    URB-2824. Add Couple to Project Meeting

--- src/Products/urban/migration/update_260.py
+++ src/Products/urban/migration/update_260.py
@@ -6,6 +6,7 @@ def add_couple_to_preliminary_notice(context):
     """
     """
     logger = logging.getLogger('urban: add Couple to Preliminary Notice')
+    logger = logging.getLogger('urban: add Couple to Project Meeting')
     logger.info("starting upgrade steps")
     setup_tool = api.portal.get_tool('portal_setup')
     setup_tool.runImportStepFromProfile('profile-Products.urban:preinstall', 'typeinfo')
