313dfb6df124eecd1a742c33a09d1322802453bd
jimbiscuit <julienchandelle@gmail.com>
2023-07-18 14:25:16 +0200

    URB-2830. Remove Upgrade step

--- src/Products/urban/migration/update_260.py
+++ src/Products/urban/migration/update_260.py
@@ -13,15 +13,3 @@ def add_couple_to_preliminary_notice(context):
     setup_tool.runImportStepFromProfile('profile-Products.urban:preinstall', 'typeinfo')
     setup_tool.runImportStepFromProfile('profile-Products.urban:preinstall', 'workflow')
     logger.info("upgrade step done!")
-
-
-def fix_inspection_title(context):
-    logger = logging.getLogger('urban: Fix inspection title')
-    logger.info("starting upgrade steps")
-    catalog = api.portal.get_tool('portal_catalog')
-    licence_brains = catalog(object_provides=IInspection.__identifier__)
-    for licence_brain in licence_brains:
-        if IInspection.providedBy(licence_brain.getObject()):
-            licence = licence_brain.getObject()
-            licence.updateTitle()
-    logger.info("upgrade done!")
