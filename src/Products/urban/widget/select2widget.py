3f35d8e4229f3d960325e766af091e274b44e4ba
Martin Peeters <martin.peeters@affinitic.be>
2023-07-26 09:34:11 +0200

    SUP-31554: Avoid errors on inexpected values on licences and log them

--- src/Products/urban/widget/select2widget.py
+++ src/Products/urban/widget/select2widget.py
@@ -6,30 +6,36 @@ from Products.urban.UrbanVocabularyTerm import UrbanVocabulary
 from zope.component import getUtility
 from zope.schema.interfaces import IVocabularyFactory
 
+import logging
+import six
+
+logger = logging.getLogger("urban debug")
+
 
 def resolve_vocabulary(context, field, values):
     if type(field.vocabulary) == UrbanVocabulary:
-        return ", ".join(
-            [
-                field.vocabulary.getAllVocTerms(context)[value].title
-                for value in values if value
-            ]
-        )
+        result = [
+            field.vocabulary.getAllVocTerms(context)[value].title
+            for value in values
+            if value
+        ]
     elif type(field.vocabulary) == str:
         display_list = getattr(context, field.vocabulary)()
-        return ", ".join(
-            [display_list.getValue(value) for value in values if value]
-        )
-    elif (
-        type(field.vocabulary) == tuple
-        and getattr(field, "vocabulary_factory", False)
+        result = [display_list.getValue(value) for value in values if value]
+    elif type(field.vocabulary) == tuple and getattr(
+        field, "vocabulary_factory", False
     ):
         vocabulary_factory = field.vocabulary_factory
         factory = getUtility(IVocabularyFactory, vocabulary_factory)
         vocabulary = factory(context)
-        return ", ".join(
-            [vocabulary.by_token[value].title for value in values if value]
+        result = [vocabulary.by_token[value].title for value in values if value]
+    if len(result) != len(filter(None, result)):
+        logger.info(
+            "{0}: Unknown value for field '{1}' in '{2}'".format(
+                context.absolute_url(), field.__name__, values
+            )
         )
+    return ", ".join(filter(None, result))
 
 
 class Select2Widget(CollectiveSelect2Widget):
@@ -41,4 +47,15 @@ class Select2Widget(CollectiveSelect2Widget):
 class MultiSelect2Widget(CollectiveMultiSelect2Widget):
     def view(self, context, field, request):
         values = super(MultiSelect2Widget, self).view(context, field, request)
+        if values != getattr(context, field.__name__):
+            # inexpected stored value
+            logger.info(
+                "{0}: Inexpected value for field '{1}'".format(
+                    context.absolute_url(),
+                    field.__name__,
+                )
+            )
+            value = getattr(context, field.__name__)
+            if isinstance(value, six.string_types):
+                values = (value,)
         return resolve_vocabulary(context, field, values)
