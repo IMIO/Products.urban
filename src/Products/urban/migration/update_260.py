27552483808231c3da9a91109107e21ea9396b6d
Martin Peeters <martin.peeters@affinitic.be>
2023-08-14 11:44:14 +0200

    URB-2864: Hide old document generation links viewlet

--- src/Products/urban/migration/update_260.py
+++ src/Products/urban/migration/update_260.py
@@ -12,3 +12,11 @@ def add_couple_to_preliminary_notice(context):
     setup_tool.runImportStepFromProfile('profile-Products.urban:preinstall', 'typeinfo')
     setup_tool.runImportStepFromProfile('profile-Products.urban:preinstall', 'workflow')
     logger.info("upgrade step done!")
+
+
+def remove_generation_link_viewlet(context):
+    logger = logging.getLogger("urban: Remove generation-link viewlet")
+    logger.info("starting upgrade steps")
+    setup_tool = api.portal.get_tool("portal_setup")
+    setup_tool.runImportStepFromProfile("profile-Products.urban:default", "viewlets")
+    logger.info("upgrade step done!")
