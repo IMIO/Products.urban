335054f043878df56e920f330daeaf29277a104c
Nicolas Demonté <nicolas@affinitic.be>
2023-12-19 13:42:39 +0100

    URB-2515: Move due date configuration to `imio.schedule`

--- src/Products/urban/browser/schedule_settings.py
+++ src/Products/urban/browser/schedule_settings.py
@@ -2,50 +2,20 @@
 
 from Products.statusmessages.interfaces import IStatusMessage
 from Products.urban import UrbanMessage as _
+from imio.schedule.interfaces import IDueDateSettings
 from plone.app.registry.browser.controlpanel import RegistryEditForm
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
-from zope import schema
-from zope.interface import invariant
-from zope.interface import Interface
-from zope.interface import Invalid
 from z3c.form import button
 from z3c.form import field
 
 
-class ISchedule(Interface):
-    """ """
-
-    color_orange_x_days_before_due_date = schema.Int(
-        title=_("Color due date in orange if it comes close to X days"),
-        description=_("Leave empty to disable"),
-        required=False,
-        default=10,
-        min=0,
-    )
-
-    color_red_x_days_before_due_date = schema.Int(
-        title=_("Color due date in red if it comes close to X days"),
-        description=_("Overrides orange color, leave empty to disable"),
-        required=False,
-        default=5,
-        min=0,
-    )
-
-    @invariant
-    def orange_is_before_red_invariant(data):
-        if data.color_orange_x_days_before_due_date is None or data.color_red_x_days_before_due_date is None:
-            return
-        if data.color_orange_x_days_before_due_date < data.color_red_x_days_before_due_date:
-            raise Invalid(_(u"The orange value should be higher than the red one, as it is used as a first warning."))
-
-
 class ScheduleEditForm(RegistryEditForm):
     """"""
-    schema = ISchedule
+    schema = IDueDateSettings
     label = _(u"Schedule alerts")
     description = _(u"""""")
 
-    fields = field.Fields(ISchedule)
+    fields = field.Fields(IDueDateSettings)
 
     @button.buttonAndHandler(_('Save'), name=None)
     def handleSave(self, action):
