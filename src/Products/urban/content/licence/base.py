068906839510d143194ad5280e3f638a8f55697d
fngaha <franck.o.ngaha@gmail.com>
2024-01-05 16:11:02 +0100

    Improve merge fields SUP-34184

--- src/Products/urban/content/licence/base.py
+++ src/Products/urban/content/licence/base.py
@@ -504,7 +504,7 @@ class UrbanBase(object):
 
     security.declarePublic("getStreetAndNumber")
 
-    def getStreetAndNumber(self, separator=False):
+    def getStreetAndNumber(self, separator=None):
         """
         Returns a string reprensenting the different streets and numbers
         """
@@ -517,7 +517,7 @@ class UrbanBase(object):
 
     security.declarePublic("getDefaultStreetAndNumber")
 
-    def getDefaultStreetAndNumber(self, separator=False):
+    def getDefaultStreetAndNumber(self, separator=None):
         """
         Returns a string reprensenting the different streets and numbers
         """
@@ -533,8 +533,8 @@ class UrbanBase(object):
             number = wl["number"]
             if number:
                 if separator:
-                    signaletic = "{} {}, {}".format(
-                        signaletic, streetName, convert_to_utf8(number)
+                    signaletic = "{} {}{} {}".format(
+                        signaletic, streetName, separator, convert_to_utf8(number)
                     )
                 else:
                     signaletic = "{} {} {}".format(
