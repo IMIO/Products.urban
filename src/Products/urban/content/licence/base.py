97db7743f7f7ded5693b12feffcab8e2b66c7e43
fngaha <franck.o.ngaha@gmail.com>
2024-01-30 11:39:57 +0100

    Use empty string for separator default value : SUP-34184

--- src/Products/urban/content/licence/base.py
+++ src/Products/urban/content/licence/base.py
@@ -517,7 +517,7 @@ class UrbanBase(object):
 
     security.declarePublic("getDefaultStreetAndNumber")
 
-    def getDefaultStreetAndNumber(self, separator=None):
+    def getDefaultStreetAndNumber(self, separator=''):
         """
         Returns a string reprensenting the different streets and numbers
         """
