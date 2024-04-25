bad1aba2dd983c52ce9c5435c2dfb5fd22cc310c
jimbiscuit <julienchandelle@gmail.com>
2024-04-25 12:58:36 +0200

    MURBMONA-48. Fix wrong files export (#231)

--- src/Products/urban/browser/exportimport/export_content.py
+++ src/Products/urban/browser/exportimport/export_content.py
@@ -7,17 +7,18 @@ from zope.component import getMultiAdapter
 
 
 class UrbanExportContent(ExportContent):
-    def _serializer(self, obj):
+    def _serializer(self, obj, with_files=False):
         serializer = getMultiAdapter((obj, self.request), ISerializeToJson)
         item = serializer()
-        sub_files = obj.listFolderContents(
-            contentFilter={"portal_type": "File"}
-        )
-        for file in sub_files:
-            file_serializer = getMultiAdapter((file, self.request), ISerializeToJson)
-            if "documents" not in item:
-                item["documents"] = []
-            item["documents"].append(self.update_data_for_migration(file_serializer(), obj))
+        if with_files:
+            sub_files = obj.listFolderContents(
+                contentFilter={"portal_type": "File"}
+            )
+            for file in sub_files:
+                file_serializer = getMultiAdapter((file, self.request), ISerializeToJson)
+                if "documents" not in item:
+                    item["documents"] = []
+                item["documents"].append(self.update_data_for_migration(file_serializer(), obj))
         item["@id"] = obj.absolute_url()
         return self.update_data_for_migration(item, obj)
 
@@ -25,7 +26,7 @@ class UrbanExportContent(ExportContent):
         item = super(UrbanExportContent, self).global_dict_hook(item, obj)
         if item["@type"] in URBAN_TYPES:
             item["events"] = [
-                self._serializer(event)
+                self._serializer(event, with_files=True)
                 for event in obj.listFolderContents(
                     contentFilter={"portal_type": [
                         "UrbanEvent",
