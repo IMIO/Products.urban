b4e99c290454d943a5bab7e0c2987ef27be67951
jimbiscuit <julienchandelle@gmail.com>
2024-11-18 10:59:54 +0100

    Black and Isort

--- src/Products/urban/contentrules/event_type.py
+++ src/Products/urban/contentrules/event_type.py
@@ -3,14 +3,16 @@
 from OFS.SimpleItem import SimpleItem
 from plone import api
 from plone.app.contentrules import PloneMessageFactory as _
-from plone.app.contentrules.browser.formhelper import AddForm, EditForm
-from plone.autoform import directives
-from plone.contentrules.rule.interfaces import IExecutable, IRuleElementData
-from z3c.form.browser.orderedselect import OrderedSelectWidget
+from plone.app.contentrules.browser.formhelper import AddForm
+from plone.app.contentrules.browser.formhelper import EditForm
+from plone.contentrules.rule.interfaces import IExecutable
+from plone.contentrules.rule.interfaces import IRuleElementData
 from zope import schema
-from zope.component import adapts, getUtility
+from zope.component import adapts
+from zope.component import getUtility
 from zope.formlib import form
-from zope.interface import Interface, implements
+from zope.interface import implements
+from zope.interface import Interface
 from zope.schema.interfaces import IVocabularyFactory
 
 
@@ -76,6 +78,7 @@ class EventTypeConditionExecutor(object):
             return True
         except TypeError:
             return False
+
 
 class EventTypeAddForm(AddForm):
     """An add form for event type condition."""
