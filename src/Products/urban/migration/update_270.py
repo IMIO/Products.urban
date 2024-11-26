5e0704f5a1e18a7667122bc5d6adc6e07f1932e2
Daniel Muyshond <33881615+dmshd@users.noreply.github.com>
2024-11-26 10:47:48 +0100

    URB-3126 : Add latest new voc terms (form_composition) (#285)
    Those are terms presents on demo at the date of today.
    They where asked by Aurore
    
    Co-authored-by: daggelpop <nicolas@affinitic.be>

--- src/Products/urban/migration/update_270.py
+++ src/Products/urban/migration/update_270.py
@@ -3,6 +3,7 @@
 from Acquisition import aq_parent
 from OFS.interfaces import IOrderedContainer
 from Products.urban.migration.utils import refresh_workflow_permissions
+from Products.urban.setuphandlers import createFolderDefaultValues
 from imio.schedule.content.object_factories import MacroCreationConditionObject
 from imio.schedule.content.object_factories import MacroEndConditionObject
 from imio.schedule.content.object_factories import MacroFreezeConditionObject
@@ -213,3 +214,55 @@ def fix_config_wrong_class(context):
             schedule_cfg.REQUEST.form = data
 
     logger.info("Upgrade step done!")
+
+
+def add_new_voc_terms_for_form_composition(context):
+    logger = logging.getLogger("urban: Add new vocabularies to portal_urban/form_composition")
+    logger.info("starting upgrade steps")
+
+    portal_urban = api.portal.get()["portal_urban"]
+    form_composition_folder = portal_urban.form_composition
+
+    # Those are the new vocabulary terms that will be added
+    # Refer to profiles/extra/config_default_values.py
+    # for the existing vocabulary terms
+    # that will be initiated in each new instance
+    form_composition_new_vocabulary_terms_to_add = [
+        {
+            "id": "10",
+            "title": u"1/1 Formulaire général permis environnement et unique"
+        },
+        {
+            "id": "11",
+            "title": u"1/2 Élevage et détention d'animaux"
+        },
+        {
+            "id": "12",
+            "title": u"Annexe V/1 - Implantation d'un commerce",
+        },
+        {
+            "id": "13",
+            "title": u"Annexe IX - Permis d'urbanisme dispensé d'un architecte ou autre que les demandes visées aux annexes 5 à 8",
+        },
+        {
+            "id": "14",
+            "title": u"Annexe X - Demande de permis d'urbanisation ou de modification de permis d'urbanisation"
+        },
+        {
+            "id": "15",
+            "title": u"Annexe XI - Demande de permis d'urbanisation ou de modification de permis d'urbanisation avec contenu simplifié"
+        },
+        {
+            "id": "16",
+            "title": u"Annexe XV - Demande de certificat d'urbanisme n°2"
+        },
+
+    ]
+
+    createFolderDefaultValues(
+        form_composition_folder,
+        form_composition_new_vocabulary_terms_to_add,
+        portal_type="UrbanVocabularyTerm",
+    )
+
+    logger.info("upgrade done!")
