0d45f449a432e3fcb43b2500575ad123849ec508
daggelpop <nicolas@affinitic.be>
2024-11-26 11:09:29 +0100

    URB-3121 migrate patrimony certificate (#287)
    * URB-3121: Add patrimony fieldset to patrimony certificate
    
    * URB-3121: Migrate patrimony certificates to their correct object class (instead of misc demand)

--- src/Products/urban/migration/update_270.py
+++ src/Products/urban/migration/update_270.py
@@ -301,3 +301,31 @@ def allow_corporate_tenant_in_inspections(context):
         isp_tool.allowed_content_types = tuple(new_allowed_types)
 
     logger.info("upgrade step done!")
+
+
+def fix_patrimony_certificate_class(context):
+    from Products.urban.content.licence.PatrimonyCertificate import PatrimonyCertificate
+
+    logger = logging.getLogger("urban: Fix patrimony certificate class")
+    logger.info("starting upgrade steps")
+
+    # fix FTI
+    portal = api.portal.get()
+    fti = portal.portal_types.PatrimonyCertificate
+    fti.content_meta_type = "PatrimonyCertificate"
+    fti.factory = "addPatrimonyCertificate"
+
+    # migrate content
+    catalog = api.portal.get_tool("portal_catalog")
+    licence_brains = catalog(portal_type="PatrimonyCertificate")
+
+    for licence_brain in licence_brains:
+        licence = licence_brain.getObject()
+        if licence.__class__ == PatrimonyCertificate:
+            continue
+        licence.__class__ = PatrimonyCertificate
+        licence.meta_type = "PatrimonyCertificate"
+        licence.schema = PatrimonyCertificate.schema
+        licence.reindexObject()
+
+    logger.info("upgrade step done!")
