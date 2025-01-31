4b40aa0e41800335aeb01b01e19b55dc3525fb66
Julien Chandelle <julienchandelle@gmail.com>
2025-01-31 10:39:54 +0100

    SUP-40979 Fix sending zem document by mail (#303)

--- src/Products/urban/vocabularies.py
+++ src/Products/urban/vocabularies.py
@@ -432,6 +432,9 @@ GigCoringUserIdVocabularyFactory = GigCoringUserIdVocabulary()
 class LicenceDocumentsVocabulary(object):
     implements(IVocabularyFactory)
 
+    def get_path(sefl, obj):
+        return "/".join(obj.getPhysicalPath())
+
     def __call__(self, context):
         contexts = get_licence_context(context, get_all_object=True)
         output = []
@@ -439,7 +442,7 @@ class LicenceDocumentsVocabulary(object):
             return SimpleVocabulary(output)
         for context in contexts:
             docs = [
-                SimpleTerm(doc.UID(), doc.UID(), doc.Title())
+                SimpleTerm(self.get_path(doc), self.get_path(doc), doc.Title())
                 for doc in context.listFolderContents(
                     contentFilter={
                         "portal_type": ["ATFile", "ATImage", "File", "Image"]
