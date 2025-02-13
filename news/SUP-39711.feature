25aca6b6b26d3600de24bcff6ddff914ef2d12d8
Julien Chandelle <julienchandelle@gmail.com>
2025-02-13 15:51:47 +0100

    SUP-39711 Add possibility to get template merged when import (#320)
    * SUP-39711 Add possibility to get template merged when import
    
    * SUP-39711 Add check to avoid testing already content while searching uid template
    
    * SUP-39711 Add testing
    
    * Black and Isort

---
+++ news/SUP-39711.feature
@@ -0,0 +1,2 @@
+Add possibility to get template merged when import
+[jchandelle]
--- src/Products/urban/tests/test_exportimport.py
+++ src/Products/urban/tests/test_exportimport.py
@@ -1,9 +1,13 @@
 #  -*- coding: utf-8 -*-
-import unittest2 as unittest
 from Products.CMFPlone.utils import base_hasattr
 from Products.urban.Extensions.imports import createStreet
+from Products.urban.browser.exportimport.import_config import ConfigImportContent
 from Products.urban.testing import URBAN_TESTS_CONFIG
 from plone import api
+
+import json
+import os
+import unittest2 as unittest
 
 
 class TestStreetImports(unittest.TestCase):
@@ -204,3 +208,157 @@ class TestStreetImports(unittest.TestCase):
         self.assertEquals(
             self.wtool.getInfoFor(rue7, "review_state"), "enabled"
         )  # previous street is unchanged
+
+
+class TestExportImportContent(unittest.TestCase):
+    layer = URBAN_TESTS_CONFIG
+
+    def setUp(self):
+        self.portal = self.layer["portal"]
+        self.utool = self.portal.portal_urban
+        self.add_globaltemplates_to_event()
+
+    def get_template_parameter(self, folder):
+        self.globaltemplates_id_list = {
+            "logo.odt": "a46d50ed-9070-4d7b-8422-a02d20adb0db",
+            "header.odt": "589d6268-1d93-416c-a8ac-271f6595c234",
+            "footer.odt": "9bc708e9-bbc7-4b1f-ac44-03e555885af0",
+            "signatures.odt": "bdb59fd7-8983-4f5c-ba8d-8f7e850e978d",
+        }
+        globaltemplates = getattr(self.utool, "globaltemplates", None)
+        if globaltemplates is None:
+            return []
+        sub_urbantemplates = getattr(globaltemplates, folder, None)
+        if sub_urbantemplates is None:
+            return []
+        globaltemplates_contents = sub_urbantemplates.contentItems()
+        globaltemplates_parameter = []
+        for id, globaltemplate in globaltemplates_contents:
+            if id not in self.globaltemplates_id_list:
+                continue
+            setattr(globaltemplate, "_plone.uuid", self.globaltemplates_id_list[id])
+            globaltemplate.reindexObject(idxs=["UID"])
+            globaltemplates_parameter.append(
+                {
+                    "do_rendering": True,
+                    "template": globaltemplate.UID(),
+                    "pod_context_name": id.split(".")[0],
+                }
+            )
+        return globaltemplates_parameter
+
+    def add_globaltemplates_to_event(self):
+        urbantemplates = self.get_template_parameter("urbantemplates")
+        brains = api.content.find(context=self.utool, portal_type="UrbanTemplate")
+
+        for brain in brains:
+            urbantemplate_obj = brain.getObject()
+            urbantemplate_obj.merge_templates = urbantemplates
+
+    def get_data(self, file):
+        directory_path = os.path.dirname(os.path.realpath(__file__))
+        json_file = os.path.normpath(os.path.join(directory_path, "json", file))
+        data = None
+        with open(json_file, "r") as f:
+            data = json.load(f)
+        return data
+
+    def test_import_template_no_other_document(self):
+        data = self.get_data("urban_template_test_no_other_document.json")
+
+        with api.env.adopt_roles(["Manager"]):
+            import_urban_config = ConfigImportContent(self.utool, self.portal.REQUEST)
+            import_urban_config.import_to_current_folder = False
+            import_urban_config.handle_existing_content = 0
+            import_urban_config.limit = None
+            import_urban_config.commit = None
+            import_urban_config.import_old_revisions = False
+            import_urban_config.fix_parent_path = True
+            import_urban_config.import_in_same_instance = False
+            import_urban_config.import_to_current_lic_config_folder = False
+            import_urban_config.handle_missing_parent = 0
+
+            import_urban_config.start()
+            import_urban_config.do_import(data)
+            import_urban_config.finish()
+
+            template_imported_uid = "a6011644490b401288a4b06262dcede8"
+            template_imported_obj = api.content.get(UID=template_imported_uid)
+            merge_templates = template_imported_obj.merge_templates
+            merge_templates = {
+                merge_template["pod_context_name"]: merge_template["template"]
+                for merge_template in merge_templates
+            }
+            self.assertEquals(
+                merge_templates["footer"], "9bc708e9-bbc7-4b1f-ac44-03e555885af0"
+            )
+            self.assertEquals(
+                merge_templates["header"], "589d6268-1d93-416c-a8ac-271f6595c234"
+            )
+
+    def test_import_template_same_procedure_type(self):
+        data = self.get_data("urban_template_test_same_procedure_type.json")
+
+        with api.env.adopt_roles(["Manager"]):
+            import_urban_config = ConfigImportContent(self.utool, self.portal.REQUEST)
+            import_urban_config.import_to_current_folder = False
+            import_urban_config.handle_existing_content = 0
+            import_urban_config.limit = None
+            import_urban_config.commit = None
+            import_urban_config.import_old_revisions = False
+            import_urban_config.fix_parent_path = True
+            import_urban_config.import_in_same_instance = False
+            import_urban_config.import_to_current_lic_config_folder = False
+            import_urban_config.handle_missing_parent = 0
+
+            import_urban_config.start()
+            import_urban_config.do_import(data)
+            import_urban_config.finish()
+
+            template_imported_uid = "96c6baa97c7d47958415100d73dde04a"
+            template_imported_obj = api.content.get(UID=template_imported_uid)
+            merge_templates = template_imported_obj.merge_templates
+
+            merge_templates = {
+                merge_template["pod_context_name"]: merge_template["template"]
+                for merge_template in merge_templates
+            }
+            self.assertEquals(
+                merge_templates["footer"], "9bc708e9-bbc7-4b1f-ac44-03e555885af0"
+            )
+            self.assertEquals(
+                merge_templates["header"], "589d6268-1d93-416c-a8ac-271f6595c234"
+            )
+
+    def test_import_template_same_event(self):
+        data = self.get_data("urban_template_test_same_event.json")
+
+        with api.env.adopt_roles(["Manager"]):
+            import_urban_config = ConfigImportContent(self.utool, self.portal.REQUEST)
+            import_urban_config.import_to_current_folder = False
+            import_urban_config.handle_existing_content = 0
+            import_urban_config.limit = None
+            import_urban_config.commit = None
+            import_urban_config.import_old_revisions = False
+            import_urban_config.fix_parent_path = True
+            import_urban_config.import_in_same_instance = False
+            import_urban_config.import_to_current_lic_config_folder = False
+            import_urban_config.handle_missing_parent = 0
+
+            import_urban_config.start()
+            import_urban_config.do_import(data)
+            import_urban_config.finish()
+
+            template_imported_uid = "ce2735a774b84383bcd41d68a1c59f63"
+            template_imported_obj = api.content.get(UID=template_imported_uid)
+            merge_templates = template_imported_obj.merge_templates
+            merge_templates = {
+                merge_template["pod_context_name"]: merge_template["template"]
+                for merge_template in merge_templates
+            }
+            self.assertEquals(
+                merge_templates["footer"], "9bc708e9-bbc7-4b1f-ac44-03e555885af0"
+            )
+            self.assertEquals(
+                merge_templates["header"], "589d6268-1d93-416c-a8ac-271f6595c234"
+            )
