0def7a4b03fd82faec54db411e5bc3fff141b9ee
jimbiscuit <julienchandelle@gmail.com>
2025-02-27 14:44:32 +0100

    URB-3237 SUP-41619 Fix ViewPageTemplateFile import

--- src/Products/urban/browser/view_utils/fix_task_uid_error.py
+++ src/Products/urban/browser/view_utils/fix_task_uid_error.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from Products.Five import BrowserView
-from zope.app.pagetemplate.viewpagetemplatefile import ViewPageTemplateFile
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from imio.schedule.interfaces import TaskConfigNotFound
 from imio.schedule.utils import get_container_tasks
 from plone import api
