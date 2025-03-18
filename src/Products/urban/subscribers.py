5c1fa584513fbebfde62f39783bf35eb1801cbc2
Wiemboudabous <wiem.boudabous@imio.be>
2025-03-18 14:51:46 +0100

    Update subscriber to use ILicenceNotificationEvent

--- src/Products/urban/subscribers.py
+++ src/Products/urban/subscribers.py
@@ -6,6 +6,8 @@ import re
 from DateTime import DateTime
 from zope.lifecycleevent.interfaces import IObjectAddedEvent, IObjectModifiedEvent
 from .utils import convert_to_europe_brussels
+from .interfaces import ICODT_ParcelOutLicence
+
 
 def after_term_deactivate(obj, event):
     if (
@@ -16,56 +18,53 @@ def after_term_deactivate(obj, event):
         return
     obj.setEndValidity(datetime(*datetime.now().date().timetuple()[0:3]))
 
-def post_save_parceloutlicence(obj,event):
-     
-        #object_state = obj.get_state()
-        parent_container = api.content.get(path='/Urban/urban/parcellings')
-        title = getattr(obj,  'title', None)
-        reference_dgatlp = getattr(obj,  'referenceDGATLP', None)
-        reference = getattr(obj,  'reference', None)
-        label = getattr(obj,  'licenceSubject', None)
-        
-        notification = obj.getLastLicenceNotification()
-        
-        if notification is not None:
-            is_favorable = notification.getDecision().lower() == "favorable"
-            
-            approbation_date = notification.getEventDate()
-            autorisation_date = notification.decisionDate
-            
-            # format dates
-            if isinstance(approbation_date, DateTime):
-                approbation_date = convert_to_europe_brussels(approbation_date)
-                
-            if isinstance(autorisation_date, DateTime):
-                autorisation_date = convert_to_europe_brussels(autorisation_date)
-        else:
-            approbation_date = autorisation_date = None
+
+def post_save_event_parceloutlicence(obj, event):
     
-        # to update 
-        match = re.search(r' - ([^-]+)$', title)
-        
-        if match is not None:
-            
-            subdivider_name = match.group(1)
-            
-        else: 
-            
-            subdivider_name = ""
-            
-        found_parcelling = api.content.find(context=parent_container, communalReference=reference, portal_type="Parcelling")
+    if not ICODT_ParcelOutLicence.providedBy(obj.aq_parent):
+        return
+
+    parent_container = api.content.get(path="/Urban/urban/parcellings")
+    title = getattr(obj.aq_parent, "title", None)
+    reference_dgatlp = getattr(obj.aq_parent, "referenceDGATLP", None)
+    reference = getattr(obj.aq_parent, "reference", None)
+    label = getattr(obj.aq_parent, "licenceSubject", None)
+    match = re.search(r" - ([^-]+)$", title)
+    if match is not None:
+        subdivider_name = match.group(1)
+    else:
+        subdivider_name = ""
+
+    is_favorable = obj.getDecision() == "favorable"
+    if not is_favorable:
+        return
+    else:
+        approbation_date = obj.getEventDate()
+        authorization_date = obj.decisionDate
+
+        # format dates
+        if isinstance(approbation_date, DateTime):
+            approbation_date = convert_to_europe_brussels(approbation_date)
+
+        if isinstance(authorization_date, DateTime):
+            authorization_date = convert_to_europe_brussels(authorization_date)
+
+        found_parcelling = api.content.find(
+            context=parent_container,
+            communalReference=reference,
+            portal_type="Parcelling",
+        )
         if found_parcelling and len(found_parcelling) > 0:
             found_parcelling = found_parcelling[0].getObject()
-            
-            #  Update fields
+            # Update fields
             found_parcelling.title = title
             found_parcelling.label = label
             found_parcelling.subdividerName = subdivider_name
             found_parcelling.communalReference = reference
             found_parcelling.DGO4Reference = reference_dgatlp
             found_parcelling.approvalDate = approbation_date
-            found_parcelling.authorizationDate = autorisation_date
-            
+            found_parcelling.authorizationDate = authorization_date
+
         else:
             # If the object doesn't exist, create a new one
             api.content.create(
@@ -76,11 +75,6 @@ def post_save_parceloutlicence(obj,event):
                 subdividerName=subdivider_name,
                 communalReference=reference,
                 DGO4Reference=reference_dgatlp,
-                approvalDate = approbation_date,
-                authorizationDate = autorisation_date
+                approvalDate=approbation_date,
+                authorizationDate=authorization_date,
             )
-        
-    
-   
-
-
