5bcff92070e54df8a0007670423e9745095b5c4b
jimbiscuit <julienchandelle@gmail.com>
2025-04-17 16:15:51 +0200

    SUP-33793 Add caching to the function

--- src/Products/urban/dashboard/vocabularies.py
+++ src/Products/urban/dashboard/vocabularies.py
@@ -4,6 +4,7 @@ from Acquisition import aq_parent
 from imio.dashboard.vocabulary import ConditionAwareCollectionVocabulary
 
 from plone import api
+from plone.memoize import ram
 
 from Products.urban import UrbanMessage as _
 from Products.urban.config import URBAN_TYPES
@@ -12,6 +13,7 @@ from Products.urban.config import URBAN_CODT_TYPES
 from Products.urban.config import URBAN_ENVIRONMENT_TYPES
 from Products.urban.dashboard import utils
 from Products.urban.UrbanVocabularyTerm import UrbanVocabulary
+from Products.urban.utils import cache_key_5min
 
 from zope.globalrequest import getRequest
 from zope.i18n import translate
@@ -158,6 +160,7 @@ class DashboardCollections(ConditionAwareCollectionVocabulary):
         """Format a UrbanType to the collection id"""
         return "collection_{0}".format(type.lower())
 
+    @ram.cache(cache_key_5min)
     def check_display(self, uid):
         folder = api.content.get(UID=uid)
         parent = aq_parent(folder)
