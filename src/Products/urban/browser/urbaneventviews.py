ff8539d8fa7e1c77aa9f0f6642cd58fccc774ab7
jimbiscuit <julienchandelle@gmail.com>
2025-04-18 15:24:23 +0200

    SUP-43917 Fix encoding in mail send notification

--- src/Products/urban/browser/urbaneventviews.py
+++ src/Products/urban/browser/urbaneventviews.py
@@ -256,10 +256,21 @@ class UrbanEventView(BrowserView):
         username = notif[-1].get("username", None)
         if username is None or username == "":
             username = user
+
+        try:
+            title = notif[-1]["title"].lower().decode("utf-8")
+        except UnicodeEncodeError:
+            title = notif[-1]["title"].lower().encode("utf-8").decode("utf-8")
+
+        try:
+            username = username.decode("utf-8")
+        except UnicodeEncodeError:
+            username = username.encode("utf-8").decode("utf-8")
+
         return _(
             "Mail already send for ${title} by ${user}, ${date}",
             mapping={
-                "title": notif[-1]['title'].lower(),
+                "title": title,
                 "user": username,
                 "date": notif[-1]["time"].strftime("%d/%m/%Y, %H:%M:%S")
             }
