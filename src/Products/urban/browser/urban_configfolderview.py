db796fe9385818f4f52c44ae09828abe999e08b0
Wiemboudabous <wiem.boudabous@imio.be>
2025-08-01 10:22:30 +0200

    add zip code listing

--- src/Products/urban/browser/urban_configfolderview.py
+++ src/Products/urban/browser/urban_configfolderview.py
@@ -114,6 +114,8 @@ class ContactsFolderView(UrbanConfigFolderView):
         )
         self.request.response.setHeader("Content-Length", str(len(emails)))
         return emails
+    
+    
 
 
 class ArchitectsFolderView(ContactsFolderView):
@@ -138,9 +140,27 @@ class GeometriciansFolderView(ContactsFolderView):
     def getCSSClass(self):
         base_css = super(GeometriciansFolderView, self).getCSSClass()
         return "{} contenttype-geometrician".format(base_css)
+class ContactsFolderViewMixin(UrbanConfigFolderView):
+     def getpostal(self):
+        context = aq_inner(self.context)
+        contacts = context.objectValues("Contact")
+        raw_zipcode = [
+            "%s %s <%s>" % (ct.getName1(), ct.getName2(), ct.getZipcode())
+            for ct in contacts
+            if ct.getEmail()
+        ]
+        zip_code = "; ".join(raw_zipcode)
+        #zip_code = emails.replace(",", " ")
+    
+        self.request.response.setHeader("Content-type", "text/plain;charset=utf-8")
+        self.request.response.setHeader(
+            "Content-Disposition", "attachment; filename=%s_code_postal.txt" % context.id
+        )
+        self.request.response.setHeader("Content-Length", str(len(zip_code)))
+        return zip_code
 
-
-class NotariesFolderView(ContactsFolderView):
+class NotariesFolderView(ContactsFolderView,ContactsFolderViewMixin):
+    
     """
     This manage the notaries folder config view
     """
@@ -150,7 +170,7 @@ class NotariesFolderView(ContactsFolderView):
     def getCSSClass(self):
         base_css = super(NotariesFolderView, self).getCSSClass()
         return "{} contenttype-notary".format(base_css)
-
+   
 
 class SortedTitleFolderView(BrowserView):
     """
---
+++ src/Products/urban/migration/update_280.py
@@ -0,0 +1,3 @@
+
+def reimport_actions(setup):
+    setup.runImportStepFromProfile('profile-Products.urban:default', 'actions')
