cb6784da7ff58e3d4751b4e55fff054dc7fd9022
WBoudabous <wiem.boudabous@imio.be>
2025-08-19 21:15:31 +0200

    URBBDC-3222: Add partie immeuble concernee attribute (#410)
    ---------
    
    Co-authored-by: Martin Peeters <martin.peeters@affinitic.be>

--- src/Products/urban/content/licence/Housing.py
+++ src/Products/urban/content/licence/Housing.py
@@ -1,21 +1,19 @@
 # -*- coding: utf-8 -*-
 
 from Products.Archetypes.atapi import BaseFolderSchema
-from Products.Archetypes.atapi import Schema
-from Products.Archetypes.atapi import registerType
-from Products.Archetypes.atapi import StringField
 from Products.Archetypes.atapi import LinesField
 from Products.Archetypes.atapi import MultiSelectionWidget
+from Products.Archetypes.atapi import registerType
+from Products.Archetypes.atapi import Schema
+from Products.Archetypes.atapi import StringField
+from Products.urban import interfaces
+from Products.urban import UrbanMessage as _
 from Products.urban.UrbanVocabularyTerm import UrbanVocabulary
+from Products.urban.config import PROJECTNAME
 from Products.urban.content.licence.CODT_BaseBuildLicence import CODT_BaseBuildLicence
+from Products.urban.content.licence.GenericLicence import GenericLicence
 from Products.urban.content.licence.Inspection import Inspection
 from zope.interface import implements
-
-from Products.urban import interfaces
-from Products.urban.config import PROJECTNAME
-from Products.urban.content.licence.GenericLicence import GenericLicence
-from Products.urban.UrbanVocabularyTerm import UrbanVocabulary
-from Products.urban import UrbanMessage as _
 
 
 Housing_schema = (
@@ -48,6 +46,20 @@ Housing_schema += Schema(
             schemata="urban_inspection",
             vocabulary=UrbanVocabulary("buildingtype", inUrbanConfig=True),
         ),
+        LinesField(
+            name="buildingPart",
+            widget=MultiSelectionWidget(
+                format="checkbox",
+                label=_("urban_label_buildingPart", default="buildingPart"),
+                i18n_domain="urban",
+            ),
+            multiValued=True,
+            optional=True,
+            schemata="urban_inspection",
+            vocabulary=UrbanVocabulary(
+                "part_of_the_building_concerned", inUrbanConfig=True
+            ),
+        ),
     )
 )
 
@@ -75,6 +87,10 @@ class Housing(Inspection, CODT_BaseBuildLicence):
     def displayBuildingType(self):
         """Return a list of selected buildingType items"""
         return self.getValuesForTemplate("buildingType")
+
+    def displayBuildingPart(self):
+        """Return a list of selected buiding part"""
+        return self.getValuesForTemplate("buildingPart")
 
 
 registerType(Housing, PROJECTNAME)
