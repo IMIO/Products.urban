7c6a4052dc6c614220f2de60989d4b86b463bf53
Wiemboudabous <wiem.boudabous@imio.be>
2025-09-16 16:00:02 +0200

    update inadmissible notification

--- src/Products/urban/notice/notification.py
+++ src/Products/urban/notice/notification.py
@@ -122,6 +122,7 @@ class NoticeNotification(NoticeElement):
             "TRANSFERT_DOSSIER": "ns3:TwiceDefaultRequest",
             "DEMANDE_EP": "ns3:PublicSurveyRequest",
             "NOTIF_COMPLETUDE1_INCOMPLET_COMMUNE":"ns3:TwiceDefaultRequest",
+            "NOTIF_COMPLETUDE2_IRRECEVABLE_COMMUNE":"ns3:TwiceDefaultRequest",
         }
         return self._get_data("specific", specific.get(self.notice_type), "ns3:municipalityReference")
 
@@ -151,6 +152,7 @@ class NoticeNotification(NoticeElement):
     @property
     def event_configs(self):
         portal_urban_folder = api.portal.get().urban.portal_urban
+        print(self.type,"TYPE////////////////////")
         licence_type_folder = getattr(portal_urban_folder, "{0}".format(self.type.lower()))
         return getattr(licence_type_folder, "eventconfigs")
 
