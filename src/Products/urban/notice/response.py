a67560c1e0df450eb3bdf046e5263d2bf4c7ab3e
Wiemboudabous <wiem.boudabous@imio.be>
2025-09-24 14:17:31 +0200

    update notification response

--- src/Products/urban/notice/response.py
+++ src/Products/urban/notice/response.py
@@ -51,3 +51,30 @@ class NoticeOutgoingNotification(NoticeResponse):
         return {
             "tns:municipalityReference": self._reference,
         }
+
+class NoticeOutgoingSummaryReportNotification(NoticeResponse):
+    state = "FINAL"
+    type = "SummaryReportResponse"
+
+    @property
+    def _reference(self):
+        return self._licence.getReference()
+
+    @property
+    def specific(self):
+        return {
+            "tns:municipalityReference": self._reference,
+            "tns:caracteristicsComment":,
+            "tns:decisionDate": self._display_decision_date,
+            "tns:displayDecisionStartDate": self._decision,
+            "tns:displayDecisionEndDate"
+            
+        }
+        
+    @property
+    def _display_decision_date(self):
+        return self.event.getDisplayDate()
+    #return self._licence.getDecisionDate()
+    @property
+    def _decision(self):
+        return self.event.getDecision()
