4ee63e81be33244bbdce17d64289eff0b3a4d8df
daggelpop <nicolas@affinitic.be>
2025-09-26 09:01:45 +0200

    URB-2652: Add rubrics to imported Notice licences (#429)

--- src/Products/urban/notice/notification.py
+++ src/Products/urban/notice/notification.py
@@ -6,6 +6,7 @@ from Products.urban.notice.document import NoticeDocument
 from Products.urban.notice.parcel import NoticeParcel
 from Products.urban.notice.party import NoticeParty
 from Products.urban.notice.sender import NoticeSender
+from Products.urban.utils import get_rubric_obj
 from datetime import datetime
 from lxml import etree
 from plone import api
@@ -92,16 +93,12 @@ class NoticeNotification(NoticeElement):
                     elif self.notification_subtype == "PE":
                         # PE class is not yet available in TRANSFERT_DOSSIER
                         # => extract it from XML document
-                        penv_classe = None
-                        for document in self.documents:
-                            if (
-                                document.document_mimetype == "application/xml"
-                                and document.document_type_code == "PJ_FORMULAIRE"
-                            ):
-                                tree = etree.parse(document.file)
-                                classe_elements = tree.xpath("/dataStore/item/classe")
-                                if len(classe_elements) == 1:
-                                    penv_classe = classe_elements[0].text
+                        classe_elements = self._pj_formulaire_xml_tree.xpath(
+                            "/dataStore/item/classe"
+                        )
+                        penv_classe = (
+                            classe_elements[0].text if len(classe_elements) == 1 else None
+                        )
                         self._licence_type = {
                             "1": "EnvClassOne",
                             "2": "EnvClassTwo",
@@ -128,6 +125,42 @@ class NoticeNotification(NoticeElement):
             "NOTIF_COMPLETUDE1_INCOMPLET_COMMUNE":"ns3:TwiceDefaultRequest",
         }
         return self._get_data("specific", specific.get(self.notice_type), "ns3:municipalityReference")
+
+    @property
+    def _pj_formulaire_xml_tree(self):
+        """Return XML tree with all data entered by residents in Mon Espace"""
+        for document in self.documents:
+            if (
+                document.document_mimetype == "application/xml"
+                and document.document_type_code == "PJ_FORMULAIRE"
+            ):
+                return etree.parse(document.file)
+        raise ValueError("No PJ_FORMULAIRE XML document found")
+
+    @property
+    def rubrics(self):
+        """Return the rubrics as a list of UIDs, if present"""
+        found_uids = []
+        missing_rubrics = []
+
+        if self.notice_type == "TRANSFERT_DOSSIER":
+            rubrique_elements = self._pj_formulaire_xml_tree.xpath(
+                "/dataStore/projet/rubriques/item"
+            )
+            for rubrique in rubrique_elements:
+                classe = rubrique.xpath("classe/text()")[0]
+                number = rubrique.xpath("numRubrique")[0].text
+                rubric_obj = get_rubric_obj(classe, number)
+                if rubric_obj:
+                    found_uids.append(rubric_obj.UID())
+                else:
+                    missing_rubrics.append("classe {}, {}".format(classe, number))
+
+        if missing_rubrics:
+            raise ValueError(
+                "cannot find these rubrics: {}".format(" | ".join(missing_rubrics))
+            )
+        return found_uids
 
     @property
     def send_date(self):
