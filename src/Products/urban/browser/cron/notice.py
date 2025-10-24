e98aaabd497783e890def68e30cecfacd1528c1c
Wiemboudabous <wiem.boudabous@imio.be>
2025-10-24 10:09:45 +0200

    fix conflit

--- src/Products/urban/browser/cron/notice.py
+++ src/Products/urban/browser/cron/notice.py
@@ -71,6 +71,8 @@ class ImportFromNoticeView(BrowserView):
             self.process_not_admissible_folder_notification_second_tour(
                 detailed_notification
             )
+        if detailed_notification.notice_type == "NOTIF_COMPLETUDE2_IRRECEVABLE_COMMUNE":
+            self.process_inadmissible_folder_notification(detailed_notification)
 
     def _transfert_dossier(self, detailed_notification):
         container = detailed_notification.container
@@ -156,3 +158,8 @@ class ImportFromNoticeView(BrowserView):
         licence = detailed_notification.licence
         self.update_license(licence, detailed_notification, event_type="dossier-irrecevable")
         transaction.commit()
+    
+    def process_inadmissible_folder_notification(self, detailed_notification):
+        license = detailed_notification.licence
+        self.update_license(license, detailed_notification, event_type="dossier-irrecevable")
+        transaction.commit() 
