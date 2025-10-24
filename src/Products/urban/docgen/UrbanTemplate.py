3714d4f468d4ba5ff3c78d71222c7b5b5a6f4357
Martin Peeters <martin.peeters@affinitic.be>
2025-10-24 17:08:14 +0200

    URBBDC-3205 Urban Liege docgen

--- src/Products/urban/docgen/UrbanTemplate.py
+++ src/Products/urban/docgen/UrbanTemplate.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
 
 from collective.documentgenerator import _
-from collective.documentgenerator.content.pod_template import IConfigurablePODTemplate
 from collective.documentgenerator.content.pod_template import ConfigurablePODTemplate
-
+from collective.documentgenerator.content.pod_template import IConfigurablePODTemplate
 from plone.autoform import directives as form
-
 from z3c.form.browser.select import SelectWidget
-
 from zope import schema
 from zope.interface import implements
 
 import logging
-logger = logging.getLogger('Products.urban: UrbanTemplate')
+
+
+logger = logging.getLogger("Products.urban: UrbanTemplate")
 
 
 class IUrbanTemplate(IConfigurablePODTemplate):
@@ -20,13 +19,20 @@ class IUrbanTemplate(IConfigurablePODTemplate):
     UrbanTemplate dexterity schema.
     """
 
-    form.widget('pod_portal_types', SelectWidget, multiple='multiple', size=15)
+    form.widget("pod_portal_types", SelectWidget, multiple="multiple", size=15)
     pod_portal_types = schema.List(
-        title=_(u'Allowed portal types'),
-        description=_(u'Select for which content types the template will be available.'),
-        value_type=schema.Choice(source='collective.documentgenerator.PortalTypes'),
+        title=_(u"Allowed portal types"),
+        description=_(
+            u"Select for which content types the template will be available."
+        ),
+        value_type=schema.Choice(source="collective.documentgenerator.PortalTypes"),
         required=False,
-        default=['UrbanEvent', 'UrbanEventInquiry', 'UrbanEventOpinionRequest', 'UrbanEventAnnouncement']
+        default=[
+            "UrbanEvent",
+            "UrbanEventInquiry",
+            "UrbanEventOpinionRequest",
+            "UrbanEventAnnouncement",
+        ],
     )
 
 
