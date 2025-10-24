4f6663284bd9223c590970545d1a1f2df44cbb32
Wiemboudabous <wiem.boudabous@imio.be>
2025-10-24 11:55:43 +0200

    coninue resolving conflit

--- src/Products/urban/browser/cron/notice.py
+++ src/Products/urban/browser/cron/notice.py
@@ -63,17 +63,14 @@ class ImportFromNoticeView(BrowserView):
         detailed_notification = self.notice_service.get_notification(
             notification["noticeId"]
         )
-        if detailed_notification.notice_type == "TRANSFERT_DOSSIER":
-            self._transfert_dossier(detailed_notification)
+        #if detailed_notification.notice_type == "TRANSFERT_DOSSIER":
+        #    self._transfert_dossier(detailed_notification)
         if detailed_notification.notice_type == "NOTIF_COMPLETUDE1_INCOMPLET_COMMUNE":
             self.process_incomplete_folder_notification(detailed_notification)
-<<<<<<< HEAD
         if detailed_notification.notice_type == "NOTIF_COMPLETUDE2_NON_RECEVABLE_COMMUNE":
             self.process_not_admissible_folder_notification_second_tour(
                 detailed_notification
             )
-=======
->>>>>>> 405c0e39b (implement inadmissible notification secod tour)
         if detailed_notification.notice_type == "NOTIF_COMPLETUDE2_IRRECEVABLE_COMMUNE":
             self.process_inadmissible_folder_notification(detailed_notification)
 
@@ -127,6 +124,7 @@ class ImportFromNoticeView(BrowserView):
         transaction.commit()  # Useful in case of an error
     
     def update_license(self, license, detailed_notification, event_type=None):
+        
         if not event_type:
             return
         event_configs = detailed_notification.event_configs
@@ -142,12 +140,6 @@ class ImportFromNoticeView(BrowserView):
                 configs.append((etype,event_config))
         if not configs:
             return
-<<<<<<< HEAD
-=======
-        event_type_to_transition = {"dossier-incomplet": "isincomplete",
-                                    "dossier-irrecevable": "inacceptable"}
-
->>>>>>> 405c0e39b (implement inadmissible notification secod tour)
         with api.env.adopt_roles(["Manager"]):
             for etype, event_config in configs:
                 event = license.createUrbanEvent(event_config)
@@ -162,20 +154,14 @@ class ImportFromNoticeView(BrowserView):
         license = detailed_notification.licence
         self.update_license(license, detailed_notification, event_type="dossier-incomplet")
         transaction.commit() 
-<<<<<<< HEAD
 
     def process_not_admissible_folder_notification_second_tour(self, detailed_notification):
         licence = detailed_notification.licence
         self.update_license(licence, detailed_notification, event_type="dossier-irrecevable")
         transaction.commit()
-=======
->>>>>>> 405c0e39b (implement inadmissible notification secod tour)
     
     def process_inadmissible_folder_notification(self, detailed_notification):
         license = detailed_notification.licence
+        print(license,"-*-*-*-*-*lICENCE")
         self.update_license(license, detailed_notification, event_type="dossier-irrecevable")
-<<<<<<< HEAD
         transaction.commit()
-=======
-        transaction.commit() 
->>>>>>> 405c0e39b (implement inadmissible notification secod tour)
--- src/Products/urban/browser/cron/tests/test_notice.py
+++ src/Products/urban/browser/cron/tests/test_notice.py
@@ -150,7 +150,7 @@ class TestNoticeCronPE2(unittest.TestCase):
             api.content.delete(obj=licence)
 
 
-    @mock.patch(
+    """@mock.patch(
         "Products.urban.services.notice.WebserviceNotice.get_notifications",
         return_value=load_notif_json("DEMANDE_EP", "1342038_notifications.json"),
     )
@@ -163,7 +163,7 @@ class TestNoticeCronPE2(unittest.TestCase):
 
         with api.env.adopt_roles(["Manager"]):
             import_view = self.portal.restrictedTraverse("@@import-from-notice")
-            return import_view()
+            return import_view()"""
     
     @mock.patch(
         "Products.urban.services.notice.WebserviceNotice.get_notifications",
@@ -270,13 +270,13 @@ class TestNoticeCronPE2(unittest.TestCase):
     
     @mock.patch(
         "Products.urban.services.notice.WebserviceNotice.get_notifications",
-        return_value=load_notif_json("INADMISSIBLE", "xx-NOTIFICATIONS.json"),
+        return_value=load_notif_json("INADMISSIBLE", "1443524-NOTIFICATIONS.json"),
     )
     @mock.patch(
         "Products.urban.services.notice.WebserviceNotice._get_notification",
-        return_value=MockedRequest(load_notif_json("INADMISSIBLE", "xx-INADMISSIBLE-NOTIFICATION.json")),
+        return_value=MockedRequest(load_notif_json("INADMISSIBLE", "1443524-INADMISSIBLE-NOTIFICATION.json")),
     )
-    def _create_incomplete_folder(self, notif_patch, notifs_patch):
+    def _create_inadmissible_folder(self, notif_patch, notifs_patch):
         notif_patch, notifs_patch  
         self.notif_patch = notif_patch
         
