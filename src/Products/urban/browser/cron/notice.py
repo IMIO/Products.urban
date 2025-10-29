f4d2fa029187091e2a176a3997f3f83a5b5d2729
Wiemboudabous <wiem.boudabous@imio.be>
2025-10-29 09:18:45 +0100

    apply flake8

--- src/Products/urban/browser/cron/notice.py
+++ src/Products/urban/browser/cron/notice.py
@@ -194,12 +194,10 @@ class ImportFromNoticeView(BrowserView):
             licence, detailed_notification, event_type="dossier-incomplet"
         )
         transaction.commit()
-        
+
     def process_extension_of_deadline_notification(self, detailed_notification):
         license = detailed_notification.licence
-        
         license.getField('prorogation').set(license, True)
         license.reindexObject()
         self.update_license(license, detailed_notification, event_type="prorogation-30-jours")
         notify(ObjectModifiedEvent(license))
-        
