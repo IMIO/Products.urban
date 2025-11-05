8a7b2fb778a6525dceea7d4e71827d16924f458f
daggelpop <nicolas@affinitic.be>
2025-11-05 08:27:44 +0100

    URB-3307: Handle Notice errors (#434)

--- src/Products/urban/browser/notice_settings.py
+++ src/Products/urban/browser/notice_settings.py
@@ -2,10 +2,12 @@
 
 from Products.statusmessages.interfaces import IStatusMessage
 from Products.urban import UrbanMessage as _
+from plone import api
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 from plone.app.registry.browser.controlpanel import RegistryEditForm
 from z3c.form import button
 from z3c.form.interfaces import DISPLAY_MODE
+from zExceptions import Redirect
 from zope import schema
 from zope.interface import Interface
 
@@ -30,6 +32,13 @@ class INoticeSettings(Interface):
     last_import_date = schema.Datetime(
         title=_("Last import date from Notice Webservice"),
         required=False,
+    )
+
+    failed_notifications = schema.List(
+        title=_("Failed notifications"),
+        value_type=schema.ASCIILine(),
+        required=False,
+        default=[],
     )
 
 
@@ -59,6 +68,26 @@ class NoticeSettingsEditForm(RegistryEditForm):
         IStatusMessage(self.request).addStatusMessage(_("Edit cancelled"), "info")
         self.request.response.redirect(
             "%s/%s" % (self.context.absolute_url(), self.control_panel_view)
+        )
+
+    def show_retry_button(self):
+        failed_notifications = api.portal.get_registry_record(
+            "Products.urban.browser.notice_settings.INoticeSettings.failed_notifications",
+            default=[],
+        )
+        return failed_notifications
+
+    @button.buttonAndHandler(
+        _("Retry failed notifications"),
+        name="retry",
+        condition=lambda form: form.show_retry_button(),
+    )
+    def handleRetry(self, action):
+        IStatusMessage(self.request).addStatusMessage(
+            _("Retrying failed notifications"), "info"
+        )
+        raise Redirect(
+            "%s/%s" % (api.portal.get().absolute_url(), "@@import-from-notice?retry=1")
         )
 
 
