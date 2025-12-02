39085a401065bc268276f617aa21716b36720afe
Julien Chandelle <julienchandelle@gmail.com>
2025-12-02 10:14:14 +0100

    URBBDC-3205: Urban Liege Dashboard / Action Panel (#448)

--- src/Products/urban/dashboard/utils.py
+++ src/Products/urban/dashboard/utils.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+from Products.urban.utils import get_env_variable_value
+
+import os
+
 
 def get_procedure_category(context, request):
     """
@@ -12,3 +16,9 @@ def get_procedure_category(context, request):
     if context.id.startswith("codt"):
         return "CODT"
     return "CWATUPE"
+
+
+def switch_config_folder(config_file, base_folder="config"):
+    current_path = os.path.dirname(os.path.abspath(__file__))
+    config = get_env_variable_value("URBAN_DASHBOARD_CONFIGS", "classic")
+    return os.path.join(current_path, base_folder, config, config_file)
