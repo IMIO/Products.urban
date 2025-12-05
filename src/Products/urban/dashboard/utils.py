ad1f3ddb6751e775a208d508852c29c1ac2dbf23
Julien Chandelle <julienchandelle@gmail.com>
2025-12-05 16:34:22 +0100

    URBBDC-3205: Fix env variable getter (#474)

--- src/Products/urban/dashboard/utils.py
+++ src/Products/urban/dashboard/utils.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 
-from Products.urban.utils import get_env_variable_value
-
 import os
 
 
@@ -20,5 +18,5 @@ def get_procedure_category(context, request):
 
 def switch_config_folder(config_file, base_folder="config"):
     current_path = os.path.dirname(os.path.abspath(__file__))
-    config = get_env_variable_value("URBAN_DASHBOARD_CONFIGS", "classic")
+    config = os.environ.get("URBAN_DASHBOARD_CONFIGS", "classic")
     return os.path.join(current_path, base_folder, config, config_file)
