d1b21cda5104369d7360c212f58f8ec005ee167f
Julien Chandelle <julienchandelle@gmail.com>
2025-12-05 15:02:54 +0100

    URBBDC-3205: Urban Liege browser (#469)

--- src/Products/urban/browser/licenceconfigview.py
+++ src/Products/urban/browser/licenceconfigview.py
@@ -2,6 +2,7 @@
 
 from Acquisition import aq_inner
 from Products.Five import BrowserView
+from plone import api
 
 
 class LicenceConfigView(BrowserView):
@@ -45,3 +46,18 @@ class LicenceConfigView(BrowserView):
         context = aq_inner(self.context)
         schedule_folder = getattr(context, "schedule")
         return [schedule_folder]
+
+    def getTestConfigs(self):
+        context = aq_inner(self.context)
+        test_folder = getattr(context, "test")
+        return [test_folder]
+
+    def get_events(self):
+        licence = aq_inner(self.context)
+        tool = api.portal.get_tool("portal_types")
+        portal_type = tool[licence.licencePortalType]
+        config_id = portal_type.id.lower()
+        portal_urban = api.portal.get_tool("portal_urban")
+        eventtypes = portal_urban.listEventTypes(licence, urbanConfigId=config_id)
+        events_objects = [event.getObject() for event in eventtypes]
+        return events_objects
