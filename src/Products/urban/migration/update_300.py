f643ca51c3db15a43b78a14032b9e99c51b68c02
Martin Peeters <martin.peeters@affinitic.be>
2025-12-07 22:07:37 +0100

    URBBDC-3205: Add a missing upgradre step for offdays settings and fix upgrade step order

--- src/Products/urban/migration/update_300.py
+++ src/Products/urban/migration/update_300.py
@@ -1,5 +1,12 @@
+# -*- coding: utf-8 -*-
+
 from Products.urban.utils import set_default_optional_field
 from plone import api
+from plone.registry import Record
+from plone.registry.field import Choice
+from plone.registry.field import List
+from plone.registry.interfaces import IRegistry
+from zope.component import getUtility
 
 import logging
 
@@ -20,3 +27,48 @@ def set_additional_reference_as_default(context):
 def install_urban_core(context):
     portal_setup = api.portal.get_tool('portal_setup')
     portal_setup.runAllImportStepsFromProfile('profile-imio.urban.core:default')
+
+
+def add_missing_registry_record(context):
+    logger = logging.getLogger("urban: add offdays settings")
+    logger.info("starting migration steps")
+
+    from collective.z3cform.datagridfield.registry import DictRow
+    from Products.urban.browser.offdays_settings import IOffDay
+    from Products.urban.browser.offdays_settings import IOffDayPeriod
+
+    registry = getUtility(IRegistry)
+
+    key = "Products.urban.browser.offdays_settings.IOffDays.week_offdays"
+    registry_field = List(
+        title=u"Week off days",
+        description=u"",
+        value_type=Choice(
+            title=u"weekdays", vocabulary=u"urban.vocabularies.weekdays"
+        ),
+    )
+    registry_record = Record(registry_field)
+    registry_record.value = []
+    registry.records[key] = registry_record
+
+    key = "Products.urban.browser.offdays_settings.IOffDays.periods"
+    registry_field = List(
+        title=u"Off days period",
+        description=u"",
+        value_type=DictRow(title=u"Period", schema=IOffDayPeriod, required=False),
+    )
+    registry_record = Record(registry_field)
+    registry_record.value = []
+    registry.records[key] = registry_record
+
+    key = "Products.urban.browser.offdays_settings.IOffDays.offdays"
+    registry_field = List(
+        title=u"Off days",
+        description=u"",
+        value_type=DictRow(title=u"Day", schema=IOffDay, required=False),
+    )
+    registry_record = Record(registry_field)
+    registry_record.value = []
+    registry.records[key] = registry_record
+
+    logger.info("migration done!")
