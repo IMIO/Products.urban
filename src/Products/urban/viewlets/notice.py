c1afa9a9a898d905847caa4172d2b5d49db47641
Nicolas Demonté <nicolas@affinitic.be>
2025-12-15 15:34:12 +0100

    Add translations

--- src/Products/urban/viewlets/notice.py
+++ src/Products/urban/viewlets/notice.py
@@ -3,6 +3,7 @@
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from plone.app.layout.viewlets import ViewletBase
 from zope.annotation.interfaces import IAnnotations
+from zope.i18n import translate
 
 
 class NoticeTransmitState(ViewletBase):
@@ -12,7 +13,10 @@ class NoticeTransmitState(ViewletBase):
         annotations = IAnnotations(self.context)
         dates = annotations.get("notice_transmit_dates", {})
         return [
-            {"label": label, "date": date.strftime("%d/%m/%Y")}
+            {
+                "label": translate(label, "urban", context=self.REQUEST),
+                "date": date.strftime("%d/%m/%Y"),
+            }
             for (label, date) in dates.items()
         ]
 
