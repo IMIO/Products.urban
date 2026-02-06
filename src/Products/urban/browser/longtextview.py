617de151e0913233087328b43534e40d79bb5555
Nicolas Demonté <nicolas@affinitic.be>
2026-02-06 14:38:16 +0100

    Show form data sent to Notice

--- src/Products/urban/browser/longtextview.py
+++ src/Products/urban/browser/longtextview.py
@@ -2,6 +2,7 @@ from Acquisition import aq_inner
 from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone import PloneMessageFactory as _
 from Products.Five import BrowserView
+from zope.annotation.interfaces import IAnnotations
 from zope.i18n import translate
 
 
@@ -77,3 +78,45 @@ class PmSummaryTextView(BrowserView):
         summary = "<br /><br />".join(summary)
 
         return summary
+
+
+class NoticeFormDataTextView(BrowserView):
+    def __init__(self, context, request):
+        super(NoticeFormDataTextView, self).__init__(context, request)
+        self.action_code = self.request.get("action_code", "")
+        if not self.action_code:
+            plone_utils = getToolByName(self.context, "plone_utils")
+            plone_utils.addPortalMessage(_("Nothing to show !!!"), type="error")
+
+    def getFieldText(self):
+        """
+        Returns an HTML structure of the form data sent with a particular action code.
+        This data comes from a dict stored as annotation on the event.
+        """
+
+        annotations = IAnnotations(self.context)
+        transmits = annotations.get("notice_transmit_dates", {})
+        action_data = transmits.get(self.action_code, {})
+        field_values = action_data.get("field_values", {})
+
+        if not field_values:
+            return _("Nothing to show !!!")
+
+        html_blocks = []
+        for field_id, field_value in field_values.items():
+            field_title = translate(
+                field_id,
+                context=self.request,
+            )
+            html_blocks.append(u"<h2>{}</h2>".format(field_title))
+            if field_id in ("college_opinion", "college_decision"):
+                html_blocks.append(u"<div>{}</div>".format(field_value))
+            elif field_id == "documents":
+                points = [
+                    u"<li>{}</li>".format(document["title"]) for document in field_value
+                ]
+                html_blocks.append(u"<ul>{}</ul>".format("\n".join(points)))
+                if not field_value:
+                    html_blocks.append(u"<div>{}</div>".format(_("content_none")))
+
+        return u"<br />\n".join(html_blocks)
