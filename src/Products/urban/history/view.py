f70fd22eb6afc2c1856b64c23886735096e342fa
Julien Chandelle <julienchandelle@gmail.com>
2026-02-09 11:13:23 +0100

    SUP-50324: Fix history view for missing parameter (#510)

--- src/Products/urban/history/view.py
+++ src/Products/urban/history/view.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from imio.history.browser.views import IHContentHistoryView
+from plone import api
 
 
 class SpecificHistoryView(IHContentHistoryView):
     def __init__(self, context, request):
         self.context = context
         self.request = request
+        self.site_url = api.portal.get().absolute_url()
         fieldname = self.request.get("item", "")
         self.histories_to_handle = (
             u"update_{0}".format(fieldname),
