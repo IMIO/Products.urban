d8464882c346e6d417c14a1f9c0e538537f59b04
Julien Chandelle <julienchandelle@gmail.com>
2026-02-12 11:25:04 +0100

    URBBDC-3205: Add view to generate PortionOut plain file list (#514)

---
+++ src/Products/urban/browser/portionOut_migration.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+from Products.urban.migration.to_DX.migration_utils import GenerateCotentTypeList
+
+
+class GeneratePortioOutList(GenerateCotentTypeList):
+    src_portal_type = "PortionOut"
