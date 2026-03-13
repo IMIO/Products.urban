cda9f3f492127f7c77dfbfe80901f7ffe7f61802
Julien Chandelle <julienchandelle@gmail.com>
2026-03-13 10:00:51 +0100

    SUP-51288: Add an upgrade step to fix parcel `street_number` (#537)

---
+++ src/Products/urban/browser/create_list_file.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from Products.urban.migration.to_DX.migration_utils import GenerateContentTypeList
+
+
+class GeneratePortioOutList(GenerateContentTypeList):
+    src_portal_type = "PortionOut"
+
+
+class GenerateParcelList(GenerateContentTypeList):
+    src_portal_type = "Parcel"
+    sort_order="ascending"
+
+    def condition(self, data):
+        number = getattr(data, "street_number", None)
+        if number is None:
+            return False
+        return isinstance(number, int) or isinstance(number, float)
