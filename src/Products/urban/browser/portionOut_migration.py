0f5dd8703959c2b4e4343271cf0002a7d649af5b
Julien Chandelle <julienchandelle@gmail.com>
2026-03-13 10:00:51 +0100

    SUP-51288: Add an upgrade step to fix parcel `street_number` (#537)

--- src/Products/urban/browser/portionOut_migration.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from Products.urban.migration.to_DX.migration_utils import GenerateCotentTypeList
-
-
-class GeneratePortioOutList(GenerateCotentTypeList):
-    src_portal_type = "PortionOut"
