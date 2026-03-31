992c3eaffa25ce74eee8ee505f929c1eda21648c
Nicolas Demonté <nicolas@affinitic.be>
2026-03-31 19:42:04 +0200

    URB-3430: Add form for `RENVOI_DATES_EP`

--- src/Products/urban/browser/actionspanel/actionspanel.py
+++ src/Products/urban/browser/actionspanel/actionspanel.py
@@ -48,6 +48,7 @@ class EventActionsPanelView(ActionsPanelView):
             "transfer_opinion",
             "transfer_decision",
             "transfer_decision_display",
+            "transfer_dates_gesper_ep",
         )
 
 
